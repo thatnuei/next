a3cb60999c023f024bf271bf1b88079121fe7f01
nuei <61118778+thatnuei@users.noreply.github.com>
2018-08-29 22:16:45 -0500

    write up full select character screen cosmetics

--- src/app/SelectCharacterModal.tsx
+++ src/app/SelectCharacterModal.tsx
@@ -1,4 +1,8 @@
+import { Formik, FormikProps } from "formik"
+import { action, observable } from "mobx"
+import { observer } from "mobx-react"
 import React from "react"
+import { getAvatarUrl } from "../network/api"
 import { sessionStore } from "../session/SessionStore"
 import { Button } from "../ui/Button"
 import { Form } from "../ui/Form"
@@ -6,31 +10,92 @@ import { FormField } from "../ui/FormField"
 import { Modal } from "../ui/Modal"
 import { styled } from "../ui/styled"
 
+type FormValues = {
+  character: string
+}
+
+@observer
 export class SelectCharacterModal extends React.Component {
+  @observable
+  lastCharacter?: string
+
+  @action
+  loadLastCharacter() {
+    this.lastCharacter = localStorage.getItem("lastCharacter") || sessionStore.characters[0]
+  }
+
+  componentDidMount() {
+    this.loadLastCharacter()
+  }
+
   render() {
     return (
       <Modal>
         <ContentContainer>
           <HeaderText>Select a Character</HeaderText>
-          <Form>
-            <FormField>
-              <div style={{ textAlign: "center" }}>
-                <select>
-                  {sessionStore.characters.map((name) => (
-                    <option value={name} key={name}>
-                      {name}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            </FormField>
-            <div style={{ textAlign: "center" }}>
-              <Button>Enter Chat</Button>
-            </div>
-          </Form>
+          {this.lastCharacter ? (
+            <Formik<FormValues>
+              initialValues={{
+                character: this.lastCharacter,
+              }}
+              render={this.renderForm}
+              onSubmit={this.handleSubmit}
+            />
+          ) : null}
         </ContentContainer>
       </Modal>
     )
+  }
+
+  private renderForm = (props: FormikProps<FormValues>) => {
+    const formStyle: React.CSSProperties = {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    }
+
+    return (
+      <Form onSubmit={props.handleSubmit} style={formStyle}>
+        <FormField>
+          <img
+            key={props.values.character}
+            src={getAvatarUrl(props.values.character)}
+            width={100}
+            height={100}
+          />
+        </FormField>
+        <FormField>
+          <div style={{ textAlign: "center" }}>
+            <select
+              name="character"
+              value={props.values.character}
+              onChange={(event) => this.handleChange(event, props)}
+            >
+              {sessionStore.characters.map((name) => (
+                <option value={name} key={name}>
+                  {name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </FormField>
+        <div style={{ textAlign: "center" }}>
+          <Button>Enter Chat</Button>
+        </div>
+      </Form>
+    )
+  }
+
+  private handleChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+    props: FormikProps<FormValues>,
+  ) => {
+    localStorage.setItem("lastCharacter", event.target.value)
+    props.handleChange(event)
+  }
+
+  private handleSubmit = (values: FormValues) => {
+    console.log(values)
   }
 }
 
