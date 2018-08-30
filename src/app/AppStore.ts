437ceecaa01f4b9d98788654e2880aac73e637f7
nuei <61118778+thatnuei@users.noreply.github.com>
2018-08-29 23:19:13 -0500

    handle appropriate socket events in AppStore

--- src/app/AppStore.ts
+++ src/app/AppStore.ts
@@ -1,10 +1,20 @@
 import { action, observable } from "mobx"
+import { socketStore } from "../network/SocketStore"
 
-export type AppScreen = "setup" | "login" | "selectCharacter"
+export type AppScreen = "setup" | "login" | "selectCharacter" | "chat"
 
 export class AppStore {
   @observable
   screen: AppScreen = "setup"
+
+  setupListeners() {
+    socketStore.addCommandListener("IDN", () => this.setScreen("chat"))
+
+    socketStore.addDisconnectListener(() => {
+      alert("Disconnected from server :(")
+      this.setScreen("login")
+    })
+  }
 
   @action
   setScreen(screen: AppScreen) {
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import { configure } from "mobx"
 import React from "react"
 import ReactDOM from "react-dom"
 import { App } from "./app/App"
+import { appStore } from "./app/AppStore"
 import { chatStore } from "./chat/ChatStore"
 import { applyGlobalStyles } from "./ui/globalStyles"
 
 configure({ enforceActions: "observed" })
 
+appStore.setupListeners()
 chatStore.setupListeners()
 
 applyGlobalStyles()
