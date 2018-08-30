407d8866f42ad3afc195cf2646afbe8fccfa1b57
nuei <61118778+thatnuei@users.noreply.github.com>
2018-08-29 20:42:44 -0500

    enable strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
+import { configure } from "mobx"
 import React from "react"
 import ReactDOM from "react-dom"
 import { App } from "./app/App"
 import { applyGlobalStyles } from "./ui/globalStyles"
+
+configure({ enforceActions: true })
 
 applyGlobalStyles()
 ReactDOM.render(<App />, document.querySelector("#root"))
