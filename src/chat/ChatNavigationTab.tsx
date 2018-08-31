929f3c2a02695ba9f4f4e7d8660166064c539196
nuei <61118778+thatnuei@users.noreply.github.com>
2018-08-30 23:35:21 -0500

    some tab tweaks: add close button + account for long titles

--- src/chat/ChatNavigationTab.tsx
+++ src/chat/ChatNavigationTab.tsx
@@ -1,3 +1,4 @@
+import { mdiClose } from "@mdi/js"
 import { Icon } from "@mdi/react"
 import React from "react"
 import { Avatar } from "../character/Avatar"
@@ -13,24 +14,26 @@ type ChatNavigationTabProps = {
 
 export const ChatNavigationTab = (props: ChatNavigationTabProps) => {
   return (
-    <Anchor {...props}>
-      <IconContainer>
-        {props.icon && <Icon path={props.icon} size={1} color={clouds} />}
-        {props.avatar && <Avatar name={props.avatar} size={24} />}
-      </IconContainer>
-      <div>{props.text}</div>
-    </Anchor>
+    <Container {...props}>
+      <TitleContainer>
+        <IconContainer>
+          {props.icon && <Icon path={props.icon} size={1} color={clouds} />}
+          {props.avatar && <Avatar name={props.avatar} size={24} />}
+        </IconContainer>
+        <TextContainer>{props.text}</TextContainer>
+      </TitleContainer>
+      <CloseButton>
+        <Icon path={mdiClose} size={0.8} color={clouds} />
+      </CloseButton>
+    </Container>
   )
 }
 
-const IconContainer = styled.div`
-  padding-right: 0.4rem;
+const Container = styled.div<ChatNavigationTabProps>`
+  display: flex;
+  align-items: center;
 
-  /* to remove the random spacing on the icon */
-  svg,
-  img {
-    display: block;
-  }
+  ${(props) => (props.active ? activeStyle : inactiveStyle)};
 `
 
 const activeStyle = css`
@@ -45,11 +48,41 @@ const inactiveStyle = css`
   }
 `
 
-const Anchor = styled.a.attrs({ href: "#" })<ChatNavigationTabProps>`
+const TitleContainer = styled.a.attrs({ href: "#" })`
+  flex-grow: 1;
+
   padding: 0.6rem 0.6rem;
 
   display: flex;
   align-items: center;
 
-  ${(props) => (props.active ? activeStyle : inactiveStyle)};
+  white-space: nowrap;
+  overflow: hidden;
+`
+
+const IconContainer = styled.div`
+  padding-right: 0.4rem;
+
+  /* to remove the random spacing on the icon */
+  svg,
+  img {
+    display: block;
+  }
+`
+
+const TextContainer = styled.div`
+  text-overflow: ellipsis;
+  flex-grow: 1;
+  overflow: hidden;
+`
+
+const CloseButton = styled.a.attrs({ href: "#" })`
+  padding: 0.3rem 0.4rem;
+  flex-shrink: 0;
+
+  /* to remove the random spacing on the icon */
+  svg,
+  img {
+    display: block;
+  }
 `
