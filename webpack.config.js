a65ea0b396e0f6748cb8b09175f2f19ba21e697b
nuei <61118778+thatnuei@users.noreply.github.com>
2018-08-30 20:41:06 -0500

    use custom webpack build

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// @ts-check
+const { join } = require("path")
+const HtmlPlugin = require("html-webpack-plugin")
+const CleanPlugin = require("clean-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
+
+const rootFolder = __dirname
+const sourceFolder = join(rootFolder, "src")
+const buildFolder = join(rootFolder, "build")
+const publicFolder = join(rootFolder, "public")
+
+/** @type {import('webpack').Configuration} */
+const config = {
+  context: rootFolder,
+  entry: join(sourceFolder, "index"),
+  output: {
+    path: buildFolder,
+    filename: "[name].[hash].js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, use: "ts-loader" }],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  // @ts-ignore
+  plugins: [
+    new HtmlPlugin({
+      template: join(sourceFolder, "index.html"),
+    }),
+    new CleanPlugin(buildFolder, { verbose: false }),
+    new CopyPlugin([{ from: publicFolder, to: buildFolder }]),
+  ],
+  mode: "development",
+}
+
+module.exports = config
