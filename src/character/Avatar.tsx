d83107398b4d6f56a0de20dbb09366be137ba338
nuei <61118778+thatnuei@users.noreply.github.com>
2018-08-30 23:11:37 -0500

    allow Avatar to accept size prop

--- src/character/Avatar.tsx
+++ src/character/Avatar.tsx
@@ -1,8 +1,16 @@
 import React from "react"
 import { getAvatarUrl } from "../flist/helpers"
 
-type Props = { name: string }
+type Props = {
+  name: string
+  size?: number
+}
 
 export const Avatar = (props: Props) => (
-  <img src={getAvatarUrl(props.name)} alt={`Avatar for ${props.name}`} width={100} height={100} />
+  <img
+    src={getAvatarUrl(props.name)}
+    alt={`Avatar for ${props.name}`}
+    width={props.size || 100}
+    height={props.size || 100}
+  />
 )
