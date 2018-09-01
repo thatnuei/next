d18d727f0048311cef5df09a6bc81d22c0a7bf6e
nuei <61118778+thatnuei@users.noreply.github.com>
2018-09-01 15:52:39 -0500

    message state changes
    - have message component accept a full message model and use that
    - only store timestamp numbers in state, no full date objects
    - precalculate the locale time string, render performance gains

--- src/message/Message.tsx
+++ src/message/Message.tsx
@@ -1,40 +1,28 @@
 import React from "react"
-import { CharacterModel } from "../character/CharacterModel"
 import { CharacterName } from "../character/CharacterName"
 import { styled } from "../ui/styled"
+import { MessageModel, MessageType } from "./MessageModel"
 
-const messageTypeHighlights = {
+const messageTypeHighlights: Record<MessageType, string> = {
   ad: "rgba(39, 174, 96, 0.15)",
-  mention: "rgba(41, 128, 185, 0.3)",
   admin: "rgba(231, 76, 60, 0.2)",
-  none: "transparent",
+  normal: "transparent",
+  // mention: "rgba(41, 128, 185, 0.3)",
 }
 
-const senderCharacter = new CharacterModel("Athena Light", "Female", "online")
-
 type Props = {
-  type?: keyof typeof messageTypeHighlights
+  model: MessageModel
 }
 
 export const Message = (props: Props) => {
+  const { sender, text, type, localeTimeString } = props.model
+
   return (
     <Container>
-      <Highlight style={{ backgroundColor: messageTypeHighlights[props.type || "none"] }}>
-        <Timestamp>[{new Date().toLocaleTimeString()}]</Timestamp>
-        <Sender>
-          <CharacterName character={senderCharacter} />
-        </Sender>
-        <MessageText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae eros in lacus varius
-          semper. Quisque at massa ac risus consectetur semper. Interdum et malesuada fames ac ante
-          ipsum primis in faucibus. Curabitur pretium ligula non ligula sollicitudin, et varius
-          lectus blandit. Vivamus rutrum, turpis a porttitor luctus, metus elit sagittis quam,
-          eleifend fringilla mi tortor vel lacus. Nunc ac feugiat urna. Fusce vel diam mollis,
-          rutrum odio at, aliquam massa. Praesent at purus vel justo malesuada finibus. Nam sit amet
-          sodales magna. Morbi vestibulum pulvinar mauris ac dictum. Curabitur ex est, fringilla
-          sodales nisl at, feugiat maximus enim. Nam commodo ultrices ligula sit amet hendrerit.
-          Nunc in augue faucibus, ultrices augue consequat, congue ex. Ut a fringilla lectus.
-        </MessageText>
+      <Highlight style={{ backgroundColor: messageTypeHighlights[type] }}>
+        <Timestamp>[{localeTimeString}]</Timestamp>
+        <Sender>{sender ? <CharacterName name={sender} /> : "System"}</Sender>
+        <MessageText>{text}</MessageText>
       </Highlight>
     </Container>
   )
--- src/message/MessageModel.ts
+++ src/message/MessageModel.ts
@@ -1,16 +1,22 @@
 export type MessageType = "normal" | "ad" | "admin"
 
-export type MessageModelOptions = Pick<MessageModel, "sender" | "text" | "type">
+export type MessageModelOptions = Pick<MessageModel, "sender" | "text" | "type"> & {
+  timestamp?: number
+}
 
 export class MessageModel {
+  id = String(Math.random())
   sender?: string
   text: string
   type: MessageType
-  timestamp = new Date()
+  timestamp: number
+  localeTimeString: string
 
   constructor(options: MessageModelOptions) {
     this.sender = options.sender
     this.text = options.text
     this.type = options.type
+    this.timestamp = options.timestamp || Date.now()
+    this.localeTimeString = new Date(this.timestamp).toLocaleTimeString()
   }
 }
