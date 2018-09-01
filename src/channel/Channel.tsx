df0bb4a2b55fa87543fe60069ac167c18f035d3e
nuei <61118778+thatnuei@users.noreply.github.com>
2018-09-01 16:27:22 -0500

    add observer tag
    to account for messages list being replaced

--- src/channel/Channel.tsx
+++ src/channel/Channel.tsx
@@ -1,3 +1,4 @@
+import { observer } from "mobx-react"
 import React from "react"
 import { ConversationLayout } from "../conversation/ConversationLayout"
 import { ChannelModel } from "./ChannelModel"
@@ -6,6 +7,7 @@ export interface ChannelProps {
   model: ChannelModel
 }
 
+@observer
 export class Channel extends React.Component<ChannelProps> {
   render() {
     const { model } = this.props
