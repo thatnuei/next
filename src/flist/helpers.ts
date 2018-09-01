7f4e559b6e8db4d87f021333a9a8b24ef561d8af
nuei <61118778+thatnuei@users.noreply.github.com>
2018-08-31 20:41:09 -0500

    add helper for getting a character's profile url

--- src/flist/helpers.ts
+++ src/flist/helpers.ts
@@ -1,3 +1,7 @@
 export function getAvatarUrl(name: string) {
   return `https://static.f-list.net/images/avatar/${encodeURI(name.toLowerCase())}.png`
 }
+
+export function getProfileUrl(name: string) {
+  return `https://www.f-list.net/c/${encodeURI(name.toLowerCase())}`
+}
