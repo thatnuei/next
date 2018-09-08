dc16346a5c169cbe18a544106ad28b3339f21114
nuei <61118778+thatnuei@users.noreply.github.com>
2018-09-08 17:26:30 -0500

    flesh out private chat header

--- src/privateChat/PrivateConversation.tsx
+++ src/privateChat/PrivateConversation.tsx
@@ -1,6 +1,11 @@
 import { observer } from "mobx-react"
 import React from "react"
+import { Avatar } from "../character/Avatar"
+import { CharacterName } from "../character/CharacterName"
+import { CharacterStatus } from "../character/CharacterStatus"
+import { characterStore } from "../character/CharacterStore"
 import { ConversationLayout } from "../conversation/ConversationLayout"
+import { styled } from "../ui/styled"
 import { PrivateChatModel } from "./PrivateChatModel"
 
 export interface PrivateConversationProps {
@@ -11,13 +16,31 @@ export interface PrivateConversationProps {
 export class PrivateConversation extends React.Component<PrivateConversationProps> {
   render() {
     const { privateChat } = this.props
+    const character = characterStore.getCharacter(privateChat.partner)
 
     const headerContent = (
-      <div style={{ padding: "0.5rem" }}>
-        <h2>{privateChat.partner}</h2>
-      </div>
+      <HeaderContainer>
+        <div style={{ gridRow: "span 2", margin: "auto", marginRight: "0.5rem" }}>
+          <Avatar name={privateChat.partner} size={60} />
+        </div>
+        <div style={{ overflowY: "auto" }}>
+          <h2 style={{ marginBottom: "0.2rem" }}>
+            <CharacterName name={privateChat.partner} />
+          </h2>
+          <div style={{ fontSize: "80%", fontStyle: "italic" }}>
+            <CharacterStatus status={character.status} statusMessage={character.statusMessage} />
+          </div>
+        </div>
+      </HeaderContainer>
     )
 
     return <ConversationLayout headerContent={headerContent} messages={privateChat.messages} />
   }
 }
+
+const HeaderContainer = styled.div`
+  padding: 0.5rem;
+  display: flex;
+
+  max-height: 6rem;
+`
