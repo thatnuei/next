db8e3f40a717c4b1519db98401e1376ec5c0fb0b
nuei <61118778+thatnuei@users.noreply.github.com>
2018-09-07 21:28:16 -0500

    remove useless session component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,28 @@
 import React from "react"
-import { Session } from "../session/Session"
-import { SessionProvider } from "../session/SessionContext"
+import { Chat } from "../chat/Chat"
 import { SessionStore } from "../session/SessionStore"
+import { LoginModal } from "./LoginModal"
+import { SelectCharacterModal } from "./SelectCharacterModal"
 
 export interface AppProps {
   session: SessionStore
 }
 
 export class App extends React.Component<AppProps> {
+  get session() {
+    return this.props.session
+  }
+
   render() {
-    return (
-      <SessionProvider value={this.props.session}>
-        <Session state={this.props.session} />
-      </SessionProvider>
-    )
+    switch (this.session.screen) {
+      case "setup":
+        return <div>Setting things up...</div>
+      case "login":
+        return <LoginModal session={this.session} />
+      case "selectCharacter":
+        return <SelectCharacterModal session={this.session} />
+      case "chat":
+        return <Chat session={this.session} />
+    }
   }
 }
