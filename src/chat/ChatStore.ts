886cfa78b5b9cdda6f5987293635b451d68221fa
nuei <61118778+thatnuei@users.noreply.github.com>
2018-09-20 00:37:01 -0500

    handle global admin command

--- src/chat/ChatStore.ts
+++ src/chat/ChatStore.ts
@@ -6,12 +6,24 @@ export class ChatStore {
   @observable
   serverVariables = new Map<string, number | string | ReadonlyArray<string>>()
 
+  @observable
+  admins = new Map<string, true>()
+
   constructor(socketEvents: SocketEventBus) {
     socketEvents.listen("VAR", this.handleServerVariables)
+    socketEvents.listen("ADL", this.handleAdminList)
   }
 
   @action.bound
-  handleServerVariables({ variable, value }: ServerCommands["VAR"]) {
+  private handleServerVariables({ variable, value }: ServerCommands["VAR"]) {
     this.serverVariables.set(variable, value)
+  }
+
+  @action.bound
+  private handleAdminList(params: ServerCommands["ADL"]) {
+    this.admins.clear()
+    for (const name of params.ops) {
+      this.admins.set(name, true)
+    }
   }
 }
