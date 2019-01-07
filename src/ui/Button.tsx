9561e49796f631ca4a2370d569af907b39612335
nuei <61118778+thatnuei@users.noreply.github.com>
2019-01-06 21:59:00 -0600

    update flat button styling

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -6,9 +6,12 @@ type ButtonProps = {
 
 const flatStyle = css`
   background: none;
+  opacity: 0.5;
 
-  :hover {
+  :hover,
+  :focus {
     background: none;
+    opacity: 1;
   }
 `
 
@@ -18,10 +21,12 @@ export const Button = styled.button<ButtonProps>`
   border: none;
   background: rgba(0, 0, 0, 0.5);
   padding: 0.5rem 0.7rem;
-  transition: 0.2s background-color;
+  transition: 0.2s;
+  transition-property: background-color, opacity;
   cursor: pointer;
 
-  :hover {
+  :hover,
+  :focus {
     background: rgba(0, 0, 0, 0.8);
   }
 
