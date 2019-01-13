ef76b0128f10aa6eaf884d4e5534e5998cbba387
nuei <61118778+thatnuei@users.noreply.github.com>
2019-01-12 21:26:56 -0600

    render state containers in index
    so that the app will hot-reload with previous state

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import AppStore from "./app/AppStore"
 import Root from "./Root"
 
 function renderApp() {
-  ReactDOM.render(<Root />, document.querySelector("#root"))
+  ReactDOM.render(
+    <AppStore.Provider>
+      <Root />
+    </AppStore.Provider>,
+    document.querySelector("#root"),
+  )
 }
 
 function main() {
