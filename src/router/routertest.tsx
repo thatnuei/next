3fc8d51ec8c6421bc82d8289c70cf651b8e49e00
nuei <61118778+thatnuei@users.noreply.github.com>
2019-01-20 15:52:50 -0600

    move router test to router folder

--- src/router/routertest.tsx
+++ src/router/routertest.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, Redirect, Route, Router, Switch } from "./router"
+import { Link, Redirect, Route, Router, Switch } from "."
 
 const Root = () => (
   <Router>
