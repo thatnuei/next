30afa0c7bcf49d03380378d781f9370cac67563b
nuei <61118778+thatnuei@users.noreply.github.com>
2019-01-27 22:09:53 -0600

    split chat state into a hook

--- src/chat/ChatRoute.tsx
+++ src/chat/ChatRoute.tsx
@@ -1,14 +1,8 @@
-import React, { useEffect } from "react"
+import React from "react"
 import routePaths from "../app/routePaths"
 import ChannelHeader from "../channel/ChannelHeader"
 import ChannelRoute from "../channel/ChannelRoute"
-import useChannelStore from "../channel/useChannelStore"
-import useCharacterStore from "../character/useCharacterStore"
-import { OptionalArg } from "../common/types"
-import { chatServerUrl } from "../fchat/constants"
-import createCommandHandler from "../fchat/createCommandHandler"
-import { ClientCommandMap, ServerCommand } from "../fchat/types"
-import { Route, Switch, useRouter } from "../router"
+import { Route, Switch } from "../router"
 import AppDocumentTitle from "../ui/AppDocumentTitle"
 import Button from "../ui/Button"
 import { themeColor } from "../ui/colors"
@@ -18,6 +12,7 @@ import SideOverlay from "../ui/SideOverlay"
 import { css } from "../ui/styled"
 import useOverlayState from "../ui/useOverlayState"
 import ChatSidebarContent from "./ChatSidebarContent"
+import useChatState from "./useChatState"
 
 type Props = {
   account: string
@@ -26,70 +21,7 @@ type Props = {
 }
 
 function ChatRoute(props: Props) {
-  const { history } = useRouter()
-  const characterStore = useCharacterStore()
-  const channelStore = useChannelStore(props.identity)
-
-  useEffect(() => {
-    const socket = new WebSocket(chatServerUrl)
-
-    const handleCommand = createCommandHandler({
-      IDN() {
-        sendCommand(socket, "JCH", { channel: "Frontpage" })
-        sendCommand(socket, "JCH", { channel: "Fantasy" })
-        sendCommand(socket, "JCH", { channel: "Story Driven LFRP" })
-        sendCommand(socket, "JCH", { channel: "Development" })
-      },
-
-      PIN() {
-        sendCommand(socket, "PIN")
-      },
-
-      HLO({ message }) {
-        console.info(message)
-      },
-
-      CON({ count }) {
-        console.info(`There are ${count} characters in chat`)
-      },
-    })
-
-    socket.onopen = () => {
-      sendCommand(socket, "IDN", {
-        account: props.account,
-        ticket: props.ticket,
-        character: props.identity,
-        cname: "next",
-        cversion: "0.1.0",
-        method: "ticket",
-      })
-    }
-
-    socket.onclose = () => {
-      console.log("socket closed")
-      history.push(routePaths.login)
-    }
-
-    socket.onmessage = ({ data }) => {
-      const type = data.slice(0, 3)
-      const params = data.length > 3 ? JSON.parse(data.slice(4)) : {}
-      const command: ServerCommand = { type, params }
-
-      if (handleCommand(command)) return
-      if (characterStore.handleCommand(command)) return
-      if (channelStore.handleCommand(command)) return
-
-      console.log("unhandled command", command)
-    }
-
-    return () => {
-      socket.onopen = null
-      socket.onclose = null
-      socket.onmessage = null
-      socket.close()
-    }
-  }, [])
-
+  const { channelStore, characterStore } = useChatState(props)
   const sidebar = useOverlayState()
 
   return (
@@ -142,15 +74,3 @@ const headerStyle = css`
   background-color: ${themeColor};
   min-height: 50px;
 `
-
-function sendCommand<K extends keyof ClientCommandMap>(
-  socket: WebSocket,
-  command: K,
-  ...params: OptionalArg<ClientCommandMap[K]>
-) {
-  if (params[0]) {
-    socket.send(`${command} ${JSON.stringify(params[0])}`)
-  } else {
-    socket.send(command)
-  }
-}
---
+++ src/chat/useChatState.ts
@@ -0,0 +1,93 @@
+import { useEffect } from "react"
+import routePaths from "../app/routePaths"
+import useChannelStore from "../channel/useChannelStore"
+import useCharacterStore from "../character/useCharacterStore"
+import { OptionalArg } from "../common/types"
+import { chatServerUrl } from "../fchat/constants"
+import createCommandHandler from "../fchat/createCommandHandler"
+import { ClientCommandMap, ServerCommand } from "../fchat/types"
+import { useRouter } from "../router"
+
+type UserCredentials = {
+  account: string
+  ticket: string
+  identity: string
+}
+
+function useChatState(user: UserCredentials) {
+  const { history } = useRouter()
+  const characterStore = useCharacterStore()
+  const channelStore = useChannelStore(user.identity)
+
+  useEffect(() => {
+    const socket = new WebSocket(chatServerUrl)
+
+    const handleCommand = createCommandHandler({
+      IDN() {
+        sendCommand(socket, "JCH", { channel: "Frontpage" })
+        sendCommand(socket, "JCH", { channel: "Fantasy" })
+        sendCommand(socket, "JCH", { channel: "Story Driven LFRP" })
+        sendCommand(socket, "JCH", { channel: "Development" })
+      },
+      PIN() {
+        sendCommand(socket, "PIN")
+      },
+      HLO({ message }) {
+        console.info(message)
+      },
+      CON({ count }) {
+        console.info(`There are ${count} characters in chat`)
+      },
+    })
+
+    socket.onopen = () => {
+      sendCommand(socket, "IDN", {
+        account: user.account,
+        ticket: user.ticket,
+        character: user.identity,
+        cname: "next",
+        cversion: "0.1.0",
+        method: "ticket",
+      })
+    }
+
+    socket.onclose = () => {
+      console.log("socket closed")
+      history.push(routePaths.login)
+    }
+
+    socket.onmessage = ({ data }) => {
+      const type = data.slice(0, 3)
+      const params = data.length > 3 ? JSON.parse(data.slice(4)) : {}
+      const command: ServerCommand = { type, params }
+
+      if (handleCommand(command)) return
+      if (characterStore.handleCommand(command)) return
+      if (channelStore.handleCommand(command)) return
+
+      console.log("unhandled command", command)
+    }
+
+    return () => {
+      socket.onopen = null
+      socket.onclose = null
+      socket.onmessage = null
+      socket.close()
+    }
+  }, [])
+
+  return { channelStore, characterStore }
+}
+export default useChatState
+
+function sendCommand<K extends keyof ClientCommandMap>(
+  socket: WebSocket,
+  command: K,
+  ...params: OptionalArg<ClientCommandMap[K]>
+) {
+  if (params[0]) {
+    socket.send(`${command} ${JSON.stringify(params[0])}`)
+  } else {
+    socket.send(command)
+  }
+}
