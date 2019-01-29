ff3a2bb2d4b61d44bdc8f56b22a5a3f9138fefdc
nuei <61118778+thatnuei@users.noreply.github.com>
2019-01-29 07:06:04 -0600

    filter button styling

--- src/channel/ChannelHeader.tsx
+++ src/channel/ChannelHeader.tsx
@@ -1,14 +1,25 @@
 import React from "react"
 import Button from "../ui/Button"
-import { flexGrow } from "../ui/helpers"
+import { flexGrow, flexRow, flexWrap } from "../ui/helpers"
 import Icon from "../ui/Icon"
-import { css } from "../ui/styled"
+import { css, styled } from "../ui/styled"
 import { Channel } from "./types"
 
-export default function ChannelHeader({ channel }: { channel: Channel }) {
+type Props = {
+  channel: Channel
+}
+
+export default function ChannelHeader({ channel }: Props) {
   return (
     <>
-      <h2 css={[flexGrow, channelNameStyle]}>{channel.name}</h2>
+      <div css={nameAndFilterStyle}>
+        <h2 css={channelNameStyle}>{channel.name}</h2>
+        <div css={filterGroupStyle}>
+          <FilterButton flat>Both</FilterButton>
+          <FilterButton flat>Chat</FilterButton>
+          <FilterButton flat>Ads</FilterButton>
+        </div>
+      </div>
       <Button flat>
         <Icon icon="users" />
       </Button>
@@ -16,8 +27,25 @@ export default function ChannelHeader({ channel }: { channel: Channel }) {
   )
 }
 
+const nameAndFilterStyle = css`
+  ${flexGrow};
+  ${flexRow};
+  ${flexWrap};
+  align-items: center;
+  padding: 0.5rem;
+`
+
 const channelNameStyle = css`
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
+  flex: 1 1 auto;
+  padding: 0.1rem 0;
+  margin-right: 0.5rem;
+`
+
+const filterGroupStyle = css`
+  padding: 0.1rem 0;
+  margin: -0.5rem;
+`
+
+const FilterButton = styled(Button)`
+  padding: 0.5rem;
 `
