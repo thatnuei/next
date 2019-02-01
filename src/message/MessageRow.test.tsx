381f8401e438a163373dd3ccf0648b504985fa4e
nuei <61118778+thatnuei@users.noreply.github.com>
2019-01-31 21:41:32 -0600

    add MessageRow test

---
+++ src/message/MessageRow.test.tsx
@@ -0,0 +1,55 @@
+import React from "react"
+import { render } from "react-testing-library"
+import { createCharacter } from "../character/useCharacterStore"
+import ChatStore from "../chat/ChatStore"
+import FactoryMap from "../state/FactoryMap"
+import MessageRow from "./MessageRow"
+import { Message } from "./types"
+
+describe("MessageRow", () => {
+  const message: Message = {
+    id: "fsdfdsfsafdaf",
+    text: "hello",
+    type: "chat",
+    time: 1548991576255,
+    senderName: "Testificate",
+  }
+
+  const chatStoreMock = {
+    characterStore: { characters: new FactoryMap(createCharacter) },
+  }
+
+  const setup = (extra: Partial<Message> = {}) =>
+    render(
+      <ChatStore.Context.Provider value={chatStoreMock as any}>
+        <MessageRow {...message} {...extra} />
+      </ChatStore.Context.Provider>,
+    )
+
+  it("shows the sender name", () => {
+    const { queryByText } = setup()
+    expect(queryByText(message.senderName!)).not.toBeNull()
+  })
+
+  it("shows the message", () => {
+    const { queryByText } = setup()
+    expect(queryByText(message.text)).not.toBeNull()
+  })
+
+  it("shows the timestamp", () => {
+    const { queryByText } = setup()
+    // show the time in any capacity, must show seconds
+    expect(queryByText(/\d:\d\d:\d\d/)).not.toBeNull()
+  })
+
+  it("removes /me from action messages", () => {
+    const { getByText } = setup({ text: "/me does a thing" })
+    const el = getByText("does a thing")
+    expect(el.textContent).not.toContain("/me")
+  })
+
+  it(`shows "System" if no sender`, () => {
+    const { queryByText } = setup({ senderName: undefined })
+    expect(queryByText("System")).not.toBeNull()
+  })
+})
