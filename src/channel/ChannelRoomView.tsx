71c6c0472bb115686049c817d267654f82fc49a3
nuei <61118778+thatnuei@users.noreply.github.com>
2019-02-09 15:18:41 -0600

    move layout responsibility to the room views

--- src/channel/ChannelRoomView.tsx
+++ src/channel/ChannelRoomView.tsx
@@ -1,22 +1,29 @@
 import { observe } from "mobx"
 import { observer } from "mobx-react-lite"
 import React, { useEffect, useRef } from "react"
+import ChatSidebarContent from "../chat/ChatSidebarContent"
 import MessageRow from "../message/MessageRow"
 import { useRootStore } from "../RootStore"
 import AppDocumentTitle from "../ui/AppDocumentTitle"
 import Button from "../ui/Button"
 import { themeColor } from "../ui/colors"
 import { fillArea, flexColumn, flexGrow, scrollVertical } from "../ui/helpers"
+import Icon from "../ui/Icon"
+import SideOverlay from "../ui/SideOverlay"
 import { css } from "../ui/styled"
 import TextArea from "../ui/TextArea"
+import useOverlayState from "../ui/useOverlayState"
+import ChannelHeader from "./ChannelHeader"
 import ChannelModel from "./ChannelModel"
 
 type Props = {
   channel: ChannelModel
 }
 
-function ChannelRoute({ channel }: Props) {
+function ChannelRoomView({ channel }: Props) {
   const { chatStore } = useRootStore()
+
+  const sidebar = useOverlayState()
 
   const messageListRef = useRef<HTMLElement>(null)
 
@@ -24,24 +31,37 @@ function ChannelRoute({ channel }: Props) {
 
   return (
     <AppDocumentTitle title={`${chatStore.identity} - ${channel.name}`}>
-      <section css={[fillArea, flexColumn]}>
-        <section css={[flexGrow, scrollVertical]} ref={messageListRef}>
-          {channel.filteredMessages.slice(-300).map((message) => (
-            <MessageRow key={message.id} {...message} />
-          ))}
+      <header css={headerStyle}>
+        <Button flat onClick={sidebar.open}>
+          <Icon icon="menu" />
+        </Button>
+        <ChannelHeader channel={channel} />
+      </header>
+
+      <main css={flexGrow}>
+        <section css={[fillArea, flexColumn]}>
+          <section css={[flexGrow, scrollVertical]} ref={messageListRef}>
+            {channel.filteredMessages.slice(-300).map((message) => (
+              <MessageRow key={message.id} {...message} />
+            ))}
+          </section>
+          <form
+            css={inputContainerStyle}
+            onSubmit={(event) => event.preventDefault()}
+          >
+            <TextArea />
+            <Button>Send</Button>
+          </form>
         </section>
-        <form
-          css={inputContainerStyle}
-          onSubmit={(event) => event.preventDefault()}
-        >
-          <TextArea />
-          <Button>Send</Button>
-        </form>
-      </section>
+      </main>
+
+      <SideOverlay {...sidebar.bind}>
+        <ChatSidebarContent />
+      </SideOverlay>
     </AppDocumentTitle>
   )
 }
-export default observer(ChannelRoute)
+export default observer(ChannelRoomView)
 
 const inputContainerStyle = css`
   display: grid;
@@ -49,6 +69,13 @@ const inputContainerStyle = css`
   grid-gap: 0.5rem;
   padding: 0.5rem;
   background-color: ${themeColor};
+`
+
+const headerStyle = css`
+  display: flex;
+  align-items: center;
+  background-color: ${themeColor};
+  min-height: 50px;
 `
 
 function useBottomScroll(
--- src/chat/ChatScreen.tsx
+++ src/chat/ChatScreen.tsx
@@ -1,61 +1,22 @@
 import { useObserver } from "mobx-react-lite"
 import React from "react"
-import ChannelHeader from "../channel/ChannelHeader"
 import ChannelRoomView from "../channel/ChannelRoomView"
 import { useRootStore } from "../RootStore"
 import AppDocumentTitle from "../ui/AppDocumentTitle"
-import Button from "../ui/Button"
-import { themeColor } from "../ui/colors"
-import { flexColumn, flexGrow, fullscreen } from "../ui/helpers"
-import Icon from "../ui/Icon"
-import SideOverlay from "../ui/SideOverlay"
-import { css } from "../ui/styled"
-import useOverlayState from "../ui/useOverlayState"
-import ChatSidebarContent from "./ChatSidebarContent"
+import { flexColumn, fullscreen } from "../ui/helpers"
 
 function ChatScreen() {
   const { chatStore } = useRootStore()
-  const sidebar = useOverlayState()
 
   return (
     <AppDocumentTitle title={chatStore.identity}>
       <div css={[fullscreen, flexColumn]}>
-        <header css={headerStyle}>
-          <Button flat onClick={sidebar.open}>
-            <Icon icon="menu" />
-          </Button>
-          <ChatHeader />
-        </header>
-
-        <main css={flexGrow}>
-          <ChatRoomView />
-        </main>
+        <ChatRoomView />
       </div>
-
-      <SideOverlay {...sidebar.bind}>
-        <ChatSidebarContent />
-      </SideOverlay>
     </AppDocumentTitle>
   )
 }
 export default ChatScreen
-
-function ChatHeader() {
-  const { viewStore } = useRootStore()
-
-  return useObserver(() => {
-    switch (viewStore.screen) {
-      case "console":
-        return <p>todo: console</p>
-      case "channel":
-        return <ChannelHeader channel={viewStore.currentChannel} />
-      case "privateChat":
-        return <p>todo: private chat</p>
-    }
-
-    return <p>view not found</p>
-  })
-}
 
 function ChatRoomView() {
   const { viewStore } = useRootStore()
@@ -73,10 +34,3 @@ function ChatRoomView() {
     return <p>view not found</p>
   })
 }
-
-const headerStyle = css`
-  display: flex;
-  align-items: center;
-  background-color: ${themeColor};
-  min-height: 50px;
-`
