1c16e3bdec9965bed53fd9cf44fb4e2a36941663
nuei <61118778+thatnuei@users.noreply.github.com>
2019-02-17 00:42:55 -0600

    remove unnecessary extra component

--- src/chat/ChatScreen.tsx
+++ src/chat/ChatScreen.tsx
@@ -1,32 +1,25 @@
-import { observer, useObserver } from "mobx-react-lite"
+import { observer } from "mobx-react-lite"
 import React from "react"
 import ChannelRoomView from "../channel/ChannelRoomView"
 import { useRootStore } from "../RootStore"
 import AppDocumentTitle from "../ui/AppDocumentTitle"
 
-function ChatScreen() {
-  const { chatStore } = useRootStore()
+const ChatScreen = () => {
+  const {
+    chatStore: { identity },
+    viewStore: { screen },
+    channelStore: { channels },
+  } = useRootStore()
 
-  return (
-    <AppDocumentTitle title={chatStore.identity}>
-      <ChatRoomView />
-    </AppDocumentTitle>
-  )
-}
-export default observer(ChatScreen)
-
-function ChatRoomView() {
-  const { viewStore, channelStore } = useRootStore()
-
-  return useObserver(() => {
-    const { screen } = viewStore
-    const { channels } = channelStore
-
+  const renderRoom = () => {
     switch (screen.name) {
       case "channel":
         return <ChannelRoomView channel={channels.get(screen.channel)} />
+      default:
+        return <p>view not found</p>
     }
+  }
 
-    return <p>view not found</p>
-  })
+  return <AppDocumentTitle title={identity}>{renderRoom()}</AppDocumentTitle>
 }
+export default observer(ChatScreen)
