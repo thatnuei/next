394586c2d0889d97e4becf985ca5eebbd5f70070
nuei <61118778+thatnuei@users.noreply.github.com>
2019-02-20 23:38:07 -0600

    inject dependencies from root store

--- src/RootStore.ts
+++ src/RootStore.ts
@@ -1,18 +1,22 @@
+import * as idb from "idb-keyval"
 import React, { useContext } from "react"
 import ViewStore from "./app/ViewStore"
 import ChannelStore from "./channel/ChannelStore"
 import CharacterStore from "./character/CharacterStore"
 import ChatStore from "./chat/ChatStore"
 import SocketStore from "./fchat/SocketStore"
+import FListApiService from "./flist/FListApiService"
 import UserStore from "./user/UserStore"
 
 export default class RootStore {
   socketStore = new SocketStore(this)
   viewStore = new ViewStore()
-  userStore = new UserStore()
+  userStore = new UserStore(this.api, this.storage)
   characterStore = new CharacterStore()
   channelStore = new ChannelStore(this)
   chatStore = new ChatStore(this)
+
+  constructor(private api = new FListApiService(), private storage = idb) {}
 
   async init() {
     try {
