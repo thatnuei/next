3956d0274686cbefc7dbee09019c7570306281e3
nuei <61118778+thatnuei@users.noreply.github.com>
2019-02-20 20:36:28 -0600

    remove unused helper

--- src/common/fromEntries.ts
+++
@@ -1,8 +0,0 @@
-function fromEntries<K extends keyof any, V>(pairs: [K, V][]): Record<K, V> {
-  const result: any = {}
-  for (const [key, value] of pairs) {
-    result[key] = value
-  }
-  return result
-}
-export default fromEntries
