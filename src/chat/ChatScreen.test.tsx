410c2ec86a34e1c131bab2f9ec059a959d3bdea1
nuei <61118778+thatnuei@users.noreply.github.com>
2019-02-24 00:35:18 -0600

    attempt bugfix test v2

---
+++ src/chat/ChatScreen.test.tsx
@@ -0,0 +1,71 @@
+import React from "react"
+import { act, render } from "react-testing-library"
+import RootStore, { RootStoreContext } from "../RootStore"
+import ChatScreen from "./ChatScreen"
+
+test("bug - continues to show new messages after switching channels", () => {
+  const store = new RootStore()
+
+  const { queryByText } = render(
+    <RootStoreContext.Provider value={store}>
+      <ChatScreen />
+    </RootStoreContext.Provider>,
+  )
+
+  act(() => {
+    store.chatStore.setIdentity("Testificate")
+
+    store.viewStore.setScreen({
+      name: "channel",
+      channel: "Frontpage",
+    })
+
+    store.channelStore.handleSocketCommand({
+      type: "JCH",
+      params: {
+        channel: "Frontpage",
+        title: "Frontpage",
+        character: { identity: "Testificate" },
+      },
+    })
+
+    store.channelStore.handleSocketCommand({
+      type: "JCH",
+      params: {
+        channel: "Fantasy",
+        title: "Fantasy",
+        character: { identity: "Testificate" },
+      },
+    })
+
+    store.channelStore.handleSocketCommand({
+      type: "MSG",
+      params: {
+        channel: "Frontpage",
+        character: "Testificate",
+        message: "hello frontpage",
+      },
+    })
+  })
+
+  expect(queryByText("hello frontpage")).not.toBeNull()
+
+  act(() => {
+    store.viewStore.setScreen({
+      name: "channel",
+      channel: "Fantasy",
+    })
+
+    store.channelStore.handleSocketCommand({
+      type: "MSG",
+      params: {
+        channel: "Fantasy",
+        character: "Testificate",
+        message: "hello fantasy",
+      },
+    })
+  })
+
+  expect(queryByText("hello frontpage")).toBeNull()
+  expect(queryByText("hello fantasy")).not.toBeNull()
+})
