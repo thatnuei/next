5d1ddb69c6e9673b2687cb2e4f9f8980ba7ee364
nuei <61118778+thatnuei@users.noreply.github.com>
2019-03-23 18:18:32 -0500

    nicer avatar

--- src/character/Avatar.tsx
+++ src/character/Avatar.tsx
@@ -1,17 +1,35 @@
-import React from "react"
+import React, { useCallback, useMemo, useState } from "react"
 import { getAvatarUrl } from "../flist/helpers"
+import { semiBlack } from "../ui/colors"
+import { styled } from "../ui/styled"
 
 type Props = {
   name: string
   size?: number
 }
 
-const Avatar = (props: Props) => (
-  <img
-    src={getAvatarUrl(props.name)}
-    alt={`Avatar for ${props.name}`}
-    width={props.size || 100}
-    height={props.size || 100}
-  />
-)
+const Avatar = (props: Props) => {
+  const [loaded, setLoaded] = useState(false)
+
+  const style = useMemo(() => ({ opacity: loaded ? 1 : 0 }), [loaded])
+
+  const handleLoad = useCallback(() => setLoaded(true), [])
+
+  return (
+    <Image
+      src={getAvatarUrl(props.name)}
+      width={props.size || 100}
+      height={props.size || 100}
+      style={style}
+      onLoad={handleLoad}
+      alt=""
+      role="presentation"
+    />
+  )
+}
 export default Avatar
+
+const Image = styled.img`
+  filter: drop-shadow(0 2px 4px ${semiBlack(0.3)});
+  transition: 0.2s opacity;
+`
