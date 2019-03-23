6feb1fe598e73c81dc6c3fda4539d3359fd8486b
nuei <61118778+thatnuei@users.noreply.github.com>
2019-03-23 17:49:05 -0500

    extract shared input styles

--- src/ui/TextArea.tsx
+++ src/ui/TextArea.tsx
@@ -1,23 +1,7 @@
-import React from "react"
-import { css } from "./styled"
+import inputBase from "./inputBase"
+import { styled } from "./styled"
 
-const TextArea = (props: React.ComponentPropsWithoutRef<"textarea">) => (
-  <textarea
-    css={css`
-      font: inherit;
-      color: inherit;
-      border: none;
-      background: rgba(0, 0, 0, 0.5);
-      padding: 0.5rem 0.7rem;
-      transition: 0.2s background-color;
-      display: block;
-      resize: none;
-
-      :focus {
-        background: rgba(0, 0, 0, 0.8);
-      }
-    `}
-    {...props}
-  />
-)
+const TextArea = styled.textarea`
+  ${inputBase};
+`
 export default TextArea
---
+++ src/ui/inputBase.ts
@@ -0,0 +1,25 @@
+import { semiBlack } from "./colors"
+import { css } from "./styled"
+
+const inputBase = css`
+  font: inherit;
+  color: inherit;
+  border: none;
+  background: rgba(0, 0, 0, 0.4);
+  padding: 0.5rem 0.7rem;
+  transition: 0.2s background-color;
+  box-shadow: 0px 0px 3px ${semiBlack(0.3)} inset;
+
+  display: block;
+  width: 100%;
+
+  :hover {
+    background: rgba(0, 0, 0, 0.55);
+  }
+
+  :focus {
+    background: rgba(0, 0, 0, 0.7);
+    outline: none;
+  }
+`
+export default inputBase
