a718b390657346a2af525f06d888d4ccef85e7ff
nuei <61118778+thatnuei@users.noreply.github.com>
2019-03-24 18:40:30 -0500

    extract usePopup hook

--- src/character/CharacterMenuContext.tsx
+++ src/character/CharacterMenuContext.tsx
@@ -1,55 +1,12 @@
-import React, { useContext, useLayoutEffect, useRef, useState } from "react"
+import React, { useContext, useRef, useState } from "react"
 import { css, styled } from "../ui/styled"
-import useWindowEvent from "../ui/useWindowEvent"
+import usePopup from "../ui/usePopup"
 import CharacterMenu from "./CharacterMenu"
 
 const CharacterMenuContext = React.createContext({
   open: (characterName: string, event: React.MouseEvent) => {},
   close: () => {},
 })
-
-function getClientRect(element: Element) {
-  return new Promise<ClientRect | DOMRect>((resolve, reject) => {
-    const observer = new IntersectionObserver(([entry]) => {
-      resolve(entry.boundingClientRect)
-      observer.disconnect()
-    })
-    observer.observe(element)
-  })
-}
-
-function usePopup(ref: React.RefObject<HTMLElement>) {
-  const [isVisible, setIsVisible] = useState(false)
-  const [position, setPosition] = useState({ x: 0, y: 0 })
-  const [style, setStyle] = useState<React.CSSProperties>({})
-
-  useWindowEvent("click", (event) => {
-    if (event.target === ref.current) return
-    setIsVisible(false)
-  })
-
-  useLayoutEffect(() => {
-    const menu = ref.current
-    if (!menu) return
-
-    setStyle({
-      position: "fixed",
-      left: Math.min(position.x, window.innerWidth - menu.clientWidth),
-      top: Math.min(position.y, window.innerHeight - menu.clientHeight),
-    })
-  }, [position.x, position.y, ref])
-
-  const openAt = (position: { x: number; y: number }) => {
-    setPosition(position)
-    setIsVisible(true)
-  }
-
-  const close = () => {
-    setIsVisible(false)
-  }
-
-  return { openAt, close, isVisible, style }
-}
 
 export const CharacterMenuProvider = ({
   children,
