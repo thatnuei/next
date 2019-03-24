225aa72f64966a438bf37420c7c58f2ebea7bf33
nuei <61118778+thatnuei@users.noreply.github.com>
2019-03-24 16:34:16 -0500

    more icons

--- src/ui/icons.ts
+++ src/ui/icons.ts
@@ -1,4 +1,17 @@
-import { mdiAccountMultiple, mdiMenu } from "@mdi/js"
+import {
+  mdiAccount,
+  mdiAccountMultiple,
+  mdiAlertCircle,
+  mdiBookmark,
+  mdiComment,
+  mdiMenu,
+  mdiMinusCircle,
+} from "@mdi/js"
 
 export const menu = mdiMenu
 export const users = mdiAccountMultiple
+export const user = mdiAccount
+export const message = mdiComment
+export const bookmark = mdiBookmark
+export const ignore = mdiMinusCircle
+export const report = mdiAlertCircle
