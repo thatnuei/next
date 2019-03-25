b33f7354abc488996e45b44c899012512efaa82d
nuei <61118778+thatnuei@users.noreply.github.com>
2019-03-24 19:32:20 -0500

    useWindowEvent: accept options

--- src/ui/useWindowEvent.ts
+++ src/ui/useWindowEvent.ts
@@ -1,11 +1,13 @@
 import { useEffect } from "react"
 
-export default function useWindowEvent<E extends keyof WindowEventMap>(
+function useWindowEvent<E extends keyof WindowEventMap>(
   event: E,
   handler: (event: WindowEventMap[E]) => void,
+  options?: boolean | AddEventListenerOptions,
 ) {
   useEffect(() => {
-    window.addEventListener(event, handler)
+    window.addEventListener(event, handler, options)
     return () => window.removeEventListener(event, handler)
-  }, [event, handler])
+  }, [event, handler, options])
 }
+export default useWindowEvent
