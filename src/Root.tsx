d07e721dbf60f23c3940d52a408e890ad1b2834c
nuei <61118778+thatnuei@users.noreply.github.com>
2019-04-14 18:09:49 -0500

    move design into app

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,12 +1,17 @@
+import { Grommet } from "grommet"
 import React from "react"
-import Design from "./Design"
+import App from "./app/App"
+import AppDocumentTitle from "./ui/AppDocumentTitle"
+import GlobalStyle from "./ui/globalStyle"
+import { darkTheme } from "./ui/theme"
 
-// const Root = () => (
-//   <AppDocumentTitle>
-//     <App />
-//     <GlobalStyle />
-//   </AppDocumentTitle>
-// )
-const Root = () => <Design />
+const Root = () => (
+  <AppDocumentTitle>
+    <Grommet theme={darkTheme}>
+      <App />
+      <GlobalStyle />
+    </Grommet>
+  </AppDocumentTitle>
+)
 
 export default Root
