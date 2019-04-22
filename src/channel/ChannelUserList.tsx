e0fa6675e2c83ad86d25866a5edcc9e0f3ab886e
nuei <61118778+thatnuei@users.noreply.github.com>
2019-04-22 01:17:08 -0500

    user list highlights

--- src/channel/ChannelUserList.tsx
+++ src/channel/ChannelUserList.tsx
@@ -1,22 +1,37 @@
 import { useRect } from "@reach/rect"
 import { Box } from "grommet"
 import { observer } from "mobx-react-lite"
-import React, { useRef } from "react"
+import { rgba } from "polished"
+import React, { CSSProperties, useRef } from "react"
 import { FixedSizeList, ListChildComponentProps } from "react-window"
 import CharacterName from "../character/CharacterName"
+import { useRootStore } from "../RootStore"
 import { ThemeColor } from "../ui/theme"
 import ChannelModel from "./ChannelModel"
 
 function ChannelUserList({ channel }: { channel: ChannelModel }) {
+  const { chatStore } = useRootStore()
   const { sortedUsers } = channel
 
   const listRef = useRef(null)
   const rect: ClientRect | null = useRect(listRef)
 
+  const getHighlight = (name: string) => {
+    if (chatStore.isFriend(name)) return rgba(46, 204, 113, 0.15)
+    if (chatStore.isAdmin(name)) return rgba(231, 76, 60, 0.15)
+    if (channel.ops.has(name)) return rgba(241, 196, 15, 0.15)
+  }
+
   const renderUser = ({ index, style }: ListChildComponentProps) => {
     const character = sortedUsers[index]
+
+    const fullStyle: CSSProperties = {
+      ...style,
+      backgroundColor: getHighlight(character.name),
+    }
+
     return (
-      <Box pad="xsmall" justify="center" style={style}>
+      <Box pad="xsmall" justify="center" style={fullStyle}>
         <CharacterName key={character.name} {...character} />
       </Box>
     )
