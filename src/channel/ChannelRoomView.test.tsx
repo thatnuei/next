52af179759dcb4395f2033ccda1e72b2f48dea25
nuei <61118778+thatnuei@users.noreply.github.com>
2019-04-22 01:20:46 -0500

    fix test

--- src/channel/ChannelRoomView.test.tsx
+++ src/channel/ChannelRoomView.test.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { act, render } from "react-testing-library"
-import CharacterStore from "../character/CharacterStore"
 import MessageModel from "../message/MessageModel"
+import RootStore from "../RootStore"
 import ChannelModel from "./ChannelModel"
 import ChannelRoomView from "./ChannelRoomView"
 
 test("shows new messages", () => {
-  const channel = new ChannelModel(new CharacterStore(), "Frontpage")
+  const channel = new ChannelModel(new RootStore(), "Frontpage")
 
   const { queryByText } = render(<ChannelRoomView channel={channel} />)
 
