455d343fd8090d990dc67b4c7e44971c6de064c8
nuei <61118778+thatnuei@users.noreply.github.com>
2019-04-29 18:48:44 -0500

    Avatar: forward image props and ref

--- src/character/Avatar.tsx
+++ src/character/Avatar.tsx
@@ -1,14 +1,22 @@
-import React, { useCallback, useMemo, useState } from "react"
+import React, {
+  ComponentPropsWithoutRef,
+  useCallback,
+  useMemo,
+  useState,
+} from "react"
 import { getAvatarUrl } from "../flist/helpers"
 import { semiBlack } from "../ui/colors"
 import { styled } from "../ui/styled"
 
-type Props = {
+type Props = ComponentPropsWithoutRef<"img"> & {
   name: string
   size?: number
 }
 
-const Avatar = (props: Props) => {
+const Avatar = (
+  { name, size, ...props }: Props,
+  ref: React.Ref<HTMLImageElement>,
+) => {
   const [loaded, setLoaded] = useState(false)
 
   const style = useMemo(() => ({ opacity: loaded ? 1 : 0 }), [loaded])
@@ -17,18 +25,20 @@ const Avatar = (props: Props) => {
 
   return (
     <Image
-      src={getAvatarUrl(props.name)}
-      width={props.size || 100}
-      height={props.size || 100}
+      src={getAvatarUrl(name)}
+      width={size || 100}
+      height={size || 100}
       style={style}
-      title={props.name}
+      title={name}
       onLoad={handleLoad}
       alt=""
       role="presentation"
+      ref={ref}
+      {...props}
     />
   )
 }
-export default Avatar
+export default React.forwardRef(Avatar)
 
 const Image = styled.img`
   filter: drop-shadow(0 2px 4px ${semiBlack(0.3)});
