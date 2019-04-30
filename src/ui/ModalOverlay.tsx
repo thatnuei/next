83a4c3ab1e5552c8134bd8ff8bc6c0dc7168044f
nuei <61118778+thatnuei@users.noreply.github.com>
2019-04-29 19:22:21 -0500

    spruce up ModalOverlay
    maybe share some stuff between it and the description overlay later

--- src/ui/ModalOverlay.tsx
+++ src/ui/ModalOverlay.tsx
@@ -1,23 +1,54 @@
 import React from "react"
+import { onlyOnSelf } from "../common/eventHelpers"
+import Box from "./Box"
 import { semiBlack } from "./colors"
+import FadedButton from "./FadedButton"
 import { fullscreen } from "./helpers"
-import { css } from "./styled"
+import Icon from "./Icon"
+import { styled } from "./styled"
+import { gapSizes } from "./theme"
 
 type ModalOverlayProps = {
-  children: React.ReactNode
+  children?: React.ReactNode
+  isVisible?: boolean
+  onClose?: () => void
 }
 
-const ModalOverlay = ({ children }: ModalOverlayProps) => {
-  return <div css={shade}>{children}</div>
+const ModalOverlay = (props: ModalOverlayProps) => {
+  return (
+    <Shade
+      pad={gapSizes.large}
+      overflowY="auto"
+      isVisible={props.isVisible}
+      onClick={onlyOnSelf(props.onClose)}
+    >
+      <ContentContainer
+        gap={gapSizes.xsmall}
+        align="flex-end"
+        isVisible={props.isVisible}
+      >
+        <FadedButton onClick={props.onClose}>
+          <Icon icon="close" />
+        </FadedButton>
+        {props.children}
+      </ContentContainer>
+    </Shade>
+  )
 }
 
 export default ModalOverlay
 
-const shade = css`
+const Shade = styled(Box)<{ isVisible?: boolean }>`
   ${fullscreen}
   background-color: ${semiBlack(0.5)};
-  display: flex;
-  flex-flow: column;
-  padding: 4rem 0;
-  overflow-y: auto;
+  z-index:1;
+  transition: 0.2s;
+  opacity: ${(props) => (props.isVisible ? 1 : 0)};
+  visibility: ${(props) => (props.isVisible ? "visible" : "hidden")};
+`
+
+const ContentContainer = styled(Box)<{ isVisible?: boolean }>`
+  margin: auto;
+  transition: 0.2s;
+  transform: translateY(${(props) => (props.isVisible ? 0 : "-16px")});
 `
