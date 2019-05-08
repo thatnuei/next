9ca4bf0d0295204ad0a700944cfbdc7449ecc8b7
nuei <61118778+thatnuei@users.noreply.github.com>
2019-05-07 22:23:44 -0500

    split overlay content from the panel

---
+++ src/overlay/OverlayContent.tsx
@@ -0,0 +1,48 @@
+import { observer } from "mobx-react-lite"
+import React, { CSSProperties } from "react"
+import styled from "styled-components"
+import Box, { BoxProps } from "../ui/Box"
+import { gapSizes } from "../ui/theme"
+import { useOverlay } from "./OverlayContext"
+
+type Props = BoxProps
+
+const OverlayContent = ({ style, ...props }: Props) => {
+  const overlay = useOverlay()
+
+  const baseStyle = {
+    maxWidth: "max-content",
+    ...style,
+  }
+
+  const transitionStyle: CSSProperties = overlay.isVisible
+    ? { transform: `translateY(0)` }
+    : { transform: `translateY(-30px)` }
+
+  return (
+    <Container
+      align="flex-end"
+      justify="center"
+      pad={gapSizes.medium}
+      gap={gapSizes.small}
+      style={{ ...transitionStyle, ...baseStyle }}
+      {...props}
+    >
+      {props.children}
+    </Container>
+  )
+}
+export default observer(OverlayContent)
+
+const Container = styled(Box)`
+  margin: auto;
+  transition: 0.3s;
+  width: 100%;
+  height: 100%;
+
+  /* ignore click events on this element, but not the children */
+  pointer-events: none;
+  > * {
+    pointer-events: all;
+  }
+`
--- src/overlay/OverlayPanel.tsx
+++ src/overlay/OverlayPanel.tsx
@@ -1,10 +1,9 @@
 import { observer } from "mobx-react-lite"
-import React, { CSSProperties } from "react"
+import React from "react"
 import Box from "../ui/Box"
-import { styled } from "../ui/styled"
 import { gapSizes } from "../ui/theme"
 import OverlayCloseButton from "./OverlayCloseButton"
-import { useOverlay } from "./OverlayContext"
+import OverlayContent from "./OverlayContent"
 
 type Props = {
   children?: React.ReactNode
@@ -12,49 +11,20 @@ type Props = {
 }
 
 const OverlayPanel = (props: Props) => {
-  const overlay = useOverlay()
-
-  const baseStyle: CSSProperties = {
-    maxWidth: props.maxWidth || "max-content",
-  }
-
-  const transitionStyle: CSSProperties = overlay.isVisible
-    ? { transform: `translateY(0)` }
-    : { transform: `translateY(-30px)` }
-
   return (
-    <Container
-      align="flex-end"
-      justify="center"
-      pad={gapSizes.medium}
-      gap={gapSizes.small}
-      style={{ ...baseStyle, ...transitionStyle }}
-    >
+    <OverlayContent>
       <OverlayCloseButton />
       <Box width="100%" background="theme0" elevated>
         {props.children}
       </Box>
-    </Container>
+    </OverlayContent>
   )
 }
 
 export default observer(OverlayPanel)
 
-export const OverlayPanelHeader = (props: Props) => (
+export const OverlayPanelHeader = (props: { children?: React.ReactNode }) => (
   <Box background="theme1" pad={gapSizes.small} align="center">
     <h2>{props.children}</h2>
   </Box>
 )
-
-const Container = styled(Box)`
-  margin: auto;
-  transition: 0.3s;
-  width: 100%;
-  height: 100%;
-
-  /* ignore click events on this element, but not the children */
-  pointer-events: none;
-  > * {
-    pointer-events: all;
-  }
-`
