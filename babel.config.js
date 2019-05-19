a6588652afbd077ecea8021e1121478203dfe536
nuei <61118778+thatnuei@users.noreply.github.com>
2019-05-18 22:37:34 -0500

    fix babel config

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   presets: ["@babel/env", "@babel/typescript", "@babel/react"],
   plugins: [
     ["@babel/proposal-decorators", { legacy: true }],
-    "@babel/proposal-class-properties",
+    ["@babel/proposal-class-properties", { loose: true }],
     "@babel/transform-runtime",
     "@babel/plugin-syntax-dynamic-import",
     "babel-plugin-styled-components",
