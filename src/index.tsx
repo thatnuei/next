685861aa1ec517d035ef13ca6ef3f53aa2e9ee71
nuei <61118778+thatnuei@users.noreply.github.com>
2019-06-04 00:45:11 -0500

    use react to hot reload the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,44 @@
 import "focus-visible"
-import { configure } from "mobx"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import ReactDOM from "react-dom"
 import Root from "./Root"
 import RootStore, { RootStoreContext } from "./RootStore"
 
-function renderApp(store: RootStore) {
-  ReactDOM.render(
+function HotReloader() {
+  const [store, setStore] = useState(() => new RootStore())
+  const [_, update] = useState(false)
+
+  useEffect(() => {
+    store.init()
+    return () => store.cleanup()
+  }, [store])
+
+  useEffect(() => {
+    if (module.hot) {
+      module.hot.accept("./Root", () => update((v) => !v))
+      module.hot.accept("./RootStore", () => setStore(new RootStore()))
+    }
+  }, [])
+
+  return (
     <RootStoreContext.Provider value={store}>
+      <Root />
+    </RootStoreContext.Provider>
+  )
+}
+
+function renderApp() {
+  if (process.env.NODE_ENV === "development") {
+    ReactDOM.render(<HotReloader />, document.querySelector("#root"))
+    return
+  }
+
+  ReactDOM.render(
+    <RootStoreContext.Provider value={new RootStore()}>
       <Root />
     </RootStoreContext.Provider>,
     document.querySelector("#root"),
   )
 }
 
-let store!: RootStore
-
-function initStoreAndRender() {
-  if (store) store.cleanup()
-
-  store = new RootStore()
-  store.init()
-
-  renderApp(store)
-}
-
-function main() {
-  configure({ enforceActions: "observed" })
-  initStoreAndRender()
-
-  if (process.env.NODE_ENV !== "production") {
-    if (module.hot) {
-      module.hot.accept("./Root", () => renderApp(store))
-      module.hot.accept("./RootStore", initStoreAndRender)
-    }
-
-    window.store = store
-  }
-}
-
-main()
+renderApp()
