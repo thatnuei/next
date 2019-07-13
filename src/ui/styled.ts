679511582625650e4e2e2561a296f5056a913f46
nuei <61118778+thatnuei@users.noreply.github.com>
2019-07-13 17:29:43 -0500

    fix compile errors with babel macro (still doesn't work)

--- src/ui/styled.ts
+++ src/ui/styled.ts
@@ -1,12 +1,19 @@
 /// <reference types="styled-components/cssprop" />
-import * as sc from "styled-components/macro"
+import baseStyled, {
+  createGlobalStyle as SCcreateGlobalStyle,
+  css as SCcss,
+  keyframes,
+  ThemeContext as SCThemeContext,
+  ThemedStyledComponentsModule,
+  ThemeProvider,
+} from "styled-components/macro"
 import { AppTheme } from "./theme"
 
-export const {
-  default: styled,
-  css,
-  keyframes,
-  ThemeContext,
-  ThemeProvider: StyledThemeProvider,
-  createGlobalStyle,
-} = sc as sc.ThemedStyledComponentsModule<AppTheme>
+type ThemedModule = ThemedStyledComponentsModule<AppTheme>
+
+export const styled = baseStyled as ThemedModule["default"]
+export const css = SCcss as ThemedModule["css"]
+export const ThemeContext = SCThemeContext as ThemedModule["ThemeContext"]
+export const StyledThemeProvider = ThemeProvider as ThemedModule["ThemeProvider"]
+export const createGlobalStyle = SCcreateGlobalStyle as ThemedModule["createGlobalStyle"]
+export { keyframes }
