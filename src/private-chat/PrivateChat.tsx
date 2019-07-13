dfe4f26e98d248589fc0642be69d2e5e25555275
nuei <61118778+thatnuei@users.noreply.github.com>
2019-07-13 00:04:19 -0500

    private chat view: more functionality

--- src/private-chat/PrivateChat.tsx
+++ src/private-chat/PrivateChat.tsx
@@ -1,8 +1,12 @@
 import { observer } from "mobx-react-lite"
 import React from "react"
+import Avatar from "../character/Avatar"
+import CharacterName from "../character/CharacterName"
+import CharacterStatus from "../character/CharacterStatus"
 import Chatbox from "../chat/Chatbox"
+import ChatMenuButton from "../chat/ChatMenuButton"
 import MessageList from "../message/MessageList"
-import { fillArea } from "../ui/helpers"
+import { fillArea, flexColumn, spacedChildrenHorizontal } from "../ui/helpers"
 import { styled } from "../ui/styled"
 import { spacing } from "../ui/theme"
 import { PrivateChatModel } from "./PrivateChatModel"
@@ -12,12 +16,21 @@ type Props = { privateChat: PrivateChatModel }
 function PrivateChat({ privateChat }: Props) {
   return (
     <Container>
-      <header>private chat header</header>
-      <div style={{ flex: 1 }}>
+      <HeaderContainer>
+        <ChatMenuButton />
+        <Avatar name={privateChat.partner} size={50} />
+        <NameAndStatusContainer>
+          <CharacterName name={privateChat.partner} hideStatusDot />
+          <CharacterStatus name={privateChat.partner} />
+        </NameAndStatusContainer>
+      </HeaderContainer>
+
+      <MessageListContainer>
         <MessageList messages={privateChat.messages} />
-      </div>
+      </MessageListContainer>
+
       <ChatboxContainer>
-        <Chatbox onSubmit={alert} />
+        <Chatbox onSubmit={console.log} />
       </ChatboxContainer>
     </Container>
   )
@@ -26,12 +39,28 @@ function PrivateChat({ privateChat }: Props) {
 export default observer(PrivateChat)
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
+  ${flexColumn};
   ${fillArea};
 `
 
 const ChatboxContainer = styled.div`
   background-color: ${(props) => props.theme.colors.theme0};
   padding: ${spacing.xsmall};
+`
+
+const HeaderContainer = styled.header`
+  background-color: ${(props) => props.theme.colors.theme0};
+  padding: ${spacing.small};
+
+  display: flex;
+  align-items: center;
+  ${spacedChildrenHorizontal(spacing.small)};
+`
+
+const MessageListContainer = styled.div`
+  flex: 1;
+`
+
+const NameAndStatusContainer = styled.div`
+  ${flexColumn};
 `
