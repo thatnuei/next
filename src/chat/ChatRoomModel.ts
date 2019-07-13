8be77cb494c7e1bd653f313f1d7cc7731d011df8
nuei <61118778+thatnuei@users.noreply.github.com>
2019-07-13 14:31:01 -0500

    add actions for marking read/unread

--- src/chat/ChatRoomModel.ts
+++ src/chat/ChatRoomModel.ts
@@ -1,4 +1,4 @@
-import { observable } from "mobx"
+import { action, observable } from "mobx"
 import MessageModel from "../message/MessageModel"
 
 export default abstract class ChatRoomModel {
@@ -7,4 +7,14 @@ export default abstract class ChatRoomModel {
 
   @observable
   unread = false
+
+  @action
+  markRead() {
+    this.unread = false
+  }
+
+  @action
+  markUnread() {
+    this.unread = true
+  }
 }
