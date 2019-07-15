ff86ddd089f1868eb29aa8929b1655e890628ae3
nuei <61118778+thatnuei@users.noreply.github.com>
2019-07-14 22:14:56 -0500

    unfuck styled-components module

--- src/ui/styled.ts
+++ src/ui/styled.ts
@@ -1,19 +1,12 @@
 /// <reference types="styled-components/cssprop" />
-import baseStyled, {
-  createGlobalStyle as SCcreateGlobalStyle,
-  css as SCcss,
-  keyframes,
-  ThemeContext as SCThemeContext,
-  ThemedStyledComponentsModule,
-  ThemeProvider,
-} from "styled-components/macro"
+import * as sc from "styled-components"
 import { AppTheme } from "./theme"
 
-type ThemedModule = ThemedStyledComponentsModule<AppTheme>
-
-export const styled = baseStyled as ThemedModule["default"]
-export const css = SCcss as ThemedModule["css"]
-export const ThemeContext = SCThemeContext as ThemedModule["ThemeContext"]
-export const StyledThemeProvider = ThemeProvider as ThemedModule["ThemeProvider"]
-export const createGlobalStyle = SCcreateGlobalStyle as ThemedModule["createGlobalStyle"]
-export { keyframes }
+export const {
+  default: styled,
+  css,
+  keyframes,
+  ThemeContext,
+  ThemeProvider: StyledThemeProvider,
+  createGlobalStyle,
+} = sc as sc.ThemedStyledComponentsModule<AppTheme>
