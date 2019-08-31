c3ecb0bbfb05ed4ef109ecedd4217dcff656c7dc
nuei <61118778+thatnuei@users.noreply.github.com>
2019-08-30 23:02:53 -0500

    no title for now

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,11 @@ import ChatScreen from "../chat/ChatScreen"
 import { useAppSelector } from "../store"
 import CharacterSelect from "./CharacterSelect"
 import Login from "./Login"
-import useAppDocumentTitle from "./useAppDocumentTitle"
 
 function App() {
   const screen = useAppSelector((state) => state.appView)
 
-  useAppDocumentTitle()
+  // useAppDocumentTitle()
 
   switch (screen) {
     case "login":
