80b322144ec4decc6f6cda27b2c721dd5ec86a61
nuei <61118778+thatnuei@users.noreply.github.com>
2019-09-01 18:13:05 -0500

    set up store provider and hot reload

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import App from "./components/App"
+import { createAppStore } from "./store"
 
-ReactDOM.render(<h1>lol</h1>, document.getElementById("root"))
+const store = createAppStore()
+
+function renderApp() {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root"),
+  )
+}
+
+renderApp()
+
+if (module.hot) {
+  module.hot.accept("./components/App", renderApp)
+}
