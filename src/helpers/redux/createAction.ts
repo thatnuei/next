fe9cf26514fc4c84ee71b171d72c20a737a4dd06
nuei <61118778+thatnuei@users.noreply.github.com>
2019-09-01 17:56:33 -0500

    simpler createAction

--- src/helpers/redux/createAction.ts
+++ src/helpers/redux/createAction.ts
@@ -1,16 +1,13 @@
-export function createAction<
-  T extends string,
-  P extends object = {},
-  A extends unknown[] = []
->(type: T, createPayload: (...args: A) => P = () => ({} as P)) {
-  function actionCreator(...args: A) {
-    return { type, ...createPayload(...args) }
+export function createAction<P extends object = {}>(type: string) {
+  function actionCreator(payload: P) {
+    return { type, ...payload }
   }
 
   actionCreator.type = type
 
-  actionCreator.is = (action: { type: string }): action is { type: T } & P =>
-    action.type === type
+  actionCreator.is = (action: {
+    type: string
+  }): action is { type: string } & P => action.type === type
 
   return actionCreator
 }
