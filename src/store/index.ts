21d8d4edda66f8b36fa5b66a7d510eb4498cebff
nuei <61118778+thatnuei@users.noreply.github.com>
2019-09-01 18:11:02 -0500

    create store

---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+import { applyMiddleware, combineReducers, createStore } from "redux"
+import thunk from "redux-thunk"
+import { StateFromReducerMap } from "../helpers/redux/StateFromReducerMap"
+import { navigationReducer } from "./navigation/reducer"
+
+export type AppState = StateFromReducerMap<typeof reducerMap>
+
+const reducerMap = {
+  navigation: navigationReducer,
+}
+
+export function createAppStore() {
+  const reducer = combineReducers(reducerMap)
+  return createStore(reducer, applyMiddleware(thunk))
+}
