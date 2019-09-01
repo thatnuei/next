61bab0abe7bb81065bebe5c77fefd4bc2dfe99e0
nuei <61118778+thatnuei@users.noreply.github.com>
2019-09-01 18:07:16 -0500

    i guess

--- src/helpers/redux/createAction.ts
+++ src/helpers/redux/createAction.ts
@@ -1,13 +1,14 @@
 export function createAction<P = undefined>(type: string) {
   function actionCreator(...args: P extends undefined ? [] : [P]) {
-    return { type, payload: args[0] }
+    const payload = args[0]
+    return { type, payload }
   }
 
   actionCreator.type = type
 
   actionCreator.is = (action: {
     type: string
-  }): action is { type: string } & P => action.type === type
+  }): action is { type: string; payload: P } => action.type === type
 
   return actionCreator
 }
