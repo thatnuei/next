c808f3bb766401f747ca49f2e0f4dfab10ce860f
nuei <61118778+thatnuei@users.noreply.github.com>
2019-09-01 18:23:31 -0500

    app with some dummy selectors/dispatches

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,19 @@
 import React from "react"
+import { useAppDispatch, useAppSelector } from "../store/hooks"
+import { showCharacterSelect, showLogin } from "../store/navigation/actions"
 
-type Props = {}
-
-function App(props: Props) {
-  return <p>hi, i'm App</p>
+function App() {
+  const route = useAppSelector((state) => state.navigation.route)
+  const dispatch = useAppDispatch()
+  return (
+    <main>
+      <p>{route.type}</p>
+      <button onClick={() => dispatch(showLogin())}>go to login</button>
+      <button onClick={() => dispatch(showCharacterSelect())}>
+        go to char select
+      </button>
+    </main>
+  )
 }
 
 export default App
