da31f908eff92848027b34f5e8ba07134b2ad918
nuei <61118778+thatnuei@users.noreply.github.com>
2019-09-04 21:10:31 -0500

    move store to separate file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,56 +1,8 @@
 import { createOvermind, IConfig } from "overmind"
 import { createHook } from "overmind-react"
-import { createCharacter } from "../character/helpers"
-import { Character } from "../character/types"
-import { Dictionary } from "../common/types"
 import { submitLogin } from "../flist/actions"
 import { flist } from "../flist/effects"
-
-type State = {
-  view: "login" | "characterSelect" | "chat"
-  user: {
-    account: string
-    ticket: string
-    characters: string[]
-  }
-  login: {
-    loading: boolean
-    error?: string
-  }
-  characterSelect: {
-    loading: boolean
-    error?: string
-  }
-  chat: {
-    identity: string
-    characters: Dictionary<Character>
-    readonly identityCharacter: Character
-  }
-}
-
-const state: State = {
-  view: "login",
-  user: {
-    account: "",
-    ticket: "",
-    characters: [],
-  },
-  login: {
-    loading: false,
-  },
-  characterSelect: {
-    loading: false,
-  },
-  chat: {
-    identity: "",
-    characters: {},
-
-    get identityCharacter() {
-      const { characters, identity } = this
-      return characters[identity] || createCharacter(identity)
-    },
-  },
-}
+import { state } from "./state"
 
 const config = {
   state,
