96baa76a7596715eebbd433865f52d9bcb80b633
nuei <61118778+thatnuei@users.noreply.github.com>
2019-09-07 13:49:36 -0500

    move command handler types alongside createCommandHandler

--- src/store/fchat/types.ts
+++ src/store/fchat/types.ts
@@ -7,11 +7,3 @@ export type ServerCommandMap = fchat.Connection.ServerCommands
 export type ServerCommand = Values<
   { [K in keyof ServerCommandMap]: { type: K; params: ServerCommandMap[K] } }
 >
-
-export type CommandHandler<K extends keyof ServerCommandMap> = (
-  params: ServerCommandMap[K],
-) => void
-
-export type CommandHandlerMap = {
-  [K in keyof ServerCommandMap]?: CommandHandler<K>
-}
