2e5be15fb17a149c8f23411da5e2a65b12a60a3b
nuei <61118778+thatnuei@users.noreply.github.com>
2019-09-08 20:18:47 -0500

    add constant for fchat error codes

--- src/store/fchat/constants.ts
+++ src/store/fchat/constants.ts
@@ -1,1 +1,7 @@
 export const chatServerUrl = `wss://chat.f-list.net/chat2`
+
+// https://wiki.f-list.net/F-Chat_Error_Codes
+export const errorCodes = {
+  channelMessageCooldown: 5,
+  statusUpdateCooldown: 5, // this is dumb but it is what it is
+} as const
