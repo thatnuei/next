4a2fe1bf40a85acb9a6946b3e4ba43cf92ec62c3
nuei <61118778+thatnuei@users.noreply.github.com>
2019-09-09 20:35:59 -0500

    skeleton chat

---
+++ src/chat/components/Chat.tsx
@@ -0,0 +1,81 @@
+import { cover } from "polished"
+import React from "react"
+import { useStore } from "../../store"
+import { spacedChildrenHorizontal } from "../../ui/helpers"
+import { styled } from "../../ui/styled"
+import { spacing } from "../../ui/theme"
+
+function Chat() {
+  // const {
+  //   channelStore,
+  //   privateChatStore,
+  //   overlayStore,
+  //   chatNavigationStore,
+  // } = useRootStore()
+
+  // const navigationVisible = useChatNavBreakpoint()
+
+  const {
+    state: {
+      chat: { currentRoom },
+    },
+  } = useStore()
+
+  function renderChatRoom() {
+    switch (currentRoom.type) {
+      case "console": {
+        return <p>console</p>
+      }
+
+      case "channel": {
+        return <p>channel</p>
+        // const channel = channelStore.channels.get(activeTab.channelId)
+        // return <ChannelView channel={channel} />
+      }
+
+      // case "privateChat": {
+      // return <p>private chat</p>
+      // const chat = privateChatStore.privateChats.get(activeTab.partnerName)
+      // return <PrivateChat privateChat={chat} />
+      // }
+    }
+  }
+
+  return (
+    // <CharacterMenuProvider>
+    <Container>
+      {/* {navigationVisible && <ChatNavigation />} */}
+      <RoomContainer>{renderChatRoom()}</RoomContainer>
+    </Container>
+
+    // <OverlayProvider value={overlayStore.chatNav}>
+    //   <ChatNavigationOverlay />
+    // </OverlayProvider>
+    //
+    // <OverlayProvider value={overlayStore.updateStatus}>
+    //   <StatusOverlay />
+    // </OverlayProvider>
+    //
+    // <OverlayProvider value={overlayStore.channelBrowser}>
+    //   <ChannelBrowser />
+    // </OverlayProvider>
+    //
+    // <OverlayProvider value={overlayStore.onlineUsers}>
+    //   <OnlineUsers />
+    // </OverlayProvider>
+    // </CharacterMenuProvider>
+  )
+}
+
+export default Chat
+
+const Container = styled.div`
+  background-color: ${({ theme }) => theme.colors.theme2};
+  display: flex;
+  ${cover()};
+  ${spacedChildrenHorizontal(spacing.xsmall)};
+`
+
+const RoomContainer = styled.div`
+  flex: 1;
+`
