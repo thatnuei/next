4671383e48b2f79fed520e0eaea6a4fd9532ee24
nuei <61118778+thatnuei@users.noreply.github.com>
2019-09-29 00:34:53 -0500

    go back to object, apparently class getters are broke

--- src/store/state.ts
+++ src/store/state.ts
@@ -4,51 +4,76 @@ import { createCharacter } from "../character/helpers"
 import { Character } from "../character/types"
 import { Dictionary } from "../common/types"
 
-type AvailableChannels = {
-  public: ChannelBrowserEntry[]
-  private: ChannelBrowserEntry[]
-}
-
-type AsyncState = { loading: boolean; error?: string }
-
-class UserState {
-  account = ""
-  ticket = ""
-  characters: string[] = []
-  login: AsyncState = {
-    loading: false,
-  }
-}
-
-class State {
-  view: "login" | "characterSelect" | "chat" = "login"
+type State = {
+  view: "login" | "characterSelect" | "chat"
   modal?: AppModal
 
-  user = new UserState()
+  user: {
+    account: string
+    ticket: string
+    characters: string[]
+
+    login: {
+      loading: boolean
+      error?: string
+    }
+  }
 
   // core chat data
-  identity = ""
-  characters: Dictionary<Character> = {}
-  updatingStatus = false
-  connecting = false
+  identity: string
+  characters: Dictionary<Character>
+  readonly identityCharacter: Character
+
+  // channel data
+  channels: Dictionary<Channel>
+  availableChannels: {
+    public: ChannelBrowserEntry[]
+    private: ChannelBrowserEntry[]
+  }
+
+  // loading states
+  updatingStatus: boolean
+  connecting: boolean
+  fetchingPublicChannels: boolean
+  fetchingPrivateChannels: boolean
+  readonly fetchingAvailableChannels: boolean
+}
+
+export const state: State = {
+  view: "login",
+
+  user: {
+    account: "",
+    ticket: "",
+    characters: [],
+    login: {
+      loading: false,
+    },
+  },
+
+  // core chat data
+  identity: "",
+  characters: {},
 
   get identityCharacter() {
     const characters = this.characters as Dictionary<Character>
     return characters[this.identity] || createCharacter(this.identity)
-  }
+  },
 
   // channel data
-  channels: Dictionary<Channel> = {}
-  availableChannels: AvailableChannels = {
+  channels: {},
+  availableChannels: {
     public: [],
     private: [],
-  }
-  fetchingPublicChannels = false
-  fetchingPrivateChannels = false
+  },
+
+  // loading states
+  updatingStatus: false,
+  connecting: false,
+  fetchingPublicChannels: false,
+  fetchingPrivateChannels: false,
 
   get fetchingAvailableChannels() {
     return this.fetchingPublicChannels || this.fetchingPrivateChannels
-  }
+  },
 }
-
-export const state = new State()
