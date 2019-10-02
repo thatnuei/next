50c875ccd603c5e1404e3b295bd44913a2bc2e5b
nuei <61118778+thatnuei@users.noreply.github.com>
2019-10-01 23:13:14 -0500

    asserts clauses break eslint

--- src/common/helpers/assert.ts
+++ src/common/helpers/assert.ts
@@ -1,6 +1,8 @@
-function assert(value: unknown, message?: string): asserts value {
-  if (value == null) {
-    throw new Error(message || "assertion failed: value was nil")
-  }
-}
-export default assert
+// eslint broke
+// function assert(value: unknown, message?: string): asserts value {
+//   if (value == null) {
+//     throw new Error(message || "assertion failed: value was nil")
+//   }
+// }
+// export default assert
+export {}
--- src/react/hooks/useRequiredContext.ts
+++ src/react/hooks/useRequiredContext.ts
@@ -1,11 +1,12 @@
 import { useContext } from "react"
-import assert from "../../common/helpers/assert"
 
 export function useRequiredContext<V>(
   context: React.Context<V | undefined | null>,
   errorMessage = "Context provider not found",
 ) {
   const contextValue = useContext(context)
-  assert(contextValue, errorMessage)
+  if (contextValue == null) {
+    throw new Error(errorMessage)
+  }
   return contextValue
 }
