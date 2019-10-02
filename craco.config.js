7a3a0959ee599426e5062a5d4824cbe6c3d3aef6
nuei <61118778+thatnuei@users.noreply.github.com>
2019-10-01 19:03:35 -0500

    add plugins to fix eslint

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   babel: {
-    plugins: ["lodash", "styled-components"],
+    plugins: [
+      "lodash",
+      "styled-components",
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-proposal-nullish-coalescing-operator",
+    ],
   },
 }
