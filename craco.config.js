6b535ebc34282fd6c2325b558054a39bcc96e683
nuei <61118778+thatnuei@users.noreply.github.com>
2020-01-02 13:31:47 -0600

    upgrade packages, simplify build

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   babel: {
-    plugins: [
-      "lodash",
-      "styled-components",
-      "@babel/plugin-proposal-optional-chaining",
-      "@babel/plugin-proposal-nullish-coalescing-operator",
-    ],
+    plugins: ["lodash", "styled-components"],
   },
 }
