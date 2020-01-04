82bacf5334b5cd8d1166a8fda7afda2fe04623b2
nuei <61118778+thatnuei@users.noreply.github.com>
2020-01-03 23:31:21 -0600

    store admins/ignored

--- src/chat/ChatStore.new.ts
+++ src/chat/ChatStore.new.ts
@@ -1,9 +1,34 @@
+import { observable } from "mobx"
 import { createCommandHandler } from "./helpers"
 
 export class ChatStore {
+  @observable admins = new Set<string>()
+  @observable ignored = new Set<string>()
+
   handleSocketCommand = createCommandHandler({
     IDN() {
       // join last rooms and such
+    },
+
+    ADL: ({ ops }) => {
+      this.admins = new Set(ops)
+    },
+
+    IGN: (params) => {
+      switch (params.action) {
+        case "init":
+        case "list":
+          this.ignored = new Set(params.characters)
+          break
+
+        case "add":
+          this.ignored.add(params.character)
+          break
+
+        case "delete":
+          this.ignored.delete(params.character)
+          break
+      }
     },
   })
 }
