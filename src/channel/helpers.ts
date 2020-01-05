934113aff4c1f193fbd33e46ef3b8b8ab3cad775
nuei <61118778+thatnuei@users.noreply.github.com>
2020-01-04 18:59:11 -0600

    make sure channels are observable

--- src/channel/helpers.ts
+++ src/channel/helpers.ts
@@ -1,16 +1,17 @@
+import { observable } from "mobx"
 import { Channel } from "./types"
 
 export function createChannel(id: string): Channel {
-  return {
+  return observable<Channel>({
     id,
     name: id,
     description: "",
     mode: "both",
     selectedMode: "both",
-    users: new Set(),
-    ops: new Set(),
+    users: new Set<string>(),
+    ops: new Set<string>(),
     messages: [],
     input: "",
     unread: false,
-  }
+  })
 }
