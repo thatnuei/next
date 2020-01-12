cebec5f7574d324a5daa15b0135c126bbb539629
nuei <61118778+thatnuei@users.noreply.github.com>
2020-01-11 18:21:47 -0600

    FactoryMap: createNew -> getDefault

--- src/state/classes/FactoryMap.ts
+++ src/state/classes/FactoryMap.ts
@@ -4,13 +4,13 @@ export class FactoryMap<T> {
   @observable
   private readonly items = new Map<string, T>()
 
-  constructor(private readonly createNew: (key: string) => T) {}
+  constructor(private readonly getDefault: (key: string) => T) {}
 
   set = (key: string, item: T) => {
     this.items.set(key, item)
   }
 
-  get = (key: string): T => this.items.get(key) ?? this.createNew(key)
+  get = (key: string): T => this.items.get(key) ?? this.getDefault(key)
 
   update = (key: string, update: (item: T) => void) => {
     const item = this.get(key)
