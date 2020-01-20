9e23891c21a70655f56f902ca0e468341ce9a798
nuei <61118778+thatnuei@users.noreply.github.com>
2020-01-19 21:17:43 -0600

    remove GlobalStyle

--- src/app/Root.tsx
+++ src/app/Root.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import GlobalStyle from "../ui/components/GlobalStyle"
 import { baseTheme } from "../ui/theme"
 import { ThemeProvider } from "../ui/themeContext"
 import App from "./App"
@@ -8,7 +7,6 @@ function Root() {
   return (
     <ThemeProvider theme={baseTheme}>
       <App />
-      <GlobalStyle />
     </ThemeProvider>
   )
 }
