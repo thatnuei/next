771ea1fbeb1d67943a5967a25ba0a16913ce88e9
nuei <61118778+thatnuei@users.noreply.github.com>
2020-01-27 19:38:12 -0600

    refactor Modal styling

--- src/ui/components/Modal.tsx
+++ src/ui/components/Modal.tsx
@@ -1,16 +1,25 @@
 import React from "react"
 import { FocusOn } from "react-focus-on"
+import { focusOnFillFix, raisedPanel } from "../components"
 import {
-  flexGrow,
-  focusOnFillFix,
-  fullscreen,
-  resolveStyleUnit,
-} from "../helpers"
-import { styled } from "../styled"
-import { spacing } from "../theme"
+  absolute,
+  absoluteCover,
+  bgSemiBlack,
+  fixed,
+  flex,
+  flex1,
+  h,
+  hidden,
+  m,
+  opacity,
+  p,
+  transition,
+  translateDown,
+  visible,
+  w,
+} from "../helpers.new"
 import FadedButton from "./FadedButton"
 import Icon from "./Icon"
-import RaisedPanel from "./RaisedPanel"
 import RaisedPanelHeader from "./RaisedPanelHeader"
 
 type Props = {
@@ -42,13 +51,14 @@ function Modal({
     </FadedButton>
   )
 
+  const modalPanelStyle = {
+    maxWidth: panelWidth,
+    maxHeight: panelHeight,
+  }
+
   return (
-    <Shade visible={props.visible} fillMode={fillMode}>
-      <ModalPanel
-        visible={props.visible}
-        maxWidth={panelWidth}
-        maxHeight={panelHeight}
-      >
+    <div css={shadeCss(props)}>
+      <div css={modalPanelCss(props)} style={modalPanelStyle}>
         <FocusOn
           enabled={props.visible}
           onEscapeKey={props.onClose}
@@ -58,58 +68,32 @@ function Modal({
             center={<h2>{props.title}</h2>}
             right={closeButton}
           />
-          <PanelBody>{props.children}</PanelBody>
+          <div css={[flex1, { minHeight: 0 }]}>{props.children}</div>
         </FocusOn>
-      </ModalPanel>
-    </Shade>
+      </div>
+    </div>
   )
 }
 
 export default Modal
 
-type ShadeProps = { visible: boolean; fillMode: FillMode }
+const shadeCss = (props: Props) => [
+  absoluteCover,
+  bgSemiBlack(50),
+  transition("opacity, visibility"),
+  p(3),
+  props.fillMode === "full" ? fixed : absolute,
+  props.visible ? [opacity(100), visible] : [opacity(0), hidden],
+  flex("column"),
+]
 
-const Shade = styled.div<ShadeProps>`
-  ${fullscreen};
-
-  background-color: rgba(0, 0, 0, 0.5);
-  transition: 0.3s;
-
-  display: flex;
-  flex-direction: column;
-  padding: ${spacing.small};
-
-  ${({ fillMode }) =>
-    fillMode === "full" ? { position: "fixed" } : { position: "absolute" }};
-
-  ${({ visible }) =>
-    visible
-      ? { opacity: 1, visibility: "visible" }
-      : { opacity: 0, visibility: "hidden" }}
-
-  > * {
-    margin: auto;
-  }
-`
-
-type PanelProps = { visible: boolean; maxWidth: number; maxHeight: number }
-
-const ModalPanel = styled(RaisedPanel)<PanelProps>`
-  width: 100%;
-  height: 100%;
-  max-width: ${({ maxWidth }) => resolveStyleUnit(maxWidth)};
-  max-height: ${({ maxHeight }) => resolveStyleUnit(maxHeight)};
-
-  transition: 0.3s transform;
-
-  ${({ visible }) =>
-    visible
-      ? { transform: `translateY(0)` }
-      : { transform: `translateY(20px)` }}
-
-  ${focusOnFillFix};
-`
-
-const PanelBody = styled.div`
-  ${flexGrow};
-`
+const modalPanelCss = (props: Props) => [
+  raisedPanel,
+  flex1,
+  w("full"),
+  h("full"),
+  transition("transform"),
+  m("auto"),
+  props.visible ? {} : translateDown,
+  focusOnFillFix,
+]
