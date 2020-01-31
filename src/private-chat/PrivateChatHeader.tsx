b93744393e87423c900a0a31903fbbddf563b9fb
nuei <61118778+thatnuei@users.noreply.github.com>
2020-01-30 19:28:46 -0600

    fix PrivateChatHeader styles

--- src/private-chat/PrivateChatHeader.tsx
+++ src/private-chat/PrivateChatHeader.tsx
@@ -5,32 +5,29 @@ import CharacterNameNew from "../character/CharacterName"
 import CharacterStatus from "../character/CharacterStatus"
 import { Character } from "../character/types"
 import HeaderMenuButton from "../chat/HeaderMenuButton"
-import { flexColumn, spacedChildrenHorizontal } from "../ui/helpers"
-import { styled } from "../ui/styled"
-import { spacing } from "../ui/theme"
+import {
+  alignItems,
+  bgMidnight,
+  displayNone,
+  flex,
+  media,
+  ml,
+  p,
+} from "../ui/helpers.new"
 
 type Props = { character: Character }
 
 function PrivateChatHeader({ character }: Props) {
   return (
-    <Container>
-      <HeaderMenuButton />
+    <div css={[bgMidnight(700), p(3), flex(), alignItems("center")]}>
+      <HeaderMenuButton css={[media.lg(displayNone), ml(-3)]} />
       <Avatar name={character.name} size={50} />
-      <div css={flexColumn}>
+      <div css={[flex("column"), ml(3)]}>
         <CharacterNameNew character={character} />
         <CharacterStatus {...character} />
       </div>
-    </Container>
+    </div>
   )
 }
 
 export default observer(PrivateChatHeader)
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.colors.theme0};
-  padding: ${spacing.small};
-
-  display: flex;
-  align-items: center;
-  ${spacedChildrenHorizontal(spacing.small)};
-`
