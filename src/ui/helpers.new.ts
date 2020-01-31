b3871a99d7cf79cdc480ec6819984bdbfc9f17d5
nuei <61118778+thatnuei@users.noreply.github.com>
2020-01-30 19:28:31 -0600

    add negative numbers to SizeUnit

--- src/ui/helpers.new.ts
+++ src/ui/helpers.new.ts
@@ -20,6 +20,24 @@ type SizeUnit =
   | 48
   | 56
   | 64
+  | -0
+  | -1
+  | -2
+  | -3
+  | -4
+  | -5
+  | -6
+  | -8
+  | -10
+  | -12
+  | -16
+  | -20
+  | -24
+  | -32
+  | -40
+  | -48
+  | -56
+  | -64
   | string
 
 type FlexDirection = "row" | "column" | "row-reverse" | "column-reverse"
