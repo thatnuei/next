bcd0411a0c351e5a7a343b61de6afb214d610cc3
nuei <61118778+thatnuei@users.noreply.github.com>
2020-02-01 14:36:09 -0600

    note on useChannel

--- src/state/hooks/useChannel.ts
+++ src/state/hooks/useChannel.ts
@@ -1,6 +1,8 @@
 import { useEffect } from "react"
 import { Channel } from "../classes/Channel"
 
+// TODO: rename to `useChannelListener`
+// since we'll want a useChannel that does something else
 export function useChannel<L extends (value?: any) => void>(
   channel: Channel<L>,
   listener: L,
