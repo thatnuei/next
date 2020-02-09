aba03f9aa772e2bc3ee72af9b36236266b25425a
nuei <61118778+thatnuei@users.noreply.github.com>
2020-02-09 17:58:14 -0600

    use describe i guess

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,32 +2,34 @@ import { fireEvent, render, wait } from "@testing-library/react"
 import React from "react"
 import App from "./App"
 
-it("disallows login when empty", () => {
-  const helpers = render(<App />)
+describe("App", () => {
+  it("disallows login when empty", () => {
+    const helpers = render(<App />)
 
-  expect(helpers.getByLabelText(/username/i)).toBeEmpty()
-  expect(helpers.getByLabelText(/password/i)).toBeEmpty()
-  expect(helpers.getByText(/log in/i)).toBeDisabled()
-})
-
-it("shows an error if login failed", () => {
-  const mockErrorMessage = "something really bad happened"
-
-  jest.mock("./authenticate", () => ({
-    async authenticate() {
-      throw new Error(mockErrorMessage)
-    },
-  }))
-
-  const helpers = render(<App />)
-
-  fireEvent.input(helpers.getByLabelText(/username/i), {
-    target: { value: "test" },
+    expect(helpers.getByLabelText(/username/i)).toBeEmpty()
+    expect(helpers.getByLabelText(/password/i)).toBeEmpty()
+    expect(helpers.getByText(/log in/i)).toBeDisabled()
   })
-  fireEvent.input(helpers.getByLabelText(/password/i), {
-    target: { value: "test" },
-  })
-  fireEvent.click(helpers.getByText(/log in/i))
 
-  wait(() => expect(helpers.queryByText(mockErrorMessage)).toBeDefined())
+  it("shows an error if login failed", () => {
+    const mockErrorMessage = "something really bad happened"
+
+    jest.mock("./authenticate", () => ({
+      async authenticate() {
+        throw new Error(mockErrorMessage)
+      },
+    }))
+
+    const helpers = render(<App />)
+
+    fireEvent.input(helpers.getByLabelText(/username/i), {
+      target: { value: "test" },
+    })
+    fireEvent.input(helpers.getByLabelText(/password/i), {
+      target: { value: "test" },
+    })
+    fireEvent.click(helpers.getByText(/log in/i))
+
+    wait(() => expect(helpers.queryByText(mockErrorMessage)).toBeDefined())
+  })
 })
