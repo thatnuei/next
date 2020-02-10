eb0fc59eb7b4ecf7d98c9e96856262e07e3d0a14
nuei <61118778+thatnuei@users.noreply.github.com>
2020-02-09 18:24:36 -0600

    add purgecss

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,13 @@
+const { whenProd } = require("@craco/craco")
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./@(src|public)/**/*.@(ts|tsx|html|css)"],
+})
+
 module.exports = {
   style: {
-    postcss: { plugins: [require("tailwindcss")] },
+    postcss: {
+      plugins: [require("tailwindcss"), ...whenProd(() => [purgecss], [])],
+    },
   },
 }
