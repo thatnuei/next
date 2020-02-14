25a4f18a936b3e3c5e3a65eab37115d8e5313de7
nuei <61118778+thatnuei@users.noreply.github.com>
2020-02-14 00:25:31 -0600

    swap out tailwind for emotion

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,5 @@
-const { whenProd } = require("@craco/craco")
-
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./@(src|public)/**/*.@(ts|tsx|html|css)"],
-})
-
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [require("tailwindcss"), ...whenProd(() => [purgecss], [])],
-    },
+  babel: {
+    plugins: ["@emotion/css-prop"],
   },
 }
