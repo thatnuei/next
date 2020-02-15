566574648ed2b305023805dad084c94900b5ee11
nuei <61118778+thatnuei@users.noreply.github.com>
2020-02-14 20:45:37 -0600

    add react refresh

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,38 @@
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin")
+
 module.exports = {
   babel: {
     presets: ["@emotion/css-prop"],
   },
+  webpack: {
+    configure(config, { env }) {
+      if (env === "development") addReactRefresh(config)
+      return config
+    },
+  },
+}
+
+function addReactRefresh(config) {
+  config.plugins.push(
+    new ReactRefreshWebpackPlugin({ disableRefreshCheck: true }),
+  )
+
+  // the refresh babel plugin crashes the build if it transpiles node_modules,
+  // presumably because libs from node_modules run before
+  // the refresh helper functions actually get defined by the webpack plugin?
+  //
+  // either way, our workaround is to explicitly add it as a loader here,
+  // skipping over node_modules completely
+  config.module.rules.unshift({
+    test: /\.(js|ts)x?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: require.resolve("babel-loader"),
+      options: {
+        babelrc: false,
+        configFile: false,
+        plugins: [require.resolve("react-refresh/babel")],
+      },
+    },
+  })
 }
