dd2b796a4fdd0be945d178d4d3d595166f2b687c
nuei <61118778+thatnuei@users.noreply.github.com>
2020-02-15 22:07:26 -0600

    add some jsx helper types

--- src/dom/Button.tsx
+++ src/dom/Button.tsx
@@ -1,7 +1,8 @@
 import React from "react"
+import { TagProps } from "../jsx/types"
 
 /** Regular DOM button with a better default type */
-function Button(props: React.ComponentPropsWithoutRef<"button">) {
+function Button(props: TagProps<"button">) {
   return <button type="button" {...props} />
 }
 
---
+++ src/jsx/types.ts
@@ -0,0 +1,13 @@
+/** Get the props for a given DOM element tag
+ *
+ * here to keep JSX mostly agnostic from react, and for less typing */
+export type TagProps<
+  T extends keyof JSX.IntrinsicElements
+> = React.ComponentPropsWithoutRef<T>
+
+/** Get the props for a component
+ *
+ * here to keep JSX mostly agnostic from react, and for less typing */
+export type ComponentProps<
+  C extends React.ComponentType
+> = React.ComponentPropsWithoutRef<C>
