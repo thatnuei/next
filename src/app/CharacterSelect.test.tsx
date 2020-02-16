de37bb00660cb8d998a66405756720db979b5802
nuei <61118778+thatnuei@users.noreply.github.com>
2020-02-15 23:14:43 -0600

    fix type error in tests

--- src/app/CharacterSelect.test.tsx
+++ src/app/CharacterSelect.test.tsx
@@ -14,6 +14,7 @@ describe("CharacterSelect", () => {
         characters={characters}
         initialCharacter={initialCharacter}
         onSubmit={handleSubmit}
+        onReturnToLogin={() => {}}
       />,
     )
 
