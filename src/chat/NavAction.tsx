2449ac22a00e288b72293385c740d1f1d9bb34cd
nuei <61118778+thatnuei@users.noreply.github.com>
2020-02-15 22:07:42 -0600

    NavAction: accept all button props

--- src/chat/NavAction.tsx
+++ src/chat/NavAction.tsx
@@ -1,23 +1,18 @@
 import React from "react"
 import Button from "../dom/Button"
+import { ComponentProps } from "../jsx/types"
 import { fadedButton } from "../ui/components"
 import Icon, { IconName } from "../ui/Icon"
 import { p } from "../ui/style"
 
-type Props = {
+type Props = ComponentProps<typeof Button> & {
   icon: IconName
-  title: string
-  onClick?: () => void
 }
 
-export default function NavAction(props: Props) {
+export default function NavAction({ icon, ...props }: Props) {
   return (
-    <Button
-      css={[fadedButton, p(3)]}
-      title={props.title}
-      onClick={props.onClick}
-    >
-      <Icon name={props.icon} size={3} />
+    <Button css={[fadedButton, p(3)]} {...props}>
+      <Icon name={icon} size={3} />
     </Button>
   )
 }
