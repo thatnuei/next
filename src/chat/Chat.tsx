54eee8269857c795bf96311ad41136cb07fa2fa1
nuei <61118778+thatnuei@users.noreply.github.com>
2020-02-16 19:20:07 -0600

    Chat: hoist some data

--- src/chat/Chat.tsx
+++ src/chat/Chat.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react"
-import ChannelView from "../channel/ChannelView"
 import Avatar from "../character/Avatar"
 import CharacterDetails from "../character/CharacterDetails"
 import { Character } from "../character/types"
 import { safeIndex } from "../common/safeIndex"
+import Button from "../dom/Button"
+import PrivateChatView from "../privateChat/PrivateChatView"
+import { fadedButton } from "../ui/components"
 import Icon, { iconSize } from "../ui/Icon"
 import {
+  block,
   fixedCover,
   flex1,
   flexColumn,
@@ -38,87 +41,93 @@ const testCharacter: Character = {
   statusMessage: "look at this photograph every time i do it makes me laugh",
 }
 
+const subaru: Character = {
+  name: "Subaru-chan",
+  gender: "Female",
+  status: "online",
+  statusMessage: "h",
+}
+
 type RoomTabInfo =
   | { type: "channel"; title: string }
   | { type: "private-chat"; partner: Character }
 
+const users: Character[] = [
+  {
+    name: "Testificate",
+    gender: "Male",
+    status: "online",
+    statusMessage: "look at this photograph every time i do it makes me laugh",
+  },
+  {
+    name: "Testificate",
+    gender: "Female",
+    status: "looking",
+    statusMessage: "look at this photograph every time i do it makes me laugh",
+  },
+  {
+    name: "Testificate",
+    gender: "Shemale",
+    status: "busy",
+    statusMessage: "look at this photograph every time i do it makes me laugh",
+  },
+  {
+    name: "Testificate",
+    gender: "Transgender",
+    status: "away",
+    statusMessage: "look at this photograph every time i do it makes me laugh",
+  },
+  {
+    name: "Testificate",
+    gender: "Cunt-boy",
+    status: "dnd",
+    statusMessage: "look at this photograph every time i do it makes me laugh",
+  },
+  {
+    name: "Testificate",
+    gender: "Male-Herm",
+    status: "offline",
+    statusMessage: "look at this photograph every time i do it makes me laugh",
+  },
+  {
+    name: "Testificate",
+    gender: "None",
+    status: "offline",
+    statusMessage: "look at this photograph every time i do it makes me laugh",
+  },
+]
+
+const messages = users.map((sender) => ({
+  sender,
+  text:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non vehicula metus. Suspendisse sollicitudin lacus tortor, sed ornare ante pretium ut. In accumsan, purus sit amet hendrerit convallis, libero ex porta dolor, ac varius lacus nulla id lacus. In et gravida dui. Nulla nec quam erat. Aliquam nec arcu est. Sed at elit vulputate, convallis libero sit amet, ornare sem. Maecenas condimentum risus ipsum, a malesuada sem auctor sit amet. Pellentesque a vehicula lectus, sed posuere lacus. Quisque id nulla nec magna aliquam mollis. Quisque quis dolor erat.",
+  timestamp: Date.now(),
+}))
+
+function getTabProps(tab: RoomTabInfo) {
+  if (tab.type === "channel") {
+    return {
+      key: `channel:${tab.title}`,
+      title: tab.title,
+      icon: <Icon name="public" />,
+    }
+  }
+
+  return {
+    key: `private-chat:${tab.partner.name}`,
+    title: tab.partner.name,
+    icon: <Avatar name={tab.partner.name} css={size(iconSize(3))} />,
+  }
+}
+
 function Chat(props: Props) {
   const { tabs, activeTab, setActiveTab } = useChatRoomTabs()
 
-  const users: Character[] = [
-    {
-      name: "Testificate",
-      gender: "Male",
-      status: "online",
-      statusMessage:
-        "look at this photograph every time i do it makes me laugh",
-    },
-    {
-      name: "Testificate",
-      gender: "Female",
-      status: "looking",
-      statusMessage:
-        "look at this photograph every time i do it makes me laugh",
-    },
-    {
-      name: "Testificate",
-      gender: "Shemale",
-      status: "busy",
-      statusMessage:
-        "look at this photograph every time i do it makes me laugh",
-    },
-    {
-      name: "Testificate",
-      gender: "Transgender",
-      status: "away",
-      statusMessage:
-        "look at this photograph every time i do it makes me laugh",
-    },
-    {
-      name: "Testificate",
-      gender: "Cunt-boy",
-      status: "dnd",
-      statusMessage:
-        "look at this photograph every time i do it makes me laugh",
-    },
-    {
-      name: "Testificate",
-      gender: "Male-Herm",
-      status: "offline",
-      statusMessage:
-        "look at this photograph every time i do it makes me laugh",
-    },
-    {
-      name: "Testificate",
-      gender: "None",
-      status: "offline",
-      statusMessage:
-        "look at this photograph every time i do it makes me laugh",
-    },
-  ]
-
-  const messages = users.map((sender) => ({
-    sender,
-    text:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non vehicula metus. Suspendisse sollicitudin lacus tortor, sed ornare ante pretium ut. In accumsan, purus sit amet hendrerit convallis, libero ex porta dolor, ac varius lacus nulla id lacus. In et gravida dui. Nulla nec quam erat. Aliquam nec arcu est. Sed at elit vulputate, convallis libero sit amet, ornare sem. Maecenas condimentum risus ipsum, a malesuada sem auctor sit amet. Pellentesque a vehicula lectus, sed posuere lacus. Quisque id nulla nec magna aliquam mollis. Quisque quis dolor erat.",
-    timestamp: Date.now(),
-  }))
-
-  function getTabProps(tab: RoomTabInfo) {
-    if (tab.type === "channel") {
-      return {
-        key: `channel:${tab.title}`,
-        title: tab.title,
-        icon: <Icon name="public" />,
-      }
-    }
-
-    return {
-      key: `private-chat:${tab.partner.name}`,
-      title: tab.partner.name,
-      icon: <Avatar name={tab.partner.name} css={size(iconSize(3))} />,
-    }
-  }
+  const menuButton = (
+    <Button css={[fadedButton, mr(3), hidden, screen.small(block)]}>
+      <Icon name="menu" />
+    </Button>
+  )
 
   return (
     <div css={[fixedCover, flexRow]}>
@@ -149,9 +158,10 @@ function Chat(props: Props) {
       </nav>
 
       <div css={[flex1]}>
-        <ChannelView
-          users={users}
+        <PrivateChatView
+          partner={subaru}
           messages={messages}
+          menuButton={menuButton}
           chatInput={<ChatInput identity={props.identity} />}
         />
       </div>
@@ -167,15 +177,7 @@ function useChatRoomTabs() {
     { type: "channel", title: "Fantasy" },
     { type: "channel", title: "Story Driven LFRP" },
     { type: "channel", title: "Kissaten Treehouse (Slice of Life)" },
-    {
-      type: "private-chat",
-      partner: {
-        name: "Subaru-chan",
-        gender: "Female",
-        status: "online",
-        statusMessage: "bleh",
-      },
-    },
+    { type: "private-chat", partner: subaru },
   ])
 
   const [activeTab = safeIndex(tabs, 0), setActiveTab] = useState<RoomTabInfo>()
