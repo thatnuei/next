1dbb121993335c0027f75951e4f52b9836269eff
nuei <61118778+thatnuei@users.noreply.github.com>
2020-02-22 16:38:35 -0600

    add range helper

---
+++ src/common/range.ts
@@ -0,0 +1,15 @@
+export function range(min: number, max?: number) {
+  if (max === undefined) {
+    max = min
+    min = 0
+  }
+
+  const length = max - min
+  const result = Array(length)
+
+  for (let i = 0; i < length; i++) {
+    result[i] = i + min
+  }
+
+  return result
+}
