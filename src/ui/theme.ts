c78f920b68ba65aed2256c34f28c0aaf88db6b30
nuei <61118778+thatnuei@users.noreply.github.com>
2020-03-08 11:59:04 -0500

    define new theme (wip)

--- src/ui/theme.ts
+++ src/ui/theme.ts
@@ -1,20 +1,5 @@
-import { ThemeProvider, useTheme } from "@emotion/react"
 import { rgb, shade } from "polished"
-
-type ColorPalette = Record<0 | 1 | 2, string>
-
-export type AppTheme = {
-  colors: {
-    background: ColorPalette
-    text: string
-  }
-  shadow: {
-    normal: string
-    inner: string
-  }
-}
-
-export type BackgroundColorKey = keyof AppTheme["colors"]["background"]
+import { Theme } from "theme-ui"
 
 // http://flatuicolors.com/
 const midnight = rgb(38, 65, 94)
@@ -22,32 +7,58 @@ const clouds = rgb(236, 240, 241)
 export const emerald = rgb(46, 204, 113)
 export const tomato = rgb(231, 76, 60)
 
-export const midnightTheme: AppTheme = {
+export const theme: Theme = {
   colors: {
-    background: [midnight, shade(0.3, midnight), shade(0.5, midnight)],
+    background: midnight,
+    background0: midnight,
+    background1: shade(0.3, midnight),
+    background2: shade(0.5, midnight),
     text: clouds,
-  },
-  shadow: {
-    normal: "0px 2px 12px rgba(0, 0, 0, 0.3)",
-    inner: "0px 1px 4px rgba(0, 0, 0, 0.2) inset",
+    muted: shade(0.5, midnight),
   },
 }
 
-export const lightTheme: AppTheme = {
-  ...midnightTheme,
-  colors: {
-    ...midnightTheme.colors,
-    background: [clouds, shade(0.1, clouds), shade(0.3, clouds)],
-    text: shade(0.1, midnight),
-  },
-  shadow: {
-    ...midnightTheme.shadow,
-    normal: "0px 2px 12px rgba(0, 0, 0, 0.3)",
-  },
-}
+// type ColorPalette = Record<0 | 1 | 2, string>
 
-export { ThemeProvider, useTheme }
+// export type AppTheme = {
+//   colors: {
+//     background: ColorPalette
+//     text: string
+//   }
+//   shadow: {
+//     normal: string
+//     inner: string
+//   }
+// }
 
-declare module "@emotion/react" {
-  export interface Theme extends AppTheme {}
-}
+// export type BackgroundColorKey = keyof AppTheme["colors"]["background"]
+
+// export const midnightTheme: AppTheme = {
+//   colors: {
+//     background: [midnight, shade(0.3, midnight), shade(0.5, midnight)],
+//     text: clouds,
+//   },
+//   shadow: {
+//     normal: "0px 2px 12px rgba(0, 0, 0, 0.3)",
+//     inner: "0px 1px 4px rgba(0, 0, 0, 0.2) inset",
+//   },
+// }
+
+// export const lightTheme: AppTheme = {
+//   ...midnightTheme,
+//   colors: {
+//     ...midnightTheme.colors,
+//     background: [clouds, shade(0.1, clouds), shade(0.3, clouds)],
+//     text: shade(0.1, midnight),
+//   },
+//   shadow: {
+//     ...midnightTheme.shadow,
+//     normal: "0px 2px 12px rgba(0, 0, 0, 0.3)",
+//   },
+// }
+
+// export { ThemeProvider, useTheme }
+
+// declare module "@emotion/react" {
+//   export interface Theme extends AppTheme {}
+// }
