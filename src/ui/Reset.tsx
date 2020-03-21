5b56d8bd7fbecad81f356b86102766bf7c8f3b15
nuei <61118778+thatnuei@users.noreply.github.com>
2020-03-21 17:49:02 -0500

    use tailwind reset

--- src/ui/Reset.tsx
+++ src/ui/Reset.tsx
@@ -1,65 +1,15 @@
-import { css, Global } from "@emotion/react"
+import { Global } from "@emotion/react"
 import React from "react"
-import { themeBgColor, themeTextColor } from "./style"
-import { AppTheme } from "./theme"
-
-const reset = (theme: AppTheme) => css`
-  * {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }
-
-  :root {
-    ${themeBgColor(2)(theme)};
-    ${themeTextColor(theme)};
-    font: 15px Roboto, sans-serif;
-    line-height: 1.5;
-    word-break: break-word;
-    overflow-wrap: break-word;
-    overflow: hidden;
-  }
-
-  button,
-  input,
-  textarea,
-  select {
-    background: none;
-    color: inherit;
-    font: inherit;
-    text-align: left;
-    border: none;
-
-    /* weird edge(?) bug: if we set this to 0, the border still shows up on select */
-    border-radius: 0.0000001px;
-  }
-
-  button {
-    cursor: pointer;
-  }
-
-  a {
-    font: inherit;
-    color: inherit;
-    text-decoration: none;
-  }
-
-  fieldset {
-    border: none;
-  }
-
-  ol,
-  ul,
-  li {
-    list-style: none;
-  }
-
-  img,
-  svg {
-    display: block;
-  }
-`
+import "tailwindcss/dist/base.css"
+import tw from "twin.macro"
 
 export default function Reset() {
-  return <Global styles={reset} />
+  return (
+    <Global
+      styles={{
+        ":root": tw`text-text bg-background-2 font-body`,
+        "button, input, textarea, select": tw`rounded-none`,
+      }}
+    />
+  )
 }
