0f8e66ceaa06f5c67066b39f2fc0e90f0b23aa5b
nuei <61118778+thatnuei@users.noreply.github.com>
2020-03-21 16:55:08 -0500

    define a new set of tailwind-based helpers

---
+++ src/ui/helpers.ts
@@ -0,0 +1,8 @@
+import tw from "twin.macro"
+
+export const fixedCover = tw`fixed left-0 right-0 top-0 bottom-0`
+
+export const flexRow = tw`flex`
+export const flexColumn = tw`flex flex-col`
+
+export const centerItems = tw`items-center justify-center`
