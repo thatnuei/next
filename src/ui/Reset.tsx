3d1361f19599d70a2f821713777f9c98dd6654e4
nuei <61118778+thatnuei@users.noreply.github.com>
2020-03-22 21:58:16 -0500

    reset input alignment

--- src/ui/Reset.tsx
+++ src/ui/Reset.tsx
@@ -8,7 +8,7 @@ export default function Reset() {
     <Global
       styles={{
         ":root": tw`text-text bg-background-2 font-body`,
-        "button, input, textarea, select": tw`rounded-none`,
+        "button, input, textarea, select": tw`rounded-none text-left`,
       }}
     />
   )
