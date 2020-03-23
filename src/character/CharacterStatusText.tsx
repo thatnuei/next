cb5d911ae904cdf65192c3fec991f1c3850d48f2
nuei <61118778+thatnuei@users.noreply.github.com>
2020-03-22 23:20:16 -0500

    tw

--- src/character/CharacterStatusText.tsx
+++ src/character/CharacterStatusText.tsx
@@ -1,6 +1,6 @@
 import React from "react"
+import tw from "twin.macro"
 import { TagProps } from "../jsx/types"
-import { fontSize, italic } from "../ui/style"
 import { statusColors } from "./colors"
 import { CharacterStatus } from "./types"
 
@@ -11,7 +11,7 @@ type Props = TagProps<"p"> & {
 
 function CharacterStatusText({ status, statusMessage, ...props }: Props) {
   return (
-    <p css={[italic, fontSize("small")]} {...props}>
+    <p css={tw`italic text-sm`} {...props}>
       <span css={{ color: statusColors[status] }}>{status}</span>
       {statusMessage ? ` - ${statusMessage}` : undefined}
     </p>
