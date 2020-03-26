cdf85ad38ab8aff866c8f3f25665bce8034a900d
nuei <61118778+thatnuei@users.noreply.github.com>
2020-03-26 15:00:59 -0500

    install twin.macro from yalc

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,7 +1,1 @@
 /// <reference types="react-scripts" />
-
-declare module "twin.macro" {
-  import { CSSObject } from "@emotion/react"
-  const tw: (strings: TemplateStringsArray, ...values: never[]) => CSSObject
-  export default tw
-}
