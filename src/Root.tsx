18913112c0b56610565ecf3579ae64c2b9a69cc1
nuei <61118778+thatnuei@users.noreply.github.com>
2020-03-26 21:44:55 -0500

    remove some old theme stuff

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,21 +4,15 @@ import DevTools from "./app/DevTools"
 import FocusVisible from "./ui/FocusVisible"
 import Reset from "./ui/Reset"
 import { ThemeProvider } from "./ui/theme"
-import {
-  midnightTheme,
-  ThemeProvider as OldThemeProvider,
-} from "./ui/theme.old"
 
 export default function Root() {
   return (
     <React.StrictMode>
       <ThemeProvider>
-        <OldThemeProvider theme={midnightTheme}>
-          <App />
-          <Reset />
-          <FocusVisible />
-          {process.env.NODE_ENV === "development" && <DevTools />}
-        </OldThemeProvider>
+        <App />
+        <Reset />
+        <FocusVisible />
+        {process.env.NODE_ENV === "development" && <DevTools />}
       </ThemeProvider>
     </React.StrictMode>
   )
--- src/test/renderWithProviders.tsx
+++ src/test/renderWithProviders.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react"
 import React from "react"
-import { midnightTheme, ThemeProvider } from "../ui/theme.old"
+import { ThemeProvider } from "../ui/theme"
 
 export function renderWithProviders(children: React.ReactNode) {
-  return render(<ThemeProvider theme={midnightTheme}>{children}</ThemeProvider>)
+  return render(<ThemeProvider>{children}</ThemeProvider>)
 }
