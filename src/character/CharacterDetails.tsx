354b2495edd538343c08d58d112054053f77ae39
nuei <61118778+thatnuei@users.noreply.github.com>
2020-03-26 21:50:08 -0500

    CharacterDetails: render container + accept div props

--- src/character/CharacterDetails.tsx
+++ src/character/CharacterDetails.tsx
@@ -1,17 +1,18 @@
 import React from "react"
 import tw from "twin.macro"
+import { TagProps } from "../jsx/types"
 import { headerText2 } from "../ui/components"
 import Avatar from "./Avatar"
 import CharacterStatusText from "./CharacterStatusText"
 import { genderColors } from "./colors"
 import { Character } from "./types"
 
-type Props = { character: Character }
+type Props = TagProps<"div"> & { character: Character }
 
-function CharacterDetails({ character }: Props) {
+function CharacterDetails({ character, ...props }: Props) {
   const genderColor = { color: genderColors[character.gender] }
   return (
-    <>
+    <div {...props}>
       <h1 css={[headerText2, genderColor, tw`leading-none`]}>
         {character.name}
       </h1>
@@ -21,7 +22,7 @@ function CharacterDetails({ character }: Props) {
         statusMessage={character.statusMessage}
         css={tw`px-3 py-2 bg-background-1`}
       />
-    </>
+    </div>
   )
 }
 
