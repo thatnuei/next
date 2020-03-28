50f58cbbea465f4bdd4ee3ea7adfc6a0b49610f2
nuei <61118778+thatnuei@users.noreply.github.com>
2020-03-28 17:27:45 -0500

    configure mobx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
+import { configure } from "mobx"
 import React from "react"
 import ReactDOM from "react-dom"
 import Root from "./Root"
+
+configure({
+  reactionRequiresObservable: true,
+  // observableRequiresReaction: true,
+  // computedRequiresReaction: true,
+})
 
 ReactDOM.render(<Root />, document.getElementById("root"))
