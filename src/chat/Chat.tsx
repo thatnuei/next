09a4cebdae48d8da90c1df26c4dc06e2a391e7ea
nuei <61118778+thatnuei@users.noreply.github.com>
2020-03-29 00:12:22 -0500

    show chat menu button if no room

--- src/chat/Chat.tsx
+++ src/chat/Chat.tsx
@@ -5,6 +5,7 @@ import ChannelView from "../channel/ChannelView"
 import { useMediaQuery } from "../dom/useMediaQuery"
 import { fixedCover } from "../ui/helpers"
 import { screenQueries } from "../ui/screens"
+import ChatMenuButton from "./ChatMenuButton"
 import ChatNav from "./ChatNav"
 import { useChatContext } from "./context"
 
@@ -15,8 +16,11 @@ function Chat() {
   return (
     <div css={[fixedCover, tw`flex`]}>
       {!isSmallScreen && <ChatNav css={tw`mr-gap`} />}
-      {navStore.currentChannel && (
+      {navStore.currentChannel ? (
         <ChannelView css={tw`flex-1`} channel={navStore.currentChannel} />
+      ) : (
+        // TODO: proper "no room" default header
+        <ChatMenuButton />
       )}
     </div>
   )
