7255757d68caefe0c4ab03d917e9e07d924a7d6f
nuei <61118778+thatnuei@users.noreply.github.com>
2020-03-31 00:53:08 -0500

    custom tailwind font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -71,6 +71,19 @@ module.exports = {
       "px2": "2px",
       "gap": "4px", // represents the separating gap between sections of some layouts
     },
+
+    // TODO: change these names because they're not good
+    fontSize: {
+      "sm": "0.8rem",
+      "base": "1rem",
+      "lg": "1.125rem",
+      "xl": "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+    },
   },
   variants: {},
   plugins: [],
