2b54516139d1f6b6ed4b23a713b74b513d7587c9
nuei <61118778+thatnuei@users.noreply.github.com>
2020-04-02 23:41:20 -0500

    chat types

--- src/chat/types.ts
+++ src/chat/types.ts
@@ -1,3 +1,14 @@
+import { CharacterState } from "../character/types"
+import { Dict } from "../common/types"
+import { Dispatcher } from "../state/Dispatcher"
+import { ServerCommand } from "./commands"
+
+export type ChatState = {
+  characters: Dict<CharacterState>
+}
+
+export type ServerCommandDispatcher = Dispatcher<ServerCommand>
+
 export type ChatCredentials = {
   account: string
   ticket: string
