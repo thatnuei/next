30e381d3ff65d561aec4becf11296e97e5036992
nuei <61118778+thatnuei@users.noreply.github.com>
2020-04-03 01:31:59 -0500

    give some visibility to gray/black in bbc

--- src/bbc/BBC.tsx
+++ src/bbc/BBC.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from "react"
+import React, { CSSProperties, PropsWithChildren } from "react"
 import tw from "twin.macro"
 import { useChannelActions } from "../channel/actions"
 import Avatar from "../character/Avatar"
@@ -73,7 +73,7 @@ function BBCTree({ nodes }: { nodes: Node[] }) {
 
       case "color":
         return (
-          <span style={{ color: colors[node.value] || "inherit" }}>
+          <span style={colorStyles[node.value]}>
             <BBCTree nodes={node.children} />
           </span>
         )
@@ -195,17 +195,26 @@ function BBCChannelLink({
   )
 }
 
-const colors: { [color in string]?: string } = {
-  white: "rgb(236, 240, 241)",
-  black: "rgb(52, 73, 94)",
-  red: "rgb(236, 93, 93)",
-  blue: "rgb(52, 152, 219)",
-  yellow: "rgb(241, 196, 15)",
-  green: "rgb(46, 204, 113)",
-  pink: "rgb(255,164,156)",
-  gray: "rgb(149, 165, 166)",
-  orange: "rgb(230, 126, 34)",
-  purple: "rgb(201,135,228)",
-  brown: "rgb(211, 84, 0)",
-  cyan: "rgb(85, 175, 236)",
+// TODO: make this look better while still letting gray/black be visible
+const darkColorShadow = "0px 0px 2px rgba(255, 255,255, 0.8)"
+
+const colorStyles: { [color in string]?: CSSProperties } = {
+  white: { color: "rgb(236, 240, 241)" },
+  black: {
+    color: "rgb(52, 73, 94)",
+    textShadow: darkColorShadow,
+  },
+  gray: {
+    color: "rgb(149, 165, 166)",
+    textShadow: darkColorShadow,
+  },
+  red: { color: "rgb(236, 93, 93)" },
+  blue: { color: "rgb(52, 152, 219)" },
+  yellow: { color: "rgb(241, 196, 15)" },
+  green: { color: "rgb(46, 204, 113)" },
+  pink: { color: "rgb(255,164,156)" },
+  orange: { color: "rgb(230, 126, 34)" },
+  purple: { color: "rgb(201,135,228)" },
+  brown: { color: "rgb(211, 84, 0)" },
+  cyan: { color: "rgb(85, 175, 236)" },
 }
