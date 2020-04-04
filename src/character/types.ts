86c771e3f623baa5e71847601e724a12d0ff3b3a
nuei <61118778+thatnuei@users.noreply.github.com>
2020-04-04 08:51:44 -0500

    remove CharacterState type

--- src/character/types.ts
+++ src/character/types.ts
@@ -1,11 +1,4 @@
 import * as fchat from "fchat"
 
-export type CharacterState = {
-  name: string
-  gender: CharacterGender
-  status: CharacterStatus
-  statusMessage: string
-}
-
 export type CharacterStatus = fchat.Character.Status
 export type CharacterGender = fchat.Character.Gender
