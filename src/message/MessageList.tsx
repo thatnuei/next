3780500d311409e9fbade0c23518e0fc0933eca8
nuei <61118778+thatnuei@users.noreply.github.com>
2020-04-05 21:42:32 -0500

    message list bottom scroll

--- src/message/MessageList.tsx
+++ src/message/MessageList.tsx
@@ -1,19 +1,28 @@
-import { observer } from "mobx-react-lite"
-import React from "react"
+import { useObserver } from "mobx-react-lite"
+import React, { useCallback, useLayoutEffect, useState } from "react"
 import tw from "twin.macro"
 import { scrollVertical } from "../ui/helpers"
 import { MessageListModel } from "./message-list-model"
-import MessageListItem from "./MessageListItem"
 import { MessageModel } from "./message-model"
+import MessageListItem from "./MessageListItem"
 
 type Props = {
   list: MessageListModel
   filter?: (message: MessageModel) => boolean
 }
 
+const scrolledToBottomThreshold = 100
+
 function MessageList({ list, filter: shouldShow = () => true }: Props) {
-  return (
-    <ol css={[tw`w-full h-full`, scrollVertical]}>
+  const [container, setContainer] = useState<HTMLElement | null>()
+
+  const lastMessage = useObserver(() => {
+    return list.messages.length > 0 && list.messages[list.messages.length - 1]
+  })
+  useBottomScroll(container, lastMessage)
+
+  return useObserver(() => (
+    <ol css={[tw`w-full h-full`, scrollVertical]} ref={setContainer}>
       {list.messages.map((message) =>
         shouldShow(message) ? (
           <li key={message.key}>
@@ -22,7 +31,30 @@ function MessageList({ list, filter: shouldShow = () => true }: Props) {
         ) : null,
       )}
     </ol>
-  )
+  ))
 }
 
-export default observer(MessageList)
+export default MessageList
+
+function useBottomScroll(
+  container: HTMLElement | null | undefined,
+  watchedValue: unknown,
+) {
+  const wasScrolledToBottom =
+    container &&
+    container.scrollHeight >=
+      container.scrollHeight -
+        container.clientHeight -
+        scrolledToBottomThreshold
+
+  const scrollToBottom = useCallback(() => {
+    if (!container) return
+    container.scrollTop = container.scrollHeight
+  }, [container])
+
+  useLayoutEffect(() => {
+    if (wasScrolledToBottom) scrollToBottom()
+  }, [scrollToBottom, wasScrolledToBottom, watchedValue])
+
+  useLayoutEffect(scrollToBottom, [container])
+}
