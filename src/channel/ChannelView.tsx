4792187b5fed5909624b3ab211ffafc7d1246603
nuei <61118778+thatnuei@users.noreply.github.com>
2020-04-05 22:36:27 -0500

    channel description dialog

--- src/channel/ChannelView.tsx
+++ src/channel/ChannelView.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react-lite"
-import React from "react"
+import React, { useMemo } from "react"
 import tw from "twin.macro"
+import BBC from "../bbc/BBC"
 import ChatInput from "../chat/ChatInput"
 import { useChatStream } from "../chat/streamContext"
 import ChatMenuButton from "../chatNav/ChatMenuButton"
@@ -9,8 +10,11 @@ import { useMediaQuery } from "../dom/useMediaQuery"
 import { TagProps } from "../jsx/types"
 import MessageList from "../message/MessageList"
 import { fadedButton, headerText2 } from "../ui/components"
+import { scrollVertical } from "../ui/helpers"
 import Icon from "../ui/Icon"
-import { users } from "../ui/icons"
+import { earth, users } from "../ui/icons"
+import Modal from "../ui/Modal"
+import { OverlayModel } from "../ui/OverlayModel"
 import { screenQueries } from "../ui/screens"
 import ChannelFilters from "./ChannelFilters"
 import ChannelUserList from "./ChannelUserList"
@@ -23,13 +27,27 @@ type Props = {
 function ChannelView({ channel, ...props }: Props) {
   const stream = useChatStream()
   const isLargeScreen = useMediaQuery(screenQueries.large)
+  const descriptionOverlay = useMemo(() => new OverlayModel(), [])
 
   return (
     <div css={tw`flex flex-col`} {...props}>
       <header css={tw`flex flex-row items-center p-3 bg-background-0`}>
         <ChatMenuButton css={tw`mr-3`} />
 
-        <h1 css={[headerText2, tw`flex-1`]}>{channel.title}</h1>
+        <div css={tw`flex flex-col flex-1`}>
+          <h1 css={[headerText2, tw`flex-1`]}>{channel.title}</h1>
+          <div css={tw`flex flex-row`}>
+            <Button
+              css={fadedButton}
+              tw="flex flex-row"
+              onClick={descriptionOverlay.toggle}
+            >
+              <Icon which={earth} />
+              <span css={tw`ml-1`}>Description</span>
+            </Button>
+          </div>
+        </div>
+
         <ChannelFilters
           selectedMode={channel.selectedMode}
           onModeChange={channel.setSelectedMode}
@@ -43,11 +61,25 @@ function ChannelView({ channel, ...props }: Props) {
       </header>
 
       <div css={tw`flex flex-row flex-1 min-h-0 my-gap`}>
-        <main css={tw`flex-1 bg-background-1`}>
+        <main css={tw`relative flex-1 bg-background-1`}>
           <MessageList
             list={channel.messageList}
             filter={(it) => channel.shouldShowMessage(it.type)}
           />
+          <Modal
+            title="Description"
+            width="100%"
+            height="max(60%, 400px)"
+            model={descriptionOverlay}
+            fillMode="absolute"
+            verticalPanelAlign="top"
+          >
+            <div css={[tw`w-full h-full`, scrollVertical]}>
+              <p css={tw`p-4`}>
+                <BBC text={channel.description} />
+              </p>
+            </div>
+          </Modal>
         </main>
 
         {isLargeScreen && (
