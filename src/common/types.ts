6e023debc16a14bb93d673c2387a9f4833fdc11f
nuei <61118778+thatnuei@users.noreply.github.com>
2020-04-10 14:07:52 -0500

    Dict: default key to string

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,4 +1,6 @@
-export type Dict<T, K extends string | number | symbol> = { [_ in K]?: T }
+export type Dict<T, K extends string | number | symbol = string> = {
+  [_ in K]?: T
+}
 
 export type ValueOf<T> = T extends readonly (infer V)[] ? V : T[keyof T]
 
