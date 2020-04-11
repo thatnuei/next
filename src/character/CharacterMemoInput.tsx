204c3d04928b583d888bf30f13532221f8aab541
nuei <61118778+thatnuei@users.noreply.github.com>
2020-04-11 12:35:32 -0500

    clean up memo input logic

--- src/character/CharacterMemoInput.tsx
+++ src/character/CharacterMemoInput.tsx
@@ -1,95 +1,73 @@
-import React, { useEffect, useMemo, useState } from "react"
-import { merge, Observable, of, Subject } from "rxjs"
-import {
-  catchError,
-  debounceTime,
-  filter,
-  map,
-  scan,
-  switchMap,
-} from "rxjs/operators"
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react"
 import tw from "twin.macro"
+import { debounce } from "../common/debounce"
 import Button from "../dom/Button"
 import { useApiContext } from "../flist/api-context"
 import { TagProps } from "../jsx/types"
+import { createTask, Task } from "../state/task"
 import { input, solidButton } from "../ui/components"
 
-function useObservable<T>(observable: Observable<T>, initialValue: T): T {
-  const [value, setValue] = useState(initialValue)
+type Props = {
+  name: string
+} & TagProps<"textarea">
 
-  useEffect(() => {
-    const sub = observable.subscribe(setValue)
-    return () => sub.unsubscribe()
-  }, [observable])
+type Status = "loading" | "editing" | "error"
 
-  return value
-}
-
-export default function CharacterMemoInput({
-  name,
-  ...props
-}: { name: string } & TagProps<"textarea">) {
+export default function CharacterMemoInput({ name, ...props }: Props) {
   const api = useApiContext()
 
-  type Action =
-    | { type: "loadMemo"; name: string }
-    | { type: "setMemo"; name: string; note: string }
+  const [status, setStatus] = useState<Status>("loading")
+  const [memo, setMemo] = useState("")
 
-  type State =
-    | { status: "loading" }
-    | { status: "editing"; note: string }
-    | { status: "error" }
+  const taskRef = useRef<Task>()
 
-  const actionSubject = useMemo(() => new Subject<Action>(), [])
+  const loadMemo = useCallback(
+    (name: string) => {
+      taskRef.current?.cancel()
 
-  const loadMemoObservable = useMemo(
-    () =>
-      actionSubject.pipe(
-        filter(
-          (action): action is { type: "loadMemo"; name: string } =>
-            action.type === "loadMemo",
-        ),
-        switchMap((action) =>
-          merge(
-            of<State>({ status: "loading" }),
-            of(action.name).pipe(
-              switchMap((name) => api.getMemo({ name })),
-              map((note): State => ({ status: "editing", note })),
-            ),
-          ),
-        ),
-        catchError(() => of<State>({ status: "error" })),
-      ),
-    [actionSubject, api],
+      const task = (taskRef.current = createTask({
+        run: () => api.getMemo({ name }),
+        onStart() {
+          setStatus("loading")
+        },
+        onComplete(memo) {
+          setMemo(memo)
+          setStatus("editing")
+        },
+        onError() {
+          setStatus("error")
+        },
+        onCancelled() {
+          setStatus("loading")
+        },
+      }))
+
+      task.run()
+    },
+    [api],
   )
 
-  const state = useObservable(loadMemoObservable, { status: "loading" })
-
-  type MemoData = { name: string; note: string }
-  const memoInput$ = useMemo(() => new Subject<MemoData>(), [])
-  useEffect(() => {
-    const debouncedEntries = memoInput$.pipe(debounceTime(2000))
-
-    const entriesWhereNameChanged = memoInput$.pipe(
-      scan<MemoData, MemoData[]>((prev, next) => [...prev, next].slice(-2), []),
-      filter(([prev, next]) => next && prev && next.name !== prev.name),
-      map((entry) => entry[0]),
-    )
-
-    const sub = merge(debouncedEntries, entriesWhereNameChanged).subscribe(
-      api.setMemo,
-    )
-
-    return () => sub.unsubscribe()
-  }, [api.setMemo, memoInput$])
+  const saveMemo = useMemo(
+    () => debounce(2000, (note: string) => api.setMemo({ name, note })),
+    [api, name],
+  )
 
   useEffect(() => {
-    actionSubject.next({ type: "loadMemo", name })
-  }, [actionSubject, name])
+    loadMemo(name)
+  }, [loadMemo, name])
+
+  function handleChange(newMemo: string) {
+    setMemo(newMemo)
+    saveMemo(newMemo)
+  }
+
+  function retry() {
+    loadMemo(name)
+  }
 
   const style = [input, tw`h-20 text-sm`]
 
-  if (state.status === "loading") {
+  if (status === "loading") {
     return (
       <textarea
         css={[style, tw`italic`]}
@@ -100,29 +78,23 @@ export default function CharacterMemoInput({
     )
   }
 
-  if (state.status === "editing") {
+  if (status === "editing") {
     return (
       <textarea
-        css={[style, state.note === "" && tw`italic`]}
-        value={state.note}
-        onChange={(event) => {
-          // setState({ ...state, note: event.target.value })
-          memoInput$.next({ name, note: event.target.value })
-        }}
+        css={[style, memo === "" && tw`italic`]}
+        value={memo}
+        onChange={(event) => handleChange(event.target.value)}
         placeholder="Enter a memo"
         {...props}
       />
     )
   }
 
-  if (state.status === "error") {
+  if (status === "error") {
     return (
       <div css={tw`text-sm`}>
         <div css={tw`mb-1`}>Failed to load memo</div>
-        <Button
-          css={[solidButton, tw`px-2 py-1 text-sm`]}
-          onClick={() => actionSubject.next({ type: "loadMemo", name })}
-        >
+        <Button css={[solidButton, tw`px-2 py-1 text-sm`]} onClick={retry}>
           Try again
         </Button>
       </div>
