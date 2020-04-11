31251ff12b2bd6279634854541a9a6d3bd0afdd8
nuei <61118778+thatnuei@users.noreply.github.com>
2020-04-11 12:29:52 -0500

    debounce

---
+++ src/common/debounce.ts
@@ -0,0 +1,14 @@
+export function debounce<A extends unknown[]>(
+  time: number,
+  fn: (...args: A) => void,
+) {
+  let timeout: ReturnType<typeof setTimeout>
+
+  return (...args: A) => {
+    if (timeout) clearTimeout(timeout)
+
+    timeout = setTimeout(() => {
+      fn(...args)
+    }, time)
+  }
+}
