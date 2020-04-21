030a29934e5de40885b5a7431d69ed4b637a155a
nuei <61118778+thatnuei@users.noreply.github.com>
2020-04-20 22:52:45 -0500

    configure mobx-react-lite batched imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { configure } from "mobx"
+import "mobx-react-lite/batchingForReactDom"
 import React from "react"
 import ReactDOM from "react-dom"
 import Root from "./Root"
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom/extend-expect"
 import "fake-indexeddb/auto"
+import "mobx-react-lite/batchingForReactDom"
 import { raise } from "./common/raise"
 
 // make sure we don't do any actual API requests
