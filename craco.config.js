d2f8d1fe14da88c9783ed12602f5e125d72a2e2e
nuei <61118778+thatnuei@users.noreply.github.com>
2020-05-25 15:19:54 -0500

    fix / clean up craco config

--- craco.config.js
+++ craco.config.js
@@ -1,60 +1,77 @@
+const { whenDev, whenProd } = require("@craco/craco")
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin")
 
 // TODO: make these plugins so this doesn't look as terrifying
 module.exports = {
-  babel: {
-    presets: [
-      [
-        "@emotion/babel-preset-css-prop",
-        {
-          // enabling autoLabel causes issues with css prop overrides
-          autoLabel: "never",
-        },
-      ],
-    ],
-  },
-  webpack: {
-    configure(config, { env }) {
-      if (env === "development") addReactRefresh(config)
+  plugins: [
+    emotionCssPropPlugin(),
+    ...whenDev(() => [reactRefreshPlugin()], []),
+    ...whenProd(() => [preactAliasPlugin()], []),
+  ],
+}
 
-      if (env === "production") {
-        // react-refresh doesn't work with preact
-        config.resolve = {
-          ...config.resolve,
+function emotionCssPropPlugin() {
+  return {
+    plugin: {
+      overrideCracoConfig({ cracoConfig }) {
+        cracoConfig.babel = cracoConfig.babel || {}
+        cracoConfig.babel.presets = cracoConfig.babel.presets || []
+        cracoConfig.babel.presets.push([
+          "@emotion/css-prop",
+          // autoLabel breaks css prop overrides
+          { autoLabel: "never" },
+        ])
+        return cracoConfig
+      },
+    },
+  }
+}
+
+function reactRefreshPlugin() {
+  return {
+    plugin: {
+      overrideWebpackConfig({ webpackConfig }) {
+        webpackConfig.plugins.push(new ReactRefreshWebpackPlugin())
+
+        // the refresh babel plugin crashes the build if it transpiles node_modules,
+        // presumably because libs from node_modules run before
+        // the refresh helper functions actually get defined by the webpack plugin?
+        //
+        // either way, our workaround is to explicitly add it as a loader here,
+        // skipping over node_modules completely
+        webpackConfig.module.rules.unshift({
+          test: /\.(js|ts)x?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: require.resolve("babel-loader"),
+            options: {
+              babelrc: false,
+              configFile: false,
+              plugins: [require.resolve("react-refresh/babel")],
+            },
+          },
+        })
+
+        return webpackConfig
+      },
+    },
+  }
+}
+
+function preactAliasPlugin() {
+  return {
+    plugin: {
+      overrideWebpackConfig({ webpackConfig }) {
+        webpackConfig.resolve = {
+          ...webpackConfig.resolve,
           alias: {
-            ...config.resolve.alias,
+            ...webpackConfig.resolve.alias,
             "react": "preact/compat",
             "react-dom": "preact/compat",
           },
         }
-      }
-
-      return config
-    },
-  },
-}
-
-function addReactRefresh(config) {
-  config.plugins.push(
-    new ReactRefreshWebpackPlugin({ disableRefreshCheck: true }),
-  )
-
-  // the refresh babel plugin crashes the build if it transpiles node_modules,
-  // presumably because libs from node_modules run before
-  // the refresh helper functions actually get defined by the webpack plugin?
-  //
-  // either way, our workaround is to explicitly add it as a loader here,
-  // skipping over node_modules completely
-  config.module.rules.unshift({
-    test: /\.(js|ts)x?$/,
-    exclude: /node_modules/,
-    use: {
-      loader: require.resolve("babel-loader"),
-      options: {
-        babelrc: false,
-        configFile: false,
-        plugins: [require.resolve("react-refresh/babel")],
+        return webpackConfig
       },
     },
-  })
+  }
 }
