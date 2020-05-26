55b7ec69720851b9ad57a5a196d2a5e8d06973b4
nuei <61118778+thatnuei@users.noreply.github.com>
2020-05-25 20:13:53 -0500

    comment out extra helpers from recoil
    they aren't exported in the actual lib yet *sighs in facebook*

--- types/recoil/index.d.ts
+++ types/recoil/index.d.ts
@@ -4,30 +4,58 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // Minimum TypeScript Version: 3.7
 
+import {
+  RecoilState,
+  RecoilValue,
+  RecoilValueReadOnly,
+} from "./lib/core/recoilValue"
+
 // Nominal Classes
-export { DefaultValue } from './lib/core/node';
-
-// Components
-export { RecoilRoot } from './lib/core/RecoilRoot';
-
 // Recoil state
-export { atom } from './lib/core/atom';
-export { selector } from './lib/core/selector';
-
+export { atom } from "./lib/core/atom"
 // Hooks
 export {
-    useRecoilValue,
-    useRecoilValueLoadable,
-    useRecoilState,
-    useRecoilStateLoadable,
-    useSetRecoilState,
-    useResetRecoilState,
-    useRecoilCallback,
-} from './lib/core/hooks';
-
-// Other
-export { isRecoilValue } from './lib/core/recoilValue';
-
+  useRecoilCallback,
+  useRecoilState,
+  useRecoilStateLoadable,
+  useRecoilValue,
+  useRecoilValueLoadable,
+  useResetRecoilState,
+  useSetRecoilState,
+} from "./lib/core/hooks"
 // Types
-export { Loadable } from './lib/core/loadable';
-export { RecoilValue, RecoilValueReadOnly, RecoilState } from './lib/core/recoilValue';
+export { Loadable } from "./lib/core/loadable"
+export { DefaultValue } from "./lib/core/node"
+// Components
+export { RecoilRoot } from "./lib/core/RecoilRoot"
+// Other
+export { isRecoilValue } from "./lib/core/recoilValue"
+export { selector } from "./lib/core/selector"
+export { RecoilValue, RecoilValueReadOnly, RecoilState }
+
+// custom stuff
+// uncomment later when it's actually added in recoil
+// https://github.com/facebookexperimental/Recoil/issues/105
+// export type GetRecoilValue = <T>(value: RecoilValue<T>) => T
+
+// export function atomFamily<T, Parameter>(options: {
+//   key: string
+
+//   default:
+//     | RecoilValue<T>
+//     | Promise<T>
+//     | T
+//     | ((param: Parameter) => T | RecoilValue<T> | Promise<T>)
+
+//   dangerouslyAllowMutability?: boolean
+// }): (param: Parameter) => RecoilState<T>
+
+// export function selectorFamily<T, Parameter>(options: {
+//   key: string
+
+//   get: (
+//     param: Parameter,
+//   ) => (options: { get: GetRecoilValue }) => Promise<T> | RecoilValue<T> | T
+
+//   dangerouslyAllowMutability?: boolean
+// }): (param: Parameter) => RecoilValueReadOnly<T>
