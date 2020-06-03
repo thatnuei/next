4ba72381f7c2a72c87cb977e554956fffba8043e
nuei <61118778+thatnuei@users.noreply.github.com>
2020-06-03 09:52:56 -0500

    remove useRecoilSet helper
    it's apparently far more efficient to do multiple set calls in a callback

--- src/privateChat/listeners.ts
+++ src/privateChat/listeners.ts
@@ -1,9 +1,9 @@
 import { union } from "lodash/fp"
-import { useRecoilValue, useSetRecoilState } from "recoil"
+import { useRecoilCallback, useRecoilValue, useSetRecoilState } from "recoil"
 import { useChatCredentials } from "../chat/credentialsContext"
 import { chatNavViewAtom } from "../chatNav/state"
 import { createPrivateMessage } from "../message/MessageState"
-import { useRecoilSet } from "../recoil/helpers"
+import { runCommand } from "../socket/commandHelpers"
 import { useSocketListener } from "../socket/socketContext"
 import {
   openPrivateChatPartnersAtom,
@@ -16,38 +16,44 @@ import { restorePrivateChats } from "./storage"
 export function usePrivateChatListeners() {
   const { identity } = useChatCredentials()
   const setOpenChats = useSetRecoilState(openPrivateChatPartnersAtom)
-  const set = useRecoilSet()
   const navView = useRecoilValue(chatNavViewAtom)
   const openChat = useOpenPrivateChatAction()
 
-  useSocketListener({
-    IDN() {
-      restorePrivateChats(identity)
-        .then(setOpenChats)
-        .catch((error) => {
-          console.error("Could not restore private chats", error)
-        })
-    },
-    PRI({ character, message }) {
-      openChat(character)
+  useSocketListener(
+    useRecoilCallback(({ set }, command) => {
+      return runCommand(command, {
+        IDN() {
+          restorePrivateChats(identity)
+            .then(setOpenChats)
+            .catch((error) => {
+              console.error("Could not restore private chats", error)
+            })
+        },
+        PRI({ character, message }) {
+          openChat(character)
 
-      set(
-        privateChatMessagesAtom(character),
-        union([createPrivateMessage(character, message)]),
-      )
+          set(
+            privateChatMessagesAtom(character),
+            union([createPrivateMessage(character, message)]),
+          )
 
-      const isCurrent =
-        navView?.type === "privateChat" && navView.partnerName === character
+          const isCurrent =
+            navView?.type === "privateChat" && navView.partnerName === character
 
-      if (!isCurrent) {
-        set(privateChatAtom(character), (prev) => ({ ...prev, isUnread: true }))
-      }
-    },
-    TPN({ character, status }) {
-      set(privateChatAtom(character), (prev) => ({
-        ...prev,
-        typingStatus: status,
-      }))
-    },
-  })
+          if (!isCurrent) {
+            set(privateChatAtom(character), (prev) => ({
+              ...prev,
+              isUnread: true,
+            }))
+          }
+        },
+        TPN({ character, status }) {
+          set(privateChatAtom(character), (prev) => ({
+            ...prev,
+            typingStatus: status,
+          }))
+        },
+      })
+    }),
+  )
 }
