e4c00ee872eaf61f612c7a7ebb1ed03eed2756dc
nuei <61118778+thatnuei@users.noreply.github.com>
2020-07-22 07:59:12 -0500

    less nesting/files

--- src/chatNav/ChatNav.tsx
+++ src/chatNav/ChatNav.tsx
@@ -1,16 +1,43 @@
+import { useObservable } from "micro-observables"
 import React from "react"
 import tw from "twin.macro"
 import CharacterSummary from "../character/CharacterSummary"
-import { useChatCredentials } from "../chat/helpers"
-import { TagProps } from "../jsx/types"
-import ChatNavActions from "./ChatNavActions"
+import Button from "../dom/Button"
+import { ComponentProps, TagProps } from "../jsx/types"
+import { useRootStore } from "../root/context"
+import { fadedButton } from "../ui/components"
+import Icon, { IconProps } from "../ui/Icon"
+import * as icons from "../ui/icons"
 import RoomTabList from "./RoomTabList"
 
-function ChatNav(props: TagProps<"nav">) {
-  const { identity } = useChatCredentials()
+export default function ChatNav(props: TagProps<"nav">) {
+  const root = useRootStore()
+  const identity = useObservable(root.appStore.identity)
   return (
     <nav css={tw`flex`} {...props}>
-      <ChatNavActions />
+      <div css={tw`flex flex-col`}>
+        <NavAction
+          icon={icons.list}
+          title="Browse channels"
+          onClick={root.channelBrowserStore.show}
+        />
+        <NavAction
+          icon={icons.updateStatus}
+          title="Update your status"
+          onClick={root.statusUpdateStore.show}
+        />
+        <NavAction
+          icon={icons.users}
+          title="See online friends and bookmarks"
+        />
+        <NavAction icon={icons.about} title="About next" />
+        <div css={tw`flex-1`} />
+        <NavAction
+          icon={icons.logout}
+          title="Log out"
+          onClick={root.appStore.leaveChat}
+        />
+      </div>
       <div css={tw`flex flex-col w-56 overflow-y-auto bg-background-1`}>
         <CharacterSummary
           name={identity}
@@ -24,4 +51,14 @@ function ChatNav(props: TagProps<"nav">) {
   )
 }
 
-export default ChatNav
+type NavActionProps = ComponentProps<typeof Button> & {
+  icon: IconProps["which"]
+}
+
+function NavAction({ icon, ...props }: NavActionProps) {
+  return (
+    <Button css={[fadedButton, tw`block p-3`]} {...props}>
+      <Icon which={icon} css={tw`w-5 h-5`} />
+    </Button>
+  )
+}
