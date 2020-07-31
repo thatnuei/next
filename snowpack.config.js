c7eb00c3f39444daaeecfcb5aedc7b3a5e1e4c93
nuei <61118778+thatnuei@users.noreply.github.com>
2020-07-30 22:56:17 -0500

    simplified snowpack config

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   plugins: ["@snowpack/plugin-babel"],
-  scripts: {
-    "mount:web_modules": "mount $WEB_MODULES --to /web_modules",
-    "mount:src": "mount src",
-    "mount:public": "mount public --to /",
-    "mount:tailwind": "mount node_modules/tailwindcss/dist",
-    "build:.ts,.tsx": "@snowpack/plugin-babel",
+  mount: {
+    "src": "/src",
+    "public": "/",
+    "node_modules/tailwindcss/dist": "/node_modules/tailwindcss/dist",
   },
   exclude: ["**/*.test.@(ts|tsx)", "**/src/test/**/*", "**/src/setupTests.ts"],
   devOptions: {
