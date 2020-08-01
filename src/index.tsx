9c6d29fa34b66066cc0cbb5a4ac122214ed834cd
nuei <61118778+thatnuei@users.noreply.github.com>
2020-07-31 21:18:19 -0500

    remove commented HMR code

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,3 @@ import ReactDOM from "react-dom"
 import Root from "./root/Root"
 
 ReactDOM.render(<Root />, document.getElementById("root"))
-
-// if (module.hot) {
-//   module.hot.accept("./Root", () => {
-//     ReactDOM.render(<Root />, document.getElementById("root"))
-//   })
-// }
