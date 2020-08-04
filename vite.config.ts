6e9bdc7d6481b783ea023cf952414174135edf43
nuei <61118778+thatnuei@users.noreply.github.com>
2020-08-04 01:22:12 -0500

    add class properties plugin

--- vite.config.ts
+++ vite.config.ts
@@ -47,7 +47,10 @@ const config: UserConfig = {
         "@babel/preset-typescript",
         ["@emotion/babel-preset-css-prop", { autoLabel: "never" }], // autoLabel breaks css prop overrides
       ],
-      plugins: ["babel-plugin-macros"],
+      plugins: [
+        "@babel/plugin-proposal-class-properties",
+        "babel-plugin-macros",
+      ],
     }),
   ],
 }
