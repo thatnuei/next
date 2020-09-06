718e1d06ff5191e5a15dfd043718d654e556eef8
nuei <61118778+thatnuei@users.noreply.github.com>
2020-09-06 14:48:46 -0500

    set up flistFetch and basic login

---
+++ modules/flist.ts
@@ -0,0 +1,29 @@
+export async function flistFetch<T>(
+	endpoint: string,
+	data: Record<string, string | number>,
+) {
+	const body = new FormData()
+	for (const [key, value] of Object.entries(data)) {
+		body.set(key, String(value))
+	}
+
+	const response = await fetch(
+		`https://www.f-list.net/${endpoint.replace(/^\/+/, "")}`,
+		{ method: "post", body },
+	)
+
+	if (!response.ok) {
+		throw new Error(response.statusText)
+	}
+
+	const {
+		error,
+		...responseData
+	}: T & { error?: string } = await response.json()
+
+	if (error) {
+		throw new Error(error)
+	}
+
+	return responseData
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,42 @@
+import { useState } from "react"
+import { flistFetch } from "../modules/flist"
+
 export default function Index() {
-	return <p className="text-lg text-blue-400">hi</p>
+	const [account, setAccount] = useState("")
+	const [password, setPassword] = useState("")
+
+	function handleSubmit(event: React.FormEvent) {
+		event.preventDefault()
+
+		flistFetch(`/json/getApiTicket.php`, { account, password })
+			.then(console.log)
+			.catch(console.error)
+	}
+
+	return (
+		<main>
+			<form onSubmit={handleSubmit}>
+				<label>
+					<div>Username</div>
+					<input
+						className="border"
+						value={account}
+						onChange={(e) => setAccount(e.target.value)}
+					/>
+				</label>
+				<label>
+					<div>Password</div>
+					<input
+						className="border"
+						type="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</label>
+				<div>
+					<button type="submit">Submit</button>
+				</div>
+			</form>
+		</main>
+	)
 }
