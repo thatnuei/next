2cc0df9d5663b56a6cba1e0b3375738796318bb3
nuei <61118778+thatnuei@users.noreply.github.com>
2020-09-07 10:09:31 -0500

    add title and icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import { AppProps } from "next/app"
+import Head from "next/head"
 import "../styles/index.css"
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+				<title>next</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
