6934248d9821dcbc77f1ededc141dba641938070
nuei <61118778+thatnuei@users.noreply.github.com>
2020-09-13 03:42:18 -0500

    preact alias in production because why not

--- poi.config.js
+++ poi.config.js
@@ -1,4 +1,16 @@
 module.exports = {
 	devServer: { port: 8080 },
 	reactRefresh: true,
+
+	configureWebpack:
+		process.env.NODE_ENV === "production"
+			? {
+					resolve: {
+						alias: {
+							react: "preact/compat",
+							"react-dom": "preact/compat",
+						},
+					},
+			  }
+			: undefined,
 }
