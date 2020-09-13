0779a3c05fdca1570865130e7d5deee512f4f79f
nuei <61118778+thatnuei@users.noreply.github.com>
2020-09-13 14:33:17 -0500

    note on button

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,5 +1,6 @@
 import { ComponentPropsWithoutRef } from "react"
 
+// TODO: add loading prop to show a loading spinner
 export default function Button(props: ComponentPropsWithoutRef<"button">) {
 	return <button type="button" {...props} />
 }
