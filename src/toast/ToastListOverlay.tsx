40b67afb17ff41462f1d29938d3da36f59b4d925
nuei <61118778+thatnuei@users.noreply.github.com>
2020-09-13 21:55:40 -0500

    toast transitions

--- src/toast/ToastListOverlay.tsx
+++ src/toast/ToastListOverlay.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion"
 import Button from "../ui/Button"
 import { alertIcon, closeCircleIcon } from "../ui/icons"
 import { useToastContext } from "./ToastContext"
@@ -7,23 +8,29 @@ export default function ToastListOverlay() {
 
 	return (
 		<div className="fixed inset-x-0 top-0 flex flex-col items-center p-4 space-y-4">
-			{toasts.map(({ key, text }) => (
-				<div
-					key={key}
-					className="flex items-center max-w-sm p-2 text-white bg-red-800 shadow"
-					role="alert"
-				>
-					<div>{alertIcon}</div>
-					<div className="ml-2">{text}</div>
-					<Button
-						className="ml-3"
-						aria-label="Dismiss"
-						onClick={() => removeToast(key)}
+			<AnimatePresence>
+				{toasts.map(({ key, text }) => (
+					<motion.div
+						key={key}
+						className="flex items-center max-w-sm p-2 text-white bg-red-800 shadow"
+						role="alert"
+						initial={{ opacity: 0, scale: 0.9 }}
+						animate={{ opacity: 1, scale: 1 }}
+						exit={{ opacity: 0, scale: 0.9 }}
+						transition={{ duration: 0.25, ease: "easeOut" }}
 					>
-						{closeCircleIcon}
-					</Button>
-				</div>
-			))}
+						<div>{alertIcon}</div>
+						<div className="ml-2">{text}</div>
+						<Button
+							className="ml-3"
+							aria-label="Dismiss"
+							onClick={() => removeToast(key)}
+						>
+							{closeCircleIcon}
+						</Button>
+					</motion.div>
+				))}
+			</AnimatePresence>
 		</div>
 	)
 }
