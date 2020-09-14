9d0da3a71d87493a2ba44b54720bd5e1b37ef4dc
nuei <61118778+thatnuei@users.noreply.github.com>
2020-09-14 14:04:19 -0500

    simplify tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,18 @@
 // @ts-check
-const { rgb, rgba, shade } = require("polished")
+const { rgb, shade } = require("polished")
 
 // https://flatuicolors.com/palette/defo
 const midnight = rgb(38, 65, 94)
-const clouds = rgb(236, 240, 241)
-const emerald = rgb(46, 204, 113)
-const tomato = rgb(231, 76, 60)
-const sunflower = rgb(241, 196, 15)
-const carrot = rgb(230, 126, 34)
-const river = rgb(52, 152, 219)
 
 module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				"midnight-0": midnight,
-				"midnight-1": shade(0.3, midnight),
-				"midnight-2": shade(0.5, midnight),
-
-				white: clouds,
-				"black-faded": rgba(0, 0, 0, 0.5),
+				midnight: {
+					0: midnight,
+					1: shade(0.3, midnight),
+					2: shade(0.5, midnight),
+				},
 			},
 		},
 		fontFamily: {
