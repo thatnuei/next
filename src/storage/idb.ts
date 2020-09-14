f3c4b692c5ecf328585d62142e149e988526fcf6
nuei <61118778+thatnuei@users.noreply.github.com>
2020-09-13 19:33:13 -0500

    simplify idb thing
    since we're not server rendering, no need for window check

--- src/storage/idb.ts
+++ src/storage/idb.ts
@@ -1,21 +1,12 @@
-function importIdb() {
-	if (typeof window !== "undefined") {
-		return import("idb-keyval")
-	}
-}
+import * as idb from "idb-keyval"
 
 export function createIdbStorage<T>(key: string) {
 	return {
-		get: async () => {
-			const idb = await importIdb()
-			return idb?.get<T | undefined>(key)
-		},
+		get: () => idb?.get<T | undefined>(key),
 		set: async (value: T) => {
-			const idb = await importIdb()
 			await idb?.set(key, value)
 		},
 		del: async () => {
-			const idb = await importIdb()
 			await idb?.del(key)
 		},
 	}
