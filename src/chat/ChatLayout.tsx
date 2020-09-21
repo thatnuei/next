0bdf0c534b04a4c38da372614336adc5debfd825
nuei <61118778+thatnuei@users.noreply.github.com>
2020-09-21 02:10:00 -0500

    quick and dirty side drawer

--- src/chat/ChatLayout.tsx
+++ src/chat/ChatLayout.tsx
@@ -1,8 +1,12 @@
+import { AnimatePresence, motion } from "framer-motion"
+import { useState } from "react"
 import { range } from "../helpers/range"
 import { MenuIcon, MoreIcon } from "../ui/icons"
 import { Chatbox } from "./Chatbox"
 
 export default function ChatLayout() {
+	const [menu, setMenu] = useState(false)
+
 	return (
 		<main className="absolute inset-0 flex flex-col">
 			<section className="flex items-center px-3 py-2 space-x-3 bg-midnight-0">
@@ -22,6 +26,7 @@ export default function ChatLayout() {
 					type="button"
 					className="transition-opacity duration-300 opacity-50 active:opacity-100 active:transition-none"
 					aria-label="More"
+					onClick={() => setMenu(true)}
 				>
 					<MoreIcon className="size-5" />
 				</button>
@@ -47,6 +52,31 @@ export default function ChatLayout() {
 			</ul>
 
 			<Chatbox onSubmit={console.log} />
+
+			<AnimatePresence>
+				{menu && (
+					<motion.div
+						className="fixed inset-0 bg-black bg-opacity-50"
+						onClick={(e) => {
+							if (e.target === e.currentTarget) setMenu(false)
+						}}
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition={{ duration: 0.3, ease: "easeOut" }}
+					>
+						<motion.div
+							className="fixed inset-y-0 right-0"
+							initial={{ translateX: 100 }}
+							animate={{ translateX: 0 }}
+							exit={{ translateX: 100 }}
+							transition={{ duration: 0.3, ease: "easeOut" }}
+						>
+							<section className="w-64 h-full bg-midnight-0">nani</section>
+						</motion.div>
+					</motion.div>
+				)}
+			</AnimatePresence>
 		</main>
 	)
 }
