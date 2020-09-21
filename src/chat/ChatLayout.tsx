4d2bded4095d71fcb8678fe1b373100978069034
nuei <61118778+thatnuei@users.noreply.github.com>
2020-09-21 01:51:32 -0500

    split out chatbox

--- src/chat/ChatLayout.tsx
+++ src/chat/ChatLayout.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react"
-import ExpandingTextArea from "react-expanding-textarea"
 import { range } from "../helpers/range"
-import Button from "../ui/Button"
 import { MenuIcon, MoreIcon } from "../ui/icons"
+import { Chatbox } from "./Chatbox"
 
 export default function ChatLayout() {
 	return (
@@ -50,44 +48,5 @@ export default function ChatLayout() {
 
 			<Chatbox onSubmit={console.log} />
 		</main>
-	)
-}
-
-function Chatbox(props: { onSubmit: (message: string) => void }) {
-	const [rawValue, setRawValue] = useState("")
-
-	function submit() {
-		const value = rawValue.trim()
-		if (value) {
-			props.onSubmit(value)
-			setRawValue("")
-		}
-	}
-
-	return (
-		<form
-			className="flex p-2 space-x-2 bg-midnight-0"
-			onSubmit={(e) => {
-				e.preventDefault()
-				submit()
-			}}
-		>
-			<ExpandingTextArea
-				value={rawValue}
-				onChange={(e) => setRawValue(e.currentTarget.value)}
-				onKeyDown={(e) => {
-					if (e.key === "Enter" && !e.ctrlKey && !e.shiftKey) {
-						submit()
-						e.preventDefault()
-					}
-				}}
-				className="flex-1 resize-none input-solid"
-				placeholder="Say something..."
-				aria-label="Message"
-			/>
-			<Button type="submit" className="px-4 button-solid">
-				Send
-			</Button>
-		</form>
 	)
 }
