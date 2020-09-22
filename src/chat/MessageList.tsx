3880cf24ffb5c0db1de343dc0a46839e252fc9ec
nuei <61118778+thatnuei@users.noreply.github.com>
2020-09-22 13:31:46 -0500

    nicer bottom scroll UX on MessageList

--- src/chat/MessageList.tsx
+++ src/chat/MessageList.tsx
@@ -1,51 +1,85 @@
 import clsx from "clsx"
-import React, { useEffect, useRef } from "react"
+import { AnimatePresence, motion } from "framer-motion"
+import React, { useCallback, useEffect, useState } from "react"
 import { ChannelMessage } from "../channel/types"
+import { useEventTarget } from "../dom/useEventTarget"
 
 type Props = {
 	messages: ChannelMessage[]
 }
 
+// FIXME: has scrolling performance issues
 export default function MessageList({ messages }: Props) {
-	const listRef = useRef<HTMLUListElement>(null)
+	const [list, setList] = useState<HTMLElement | null>()
 
-	const isScrolledToBottom = (() => {
-		const list = listRef.current
+	const getScrolledToBottom = () => {
 		if (!list) return false
-		return list.scrollTop >= list.scrollHeight - list.clientHeight - 10
-	})()
+		return list.scrollTop >= list.scrollHeight - list.clientHeight - 20
+	}
+
+	const [isScrolledToBottom, setIsScrolledToBottom] = useState(
+		getScrolledToBottom,
+	)
+
+	useEventTarget(list, "scroll", () => {
+		setIsScrolledToBottom(getScrolledToBottom())
+	})
+
+	const scrollToBottom = useCallback(() => {
+		list?.scrollTo({
+			top: list.scrollHeight - list.clientHeight,
+			behavior: "smooth",
+		})
+	}, [list])
 
 	useEffect(() => {
-		const list = listRef.current
 		if (list && isScrolledToBottom) {
-			list.scrollTop = list.scrollHeight - list.clientHeight
+			scrollToBottom()
 		}
-	}, [isScrolledToBottom, messages])
+	}, [isScrolledToBottom, list, messages, scrollToBottom])
 
 	return (
-		<ul
-			className="h-full min-h-0 space-y-1 overflow-y-auto"
-			tabIndex={0}
-			ref={listRef}
-		>
-			{messages.map((message) => (
-				<li key={message.id} className="bg-midnight-1">
-					<div className={clsx("px-3 py-1", messageColorClass(message))}>
-						<span className="float-right pl-2 text-sm italic opacity-50">
-							{new Date(message.time).toLocaleTimeString()}
-						</span>
+		<div className="relative h-full overflow-hidden">
+			<ul
+				className="h-full space-y-1 overflow-y-auto"
+				tabIndex={0}
+				ref={setList}
+			>
+				{messages.map((message) => (
+					<li key={message.id} className="bg-midnight-1">
+						<div className={clsx("px-3 py-1", messageColorClass(message))}>
+							<span className="float-right pl-2 text-sm italic opacity-50">
+								{new Date(message.time).toLocaleTimeString()}
+							</span>
 
-						{message.senderName && (
-							<strong className="inline-block mr-3 font-medium">
-								{message.senderName}
-							</strong>
-						)}
+							{message.senderName && (
+								<strong className="inline-block mr-3 font-medium">
+									{message.senderName}
+								</strong>
+							)}
 
-						<span>{message.text}</span>
-					</div>
-				</li>
-			))}
-		</ul>
+							<span className="whitespace-pre-line">{message.text}</span>
+						</div>
+					</li>
+				))}
+			</ul>
+
+			<AnimatePresence>
+				{!isScrolledToBottom && (
+					<motion.button
+						type="button"
+						className="absolute bottom-0 w-full p-2 text-sm text-center bg-black bg-opacity-75"
+						onClick={scrollToBottom}
+						initial={{ y: "100%" }}
+						animate={{ y: "0" }}
+						exit={{ y: "100%" }}
+						transition={{ ease: "circOut", duration: 0.3 }}
+					>
+						Scroll to bottom
+					</motion.button>
+				)}
+			</AnimatePresence>
+		</div>
 	)
 }
 
