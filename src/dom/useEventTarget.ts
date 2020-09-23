a0e280085d44e1f07e10fb2d374c0efa3832254a
nuei <61118778+thatnuei@users.noreply.github.com>
2020-09-23 00:11:07 -0500

    type fixes for useEventTarget

--- src/dom/useEventTarget.ts
+++ src/dom/useEventTarget.ts
@@ -4,21 +4,21 @@ export function useEventTarget<E extends keyof HTMLElementEventMap>(
 	target: HTMLElement | undefined | null,
 	event: E,
 	callback: (event: HTMLElementEventMap[E]) => void,
-	options?: EventListenerOptions,
+	options?: AddEventListenerOptions,
 ): void
 
 export function useEventTarget<E extends keyof WindowEventMap>(
 	target: Window | undefined | null,
 	event: E,
 	callback: (event: WindowEventMap[E]) => void,
-	options?: EventListenerOptions,
+	options?: AddEventListenerOptions,
 ): void
 
 export function useEventTarget(
 	target: EventTarget | undefined | null,
 	event: string,
 	callback: (event: Event) => void,
-	options?: boolean | AddEventListenerOptions,
+	options?: AddEventListenerOptions,
 ) {
 	useEffect(() => {
 		target?.addEventListener(event, callback, options)
