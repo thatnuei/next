05da6d0056f80608687ff0242e8a2c7a8a8bc711
nuei <61118778+thatnuei@users.noreply.github.com>
2021-02-17 22:01:31 -0600

    new Modal component, test drive on channel browser

--- src/chatNav/ChatNav.tsx
+++ src/chatNav/ChatNav.tsx
@@ -1,5 +1,7 @@
 import { useObservable } from "micro-observables"
+import { forwardRef } from "react"
 import { tw } from "twind"
+import ChannelBrowser from "../channelBrowser/ChannelBrowser"
 import CharacterSummary from "../character/CharacterSummary"
 import Button from "../dom/Button"
 import { ComponentProps, TagProps } from "../jsx/types"
@@ -7,6 +9,7 @@ import { useRootStore } from "../root/context"
 import { fadedButton } from "../ui/components"
 import Icon, { IconProps } from "../ui/Icon"
 import * as icons from "../ui/icons"
+import Modal, { ModalButton, ModalPanel } from "../ui/Modal.new"
 import RoomTabList from "./RoomTabList"
 
 export default function ChatNav(props: TagProps<"nav">) {
@@ -15,22 +18,30 @@ export default function ChatNav(props: TagProps<"nav">) {
 	return (
 		<nav className={tw`flex h-full`}>
 			<div className={tw`flex flex-col`}>
-				<NavAction
-					icon={icons.list}
-					title="Browse channels"
-					onClick={root.channelBrowserStore.show}
-				/>
+				<Modal>
+					<ModalButton>
+						<NavAction icon={icons.list} title="Browse channels" />
+					</ModalButton>
+					<ModalPanel title="Channel browser">
+						<ChannelBrowser />
+					</ModalPanel>
+				</Modal>
+
 				<NavAction
 					icon={icons.updateStatus}
 					title="Update your status"
 					onClick={root.statusUpdateStore.show}
 				/>
+
 				<NavAction
 					icon={icons.users}
 					title="See online friends and bookmarks"
 				/>
+
 				<NavAction icon={icons.about} title="About next" />
+
 				<div className={tw`flex-1`} />
+
 				<NavAction
 					icon={icons.logout}
 					title="Log out"
@@ -54,10 +65,13 @@ type NavActionProps = ComponentProps<typeof Button> & {
 	icon: IconProps["which"]
 }
 
-function NavAction({ icon, ...props }: NavActionProps) {
+const NavAction = forwardRef(function NavAction(
+	{ icon, ...props }: NavActionProps,
+	ref: React.Ref<HTMLButtonElement>,
+) {
 	return (
-		<Button className={tw([fadedButton, tw`block p-3`])} {...props}>
-			<Icon which={icon} className={tw`w-5 h-5`} />
+		<Button className={tw`${fadedButton} block p-3`} {...props} ref={ref}>
+			<Icon which={icon} />
 		</Button>
 	)
-}
+})
