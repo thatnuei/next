4b334cd708717eedabcf538ddac6b233b9a4ff3b
nuei <61118778+thatnuei@users.noreply.github.com>
2021-02-19 00:52:27 -0600

    stable menu position, keep it on screen

--- src/ui/Menu.tsx
+++ src/ui/Menu.tsx
@@ -10,6 +10,8 @@ import {
 import * as reakit from "reakit"
 import { apply, tw } from "twind"
 import { css } from "twind/css"
+import { useElementSize } from "../dom/useElementSize"
+import { useWindowSize } from "../dom/useWindowSize"
 
 const [MenuProvider, useMenuContext] = constate(() => {
 	const dialog = reakit.useDialogState({
@@ -19,14 +21,35 @@ const [MenuProvider, useMenuContext] = constate(() => {
 
 	const buttonId = useMemo(() => `menu-button-${Math.random()}`, [])
 
+	// reakit's menu would normally work fine here,
+	// but it follows the element, and that's problematic when stuff moves around,
+	// can lead to misclicks
+	//
+	// so we have this garbage to make it so it stays where it was
+	// when the button was initially clicked
+	// ...kinda like how context menus normally work
 	const [position, setPosition] = useState({ left: 0, top: 0 })
+	const [dialogElement, dialogElementRef] = useState<HTMLElement | null>()
+	const windowSize = useWindowSize()
+	const dialogSize = useElementSize(dialogElement)
 
 	const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
-		const box = event.currentTarget.getBoundingClientRect()
-		setPosition({ left: box.left, top: box.bottom + 8 })
+		const button = event.currentTarget.getBoundingClientRect()
+		setPosition({ left: button.left, top: button.bottom })
 	}
 
-	return { dialog, buttonId, position, handleClick }
+	const boundedPosition = {
+		left: Math.min(position.left, windowSize.width - dialogSize.width - 8),
+		top: Math.min(position.top, windowSize.height - dialogSize.height - 8),
+	}
+
+	return {
+		dialog,
+		buttonId,
+		position: boundedPosition,
+		handleClick,
+		dialogElementRef,
+	}
 })
 
 export default function Menu({ children }: { children: ReactNode }) {
@@ -43,23 +66,25 @@ export function MenuButton({ children }: { children: ReactElement }) {
 }
 
 export function MenuPanel({ children }: { children: ReactNode }) {
-	const { dialog, buttonId, position } = useMenuContext()
+	const { dialog, buttonId, dialogElementRef, position } = useMenuContext()
 	return (
 		<reakit.Dialog
 			{...dialog}
 			aria-labelledby={buttonId}
 			className={tw`
-				fixed transition-opacity opacity-0
+				transition-opacity opacity-0
 				${css({ "&[data-enter]": apply`opacity-100` })}	
 			`}
-			style={position}
 		>
 			<div
 				className={tw`
+					absolute
 					w-56 shadow bg-midnight-1
 					transition-transform scale-95 origin-top
 					${css({ "[data-enter] &": apply`scale-100` })}	
 				`}
+				ref={dialogElementRef}
+				style={position}
 			>
 				{dialog.visible || dialog.animating ? children : null}
 			</div>
