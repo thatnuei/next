5e53ac68128d3fdf4adcfbc126eaa4344cafc1be
nuei <61118778+thatnuei@users.noreply.github.com>
2021-03-07 12:47:23 -0600

    use `@twind/vite-plugin-jsx`

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,15 @@
+import twindJsx from "@twind/vite-plugin-jsx"
 import reactRefresh from "@vitejs/plugin-react-refresh"
-import { defineConfig, Plugin } from "vite"
-import macros from "vite-plugin-babel-macros"
+import { defineConfig } from "vite"
 
 export default defineConfig({
 	esbuild: {
-		jsxInject: `import { createElement as _createElement, Fragment as _Fragment } from 'react'`,
-		jsxFactory: `_createElement`,
-		jsxFragment: `_Fragment`,
+		jsxInject: `import * as jsx$ from 'react'`,
+		jsxFactory: `jsx$.createElement`,
+		jsxFragment: `jsx$.Fragment`,
 	},
-	plugins: [
-		reactRefresh(),
-		inject({ prefix: `import "@twind/macro";` }),
-		macros(),
-	],
+	plugins: [reactRefresh(), twindJsx()],
 	build: {
 		sourcemap: true,
 	},
 })
-
-function inject(options: { prefix?: string; postfix?: string }): Plugin {
-	return {
-		name: "inject",
-		enforce: "pre",
-		transform(source, filename) {
-			if (/\.(j|t)sx?$/i.test(filename) && !filename.includes("node_modules")) {
-				return `${options.prefix ?? ""}${source}${options.postfix ?? ""}`
-			}
-		},
-	}
-}
