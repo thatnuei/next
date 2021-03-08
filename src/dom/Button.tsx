ded574becc0b373c83f7a63f0a671c5175e93144
nuei <61118778+thatnuei@users.noreply.github.com>
2021-03-08 02:04:34 -0600

    gotta save those bytes

--- src/dom/Button.tsx
+++ src/dom/Button.tsx
@@ -2,8 +2,6 @@ import { ComponentProps } from "react"
 import { autoRef } from "../react/autoRef"
 
 /** Regular DOM button with a better default type */
-function Button(props: ComponentProps<"button">) {
+export default autoRef(function Button(props: ComponentProps<"button">) {
 	return <button type="button" {...props} />
-}
-
-export default autoRef(Button)
+})
