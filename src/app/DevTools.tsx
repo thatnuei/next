f50be25e2afba1c03cbced30de15eee33629629e
nuei <61118778+thatnuei@users.noreply.github.com>
2021-06-20 16:00:17 -0500

    add command simulator to devtools

--- src/app/DevTools.tsx
+++ src/app/DevTools.tsx
@@ -1,7 +1,16 @@
+import clsx from "clsx"
 import { useState } from "react"
+import { safeJsonParse } from "../common/json"
+import Button from "../dom/Button"
+import ExternalLink from "../dom/ExternalLink"
 import { useWindowEvent } from "../dom/useWindowEvent"
+import { useRootStore } from "../root/context"
+import type { ServerCommand } from "../socket/helpers"
+import { input, solidButton } from "../ui/components"
+import Drawer from "../ui/Drawer"
+import FormField from "../ui/FormField"
 
-function DevTools() {
+export default function DevTools() {
 	const [visible, setVisible] = useState(false)
 	const toggleVisible = () => setVisible((v) => !v)
 
@@ -13,16 +22,84 @@ function DevTools() {
 	})
 
 	return (
-		<div
-			className={`fixed top-0 left-0 right-0 p-4 bg-black bg-opacity-50 transition-all transform ${
-				visible
-					? `opacity-100 visible`
-					: `opacity-0 invisible -translate-y-full`
-			}`}
-		>
-			i might give this some other purpose later lol
-		</div>
+		<Drawer side="bottom" open={visible} onOpenChange={setVisible}>
+			<div className="w-full p-4">
+				<CommandSimulator />
+			</div>
+		</Drawer>
 	)
 }
 
-export default DevTools
+function CommandSimulator() {
+	const root = useRootStore()
+
+	const defaultCommand = "MSG"
+	const defaultParams =
+		'{"character":"Testificate","message":"test message","channel":"Development"}'
+
+	const [commandState, setCommand] = useState("")
+	const command = commandState || defaultCommand
+
+	const [paramsState, setParams] = useState("")
+	const params = paramsState || defaultParams
+
+	const paramsParseResult =
+		params.length > 2 ? safeJsonParse(params) : undefined
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
+
+		if (paramsParseResult?.result) {
+			root.socket.commands.publish({
+				type: command,
+				params: paramsParseResult.result,
+			} as ServerCommand)
+		}
+	}
+
+	return (
+		<form onSubmit={handleSubmit} className="grid gap-3">
+			<div className="grid gap-1">
+				<h2>Command Simulator</h2>
+				<p className="text-sm">
+					Simulate a fake socket command just within the client. Does not send
+					any socket messages. This can crash the app if you&apos;re not
+					careful!
+				</p>
+				<p className="text-sm">
+					See the{" "}
+					<ExternalLink
+						href="https://wiki.f-list.net/F-Chat_Server_Commands"
+						className="underline"
+					>
+						wiki
+					</ExternalLink>{" "}
+					for a list of server commands.
+				</p>
+			</div>
+			<FormField labelText="Command">
+				<input
+					className={input}
+					placeholder="MSG"
+					value={commandState}
+					onChange={(e) => setCommand(e.target.value)}
+				/>
+			</FormField>
+			<FormField labelText="Params">
+				<textarea
+					rows={3}
+					className={clsx(input, "font-mono resize-y")}
+					placeholder='{"character":"Testificate","message":"test message","channel":"Development"}'
+					value={paramsState}
+					onChange={(e) => setParams(e.target.value)}
+				/>
+			</FormField>
+			<pre className="overflow-x-auto">{paramsParseResult?.error?.message}</pre>
+			<div>
+				<Button type="submit" className={solidButton}>
+					Submit
+				</Button>
+			</div>
+		</form>
+	)
+}
