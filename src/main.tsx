415f5168547c2ad4c21306eef260af3600803385
nuei <61118778+thatnuei@users.noreply.github.com>
2021-06-20 20:46:54 -0500

    react alphaaaaa

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,4 @@ import ReactDOM from "react-dom"
 import Root from "./root/Root"
 import "./tailwind.css"
 
-ReactDOM.render(<Root />, document.getElementById("root"))
+ReactDOM.createRoot(document.getElementById("root")).render(<Root />)
