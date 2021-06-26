920adf28d98414b875947cc87d1fa332d24f63c3
nuei <61118778+thatnuei@users.noreply.github.com>
2021-06-26 16:11:32 -0500

    add preventDefault helper

---
+++ src/react/preventDefault.ts
@@ -0,0 +1,12 @@
+interface EventLike {
+	preventDefault(): void
+}
+
+export function preventDefault<Event extends EventLike>(
+	callback: (event: Event) => void,
+) {
+	return function withDefaultPrevented(event: Event) {
+		event.preventDefault()
+		callback(event)
+	}
+}
