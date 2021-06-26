696db8728b23670737c6abcc705b7b5b8e294807
nuei <61118778+thatnuei@users.noreply.github.com>
2021-06-26 16:23:50 -0500

    fix LoadingOverlay dot colors

--- src/ui/LoadingOverlay.tsx
+++ src/ui/LoadingOverlay.tsx
@@ -1,21 +1,16 @@
-interface Props { text: string }
-
-function LoadingOverlay({ text }: Props) {
-	const dotStyle = `w-5 h-5 rounded-full bg-clouds`
-
+export default function LoadingOverlay({ text }: { text: string }) {
+	const dotStyle = `w-5 h-5 rounded-full`
 	return (
 		<div
 			className={`fixed inset-0 flex flex-col items-center justify-center text-center bg-black bg-opacity-50`}
 		>
 			<div className={`grid grid-cols-2 grid-rows-2 gap-4 p-6 spin-animation`}>
 				<div className={`${dotStyle} bg-blue-500`}></div>
-				<div className={`${dotStyle}`}></div>
-				<div className={`${dotStyle}`}></div>
+				<div className={`${dotStyle} bg-clouds`}></div>
+				<div className={`${dotStyle} bg-clouds`}></div>
 				<div className={`${dotStyle} bg-blue-500`}></div>
 			</div>
 			<p className={`text-2xl font-condensed`}>{text}</p>
 		</div>
 	)
 }
-
-export default LoadingOverlay
