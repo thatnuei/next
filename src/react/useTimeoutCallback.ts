c5990bbf412f9c25b5770fb38b7f6a92b77895a2
nuei <61118778+thatnuei@users.noreply.github.com>
2021-06-29 08:52:55 -0500

    add useTimeoutCallback

---
+++ src/react/useTimeoutCallback.ts
@@ -0,0 +1,8 @@
+import { useEffect } from "react"
+
+export function useTimeoutCallback(duration: number, callback: () => void) {
+	useEffect(() => {
+		const id = setTimeout(callback, duration)
+		return () => clearTimeout(id)
+	}, [callback, duration])
+}
