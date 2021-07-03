f5d5c063d3a4f3c3841077accee2e0ba2e451205
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-02 22:16:29 -0500

    usePromiseState: use initialPromise arg

--- src/state/usePromiseState.ts
+++ src/state/usePromiseState.ts
@@ -2,7 +2,7 @@ import { useState } from "react"
 import usePromise from "./usePromise"
 
 export default function usePromiseState<T>(initialPromise?: Promise<T>) {
-	const [promise, setPromise] = useState<Promise<T>>()
+	const [promise, setPromise] = useState(initialPromise)
 	const state = usePromise(promise)
 	return { ...state, setPromise }
 }
