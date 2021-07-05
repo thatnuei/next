41332feeec88e9d5caec510c5641f050599f4c8e
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-04 23:21:47 -0500

    fix channel filters

--- src/channel/ChannelFilters.tsx
+++ src/channel/ChannelFilters.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx"
+import type { ReactNode } from "react"
 import Button from "../dom/Button"
 import { useChannel, useChannelActions } from "./state"
 import type { ChannelMode } from "./types"
@@ -7,37 +9,71 @@ interface Props {
 }
 
 function ChannelFilters({ channelId, ...props }: Props) {
-	const channel = useChannel(channelId)
+	const { mode, selectedMode } = useChannel(channelId)
 	const { updateChannel } = useChannelActions()
 
-	function setSelectedMode(mode: ChannelMode) {
-		void updateChannel({ id: channelId, selectedMode: mode })
+	function buttonProps(mode: ChannelMode) {
+		return {
+			onClick: () => updateChannel({ id: channelId, selectedMode: mode }),
+			active: selectedMode === mode,
+		}
 	}
 
-	function renderFilterButton(mode: ChannelMode, label: string) {
-		const isSelected = channel.selectedMode === mode
-
+	if (mode === "ads") {
 		return (
-			<Button
-				className={`block transition-opacity whitespace-nowrap ${
-					isSelected ? `opacity-100` : `opacity-50 hover:opacity-75`
-				}`}
-				onClick={() => setSelectedMode(mode)}
-				role="radio"
-				aria-checked={isSelected}
-			>
-				{label}
-			</Button>
+			<div className="flex flex-row gap-3" role="radiogroup" {...props}>
+				<ChannelFilterButton disabled>Chat</ChannelFilterButton>
+				<ChannelFilterButton active>Ads</ChannelFilterButton>
+				<ChannelFilterButton disabled>Both</ChannelFilterButton>
+			</div>
+		)
+	}
+
+	if (mode === "chat") {
+		return (
+			<div className="flex flex-row gap-3" role="radiogroup" {...props}>
+				<ChannelFilterButton active>Chat</ChannelFilterButton>
+				<ChannelFilterButton disabled>Ads</ChannelFilterButton>
+				<ChannelFilterButton disabled>Both</ChannelFilterButton>
+			</div>
 		)
 	}
 
 	return (
 		<div className="flex flex-row gap-3" role="radiogroup" {...props}>
-			{renderFilterButton("both", "Both")}
-			{renderFilterButton("chat", "Chat")}
-			{renderFilterButton("ads", "Ads")}
+			<ChannelFilterButton {...buttonProps("chat")}>Chat</ChannelFilterButton>
+			<ChannelFilterButton {...buttonProps("ads")}>Ads</ChannelFilterButton>
+			<ChannelFilterButton {...buttonProps("both")}>Both</ChannelFilterButton>
 		</div>
 	)
 }
 
 export default ChannelFilters
+
+function ChannelFilterButton({
+	children,
+	active,
+	disabled,
+	onClick,
+}: {
+	children: ReactNode
+	active?: boolean
+	disabled?: boolean
+	onClick?: () => void
+}) {
+	return (
+		<Button
+			className={clsx(
+				"block transition-opacity whitespace-nowrap",
+				active ? `opacity-100` : `opacity-50 hover:opacity-75`,
+				disabled && "opacity-25 pointer-events-none",
+			)}
+			onClick={onClick ?? (() => {})}
+			role="radio"
+			aria-checked={active ?? false}
+			aria-disabled={disabled ?? false}
+		>
+			{children}
+		</Button>
+	)
+}
