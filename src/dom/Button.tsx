4494bad3a23cb7ff301a2d18475d5cdf96f6ea34
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-05 00:41:23 -0500

    Button: better "disabled" behavior

--- src/dom/Button.tsx
+++ src/dom/Button.tsx
@@ -1,7 +1,19 @@
+import clsx from "clsx"
 import type { ComponentProps } from "react"
 import { autoRef } from "../react/autoRef"
 
 /** Regular DOM button with a better default type */
-export default autoRef(function Button(props: ComponentProps<"button">) {
-	return <button type="button" {...props} />
+export default autoRef(function Button({
+	disabled,
+	className,
+	...props
+}: ComponentProps<"button">) {
+	return (
+		<button
+			type="button"
+			className={clsx(disabled && `opacity-50 pointer-events-none`, className)}
+			aria-disabled={disabled ?? false}
+			{...props}
+		/>
+	)
 })
