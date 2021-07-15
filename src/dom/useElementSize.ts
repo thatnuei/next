16c96b1a6e68642a0018d673fdf55957cb5c851c
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-14 22:28:59 -0500

    useElementSize: accept a ref, use bounding client rect

--- src/dom/useElementSize.ts
+++ src/dom/useElementSize.ts
@@ -1,18 +1,22 @@
+import type { RefObject } from "react"
 import { useEffect, useState } from "react"
 import ResizeObserver from "resize-observer-polyfill"
 
-export function useElementSize(element: Element | undefined | null) {
+export function useElementSize(
+	ref: Element | RefObject<Element> | undefined | null,
+) {
 	const [size, setSize] = useState({ width: 0, height: 0 })
 
 	useEffect(() => {
+		const element = ref instanceof Element ? ref : ref?.current
 		if (!element) return
 
 		const observer = new ResizeObserver(([info]: ResizeObserverEntry[]) => {
-			if (info) setSize(info.contentRect)
+			if (info) setSize(info.target.getBoundingClientRect())
 		})
 		observer.observe(element)
 		return () => observer.disconnect()
-	}, [element])
+	}, [ref])
 
 	return size
 }
