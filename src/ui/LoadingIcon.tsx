34a95b5cb090188fa965c9b19ab76381e5686529
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-18 16:51:07 -0500

    split out the loading icon to its own component

---
+++ src/ui/LoadingIcon.tsx
@@ -0,0 +1,11 @@
+export default function LoadingIcon() {
+	const dotStyle = `w-5 h-5 rounded-full`
+	return (
+		<div className={`grid grid-cols-2 grid-rows-2 gap-4 p-6 spin-animation`}>
+			<div className={`${dotStyle} bg-blue-500`}></div>
+			<div className={`${dotStyle} bg-clouds`}></div>
+			<div className={`${dotStyle} bg-clouds`}></div>
+			<div className={`${dotStyle} bg-blue-500`}></div>
+		</div>
+	)
+}
--- src/ui/LoadingOverlay.tsx
+++ src/ui/LoadingOverlay.tsx
@@ -1,17 +1,12 @@
 import type { ReactNode } from "react"
+import LoadingIcon from "./LoadingIcon"
 
 export default function LoadingOverlay({ children }: { children: ReactNode }) {
-	const dotStyle = `w-5 h-5 rounded-full`
 	return (
 		<div
 			className={`fixed inset-0 flex flex-col items-center justify-center text-center bg-black bg-opacity-50`}
 		>
-			<div className={`grid grid-cols-2 grid-rows-2 gap-4 p-6 spin-animation`}>
-				<div className={`${dotStyle} bg-blue-500`}></div>
-				<div className={`${dotStyle} bg-clouds`}></div>
-				<div className={`${dotStyle} bg-clouds`}></div>
-				<div className={`${dotStyle} bg-blue-500`}></div>
-			</div>
+			<LoadingIcon />
 			<div className="flex flex-col items-center gap-4">{children}</div>
 		</div>
 	)
