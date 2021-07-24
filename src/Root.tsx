95642c45791de7f8e35e9bc9c731c92f0205112a
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-24 12:07:47 -0500

    move more providers into chat + make routing a chat-only thing

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,23 +1,10 @@
-import { Provider as JotaiProvider } from "jotai"
 import App from "./app/App"
 import AppErrorBoundary from "./app/AppErrorBoundary"
-import AppTitle from "./app/AppTitle"
-import NotificationToastOverlay from "./notifications/NotificationToastOverlay"
-import { RouteProvider } from "./router"
-import { SocketConnection } from "./socket/SocketConnection"
 
 export default function Root() {
 	return (
 		<AppErrorBoundary>
-			<RouteProvider>
-				<JotaiProvider>
-					<SocketConnection>
-						<AppTitle />
-						<App />
-						<NotificationToastOverlay />
-					</SocketConnection>
-				</JotaiProvider>
-			</RouteProvider>
+			<App />
 		</AppErrorBoundary>
 	)
 }
--- src/chat/Chat.tsx
+++ src/chat/Chat.tsx
@@ -1,67 +1,83 @@
 import clsx from "clsx"
-import { useDeferredValue, useEffect } from "react"
+import { Provider as JotaiProvider } from "jotai"
+import { useDeferredValue } from "react"
+import AppTitle from "../app/AppTitle"
 import ChannelView from "../channel/ChannelView"
+import { CharacterStoreProvider } from "../character/store"
 import DevTools from "../dev/DevTools"
+import type { AuthUser } from "../flist/types"
 import ChatLogBrowser from "../logging/ChatLogBrowser"
 import NotificationListScreen from "../notifications/NotificationListScreen"
+import NotificationToastOverlay from "../notifications/NotificationToastOverlay"
 import PrivateChatView from "../privateChat/PrivateChatView"
-import type { Route } from "../router"
-import { useRoute } from "../router"
-import { useSocketActions } from "../socket/SocketConnection"
-import { useIdentity } from "../user"
+import { RouteProvider, useRoute } from "../router"
+import { SocketConnection } from "../socket/SocketConnection"
 import ChatCommandHandlers from "./ChatCommandHandlers"
 import ChatNav from "./ChatNav"
 import ConnectionGuard from "./ConnectionGuard"
 import NoRoomView from "./NoRoomView"
 
-export default function Chat() {
-	const { connect, disconnect } = useSocketActions()
-	const identity = useIdentity()
+export default function Chat({
+	user,
+	identity,
+}: {
+	user: AuthUser
+	identity: string
+}) {
+	return (
+		<RouteProvider>
+			<JotaiProvider>
+				<SocketConnection>
+					<CharacterStoreProvider
+						user={user}
+						identity={identity}
+						// temporary
+						getFriendsAndBookmarks={() =>
+							Promise.resolve({ bookmarklist: [], friendlist: [] })
+						}
+					>
+						<AppTitle />
+						<ChatCommandHandlers />
+						<ConnectionGuard>
+							<div className="flex flex-row h-full gap-1">
+								<div className="hidden md:block">
+									<ChatNav />
+								</div>
+								<ChatRoutes />
+							</div>
+						</ConnectionGuard>
+						<NotificationToastOverlay />
+						{import.meta.env.DEV && <DevTools />}
+					</CharacterStoreProvider>
+				</SocketConnection>
+			</JotaiProvider>
+		</RouteProvider>
+	)
+}
 
-	useEffect(() => {
-		if (identity) {
-			connect(identity)
-			return () => {
-				disconnect()
-			}
-		}
-	}, [connect, disconnect, identity])
-
+function ChatRoutes() {
 	const route = useRoute()
 	const deferredRoute = useDeferredValue(route)
 
 	return (
-		<>
-			<ChatCommandHandlers />
-			<ConnectionGuard>
-				<div className="flex flex-row h-full gap-1">
-					<div className="hidden md:block">
-						<ChatNav />
-					</div>
-					<div
-						className={clsx(
-							"flex-1 min-w-0",
-							route !== deferredRoute && "transition-opacity opacity-50",
-						)}
-						style={{ transitionDelay: route !== deferredRoute ? "0.3s" : "0" }}
-					>
-						<ChatRoutes route={deferredRoute} />
-					</div>
-				</div>
-			</ConnectionGuard>
-			{import.meta.env.DEV && <DevTools />}
-		</>
-	)
-}
-
-function ChatRoutes({ route }: { route: Route }) {
-	return (
-		<>
-			{route.name === "channel" && <ChannelView {...route.params} />}
-			{route.name === "privateChat" && <PrivateChatView {...route.params} />}
-			{route.name === "notifications" && <NotificationListScreen />}
-			{route.name === "logs" && <ChatLogBrowser />}
-			{route.name === "chat" && <NoRoomView />}
-		</>
+		<div
+			className={clsx(
+				"flex-1 min-w-0",
+				route !== deferredRoute && "transition-opacity opacity-50",
+			)}
+			style={{
+				transitionDelay: route !== deferredRoute ? "0.3s" : "0",
+			}}
+		>
+			{deferredRoute.name === "channel" && (
+				<ChannelView {...deferredRoute.params} />
+			)}
+			{deferredRoute.name === "privateChat" && (
+				<PrivateChatView {...deferredRoute.params} />
+			)}
+			{deferredRoute.name === "notifications" && <NotificationListScreen />}
+			{deferredRoute.name === "logs" && <ChatLogBrowser />}
+			{deferredRoute.name === false && <NoRoomView />}
+		</div>
 	)
 }
