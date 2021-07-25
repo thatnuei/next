2066611a50302f455735cdf3c6ca9ab837dbfa0f
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-25 13:38:05 -0500

    add & configure mobx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import "focus-visible"
+import { configure } from "mobx"
 import ReactDOM from "react-dom"
 import "virtual:windi.css"
 import { ChatLoggerProvider } from "./logging/context"
 import { createWebChatLogger } from "./logging/web"
 import Root from "./Root"
 import "./styles.css"
+
+configure({
+	observableRequiresReaction: true,
+})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<ChatLoggerProvider logger={createWebChatLogger()}>
