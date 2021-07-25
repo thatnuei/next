df59c7ed9a46811aabd467675970d5d3e081d791
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-25 15:27:51 -0500

    loading icon lol

--- src/ui/LoadingIcon.tsx
+++ src/ui/LoadingIcon.tsx
@@ -1,7 +1,9 @@
 export default function LoadingIcon() {
 	const dotStyle = `w-5 h-5 rounded-full`
 	return (
-		<div className={`grid grid-cols-2 grid-rows-2 gap-4 p-6 spin-animation`}>
+		<div
+			className={`grid grid-cols-2 grid-rows-2 gap-4 p-6 spin-animation place-content-center`}
+		>
 			<div className={`${dotStyle} bg-blue-500`}></div>
 			<div className={`${dotStyle} bg-clouds`}></div>
 			<div className={`${dotStyle} bg-clouds`}></div>
