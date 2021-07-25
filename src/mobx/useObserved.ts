cf2838b34c366202b1023fce02537544023f84a3
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-25 13:39:09 -0500

    add useObserved helper

---
+++ src/mobx/useObserved.ts
@@ -0,0 +1,8 @@
+import { reaction } from "mobx"
+import { useEffect, useState } from "react"
+
+export function useObserved<T>(getValue: () => T): T {
+	const [value, setValue] = useState(getValue)
+	useEffect(() => reaction(getValue, setValue), [getValue])
+	return value
+}
