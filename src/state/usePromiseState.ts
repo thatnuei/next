012ecd36a81718d22d4e944d01f676d406cff3f5
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-25 15:13:36 -0500

    usePromiseState: also accept non-promise

--- src/state/usePromiseState.ts
+++ src/state/usePromiseState.ts
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import usePromise from "./usePromise"
 
-export default function usePromiseState<T>(initialPromise?: Promise<T>) {
+export default function usePromiseState<T>(initialPromise?: Promise<T> | T) {
 	const [promise, setPromise] = useState(initialPromise)
 	const state = usePromise(promise)
 	return { ...state, setPromise }
