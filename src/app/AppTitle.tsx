b44b0edf25d9d2cdce95833ebdb87162beab389f
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-25 21:06:12 -0500

    count some unread notifications in app title

--- src/app/AppTitle.tsx
+++ src/app/AppTitle.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import { isTruthy } from "../common/isTruthy"
 import type { Falsy } from "../common/types"
+import { useNotificationList } from "../notifications/state"
 import { useOpenPrivateChats } from "../privateChat/state"
 import { useIdentity } from "../user"
 
@@ -8,11 +9,18 @@ export default function AppTitle() {
 	const identity = useIdentity()
 
 	const privateChats = useOpenPrivateChats()
-	const unreadChats = privateChats.filter((chat) => chat.isUnread).length
+	const unreadChats = privateChats.filter((chat) => chat.isUnread)
 
-	const prefixParts = [identity, unreadChats > 0 && `(${unreadChats})`]
-	const prefix = joinPresentStrings(prefixParts, " ")
-	const title = joinPresentStrings([prefix, "next"], " | ")
+	const notifications = useNotificationList()
+	const unreadNotifications = notifications
+		.filter((n) => n.readStatus === "unread")
+		.filter((n) => n.type === "invite" || n.type === "broadcast")
+
+	const totalUnread = unreadChats.length + unreadNotifications.length
+
+	const prefixParts = [identity, totalUnread > 0 && `(${totalUnread})`]
+	const prefix = joinContentfulStrings(prefixParts, " ")
+	const title = joinContentfulStrings([prefix, "next"], " | ")
 
 	useEffect(() => {
 		document.title = title
@@ -21,7 +29,7 @@ export default function AppTitle() {
 	return null
 }
 
-function joinPresentStrings(
+function joinContentfulStrings(
 	strings: readonly (string | Falsy)[],
 	separator: string,
 ) {
