5d9d1148e3e807180fd4be1392192bbac84c6e1b
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-27 01:07:31 -0500

    move keyValueStore to its own file

--- src/storage/createStoredValue.ts
+++ src/storage/createStoredValue.ts
@@ -1,29 +1,5 @@
-import { openDB } from "idb"
 import type { Validator } from "../validation"
-
-const dbPromise = openDB("keyval-store", 1, {
-	upgrade(db) {
-		db.createObjectStore("keyval")
-	},
-})
-
-const keyValueStore = {
-	async get(key: string): Promise<unknown> {
-		return (await dbPromise).get("keyval", key) as unknown
-	},
-	async set(key: string, val: unknown): Promise<void> {
-		await (await dbPromise).put("keyval", val, key)
-	},
-	async delete(key: string): Promise<void> {
-		await (await dbPromise).delete("keyval", key)
-	},
-	async clear(): Promise<void> {
-		await (await dbPromise).clear("keyval")
-	},
-	async keys(): Promise<IDBValidKey[]> {
-		return (await dbPromise).getAllKeys("keyval")
-	},
-}
+import { keyValueStore } from "./keyValueStore"
 
 export function createStoredValue<V>(key: string, validator: Validator<V>) {
 	const get = async (): Promise<V> =>
---
+++ src/storage/keyValueStore.ts
@@ -0,0 +1,25 @@
+import { openDB } from "idb"
+
+const dbPromise = openDB("keyval-store", 1, {
+	upgrade(db) {
+		db.createObjectStore("keyval")
+	},
+})
+
+export const keyValueStore = {
+	async get(key: string): Promise<unknown> {
+		return (await dbPromise).get("keyval", key) as unknown
+	},
+	async set(key: string, val: unknown): Promise<void> {
+		await (await dbPromise).put("keyval", val, key)
+	},
+	async delete(key: string): Promise<void> {
+		await (await dbPromise).delete("keyval", key)
+	},
+	async clear(): Promise<void> {
+		await (await dbPromise).clear("keyval")
+	},
+	async keys(): Promise<IDBValidKey[]> {
+		return (await dbPromise).getAllKeys("keyval")
+	},
+}
