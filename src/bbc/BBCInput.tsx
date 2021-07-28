912a9f5c02856cff38fd5f0ddba0dd1bd0cdcc8d
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-27 22:16:30 -0500

    improve bbcode shortcut behavior

--- src/bbc/BBCInput.tsx
+++ src/bbc/BBCInput.tsx
@@ -9,6 +9,7 @@ import { input } from "../ui/components"
 import usePopper from "../ui/usePopper"
 import BBC from "./BBC"
 import KeyboardShortcutsPopoverButton from "./KeyboardShortcutsPopoverButton"
+import type { BBCodeShortcut } from "./shortcuts"
 import { shortcuts } from "./shortcuts"
 
 interface BBCTextAreaProps extends Omit<TagProps<"textarea">, "className"> {
@@ -39,38 +40,8 @@ export default function BBCTextArea({
 
 	const popper = usePopper()
 
-	function runBBCodeShortcut(
-		event: SyntheticEvent<HTMLTextAreaElement>,
-		tag: string,
-	): void {
-		event.preventDefault()
-
-		const target = event.currentTarget
-		const { selectionStart, selectionEnd, value } = target
-
-		const tagStart = `[${tag}]`
-		const tagEnd = `[/${tag}]`
-
-		const newText =
-			value.slice(0, selectionStart) +
-			tagStart +
-			value.slice(selectionStart, selectionEnd) +
-			tagEnd +
-			value.slice(selectionEnd)
-
-		onChangeText(newText)
-
-		// need to wait for a re-render before setting the selection
-		requestAnimationFrame(() => {
-			target.setSelectionRange(
-				selectionStart,
-				selectionEnd + tagStart.length + tagEnd.length,
-			)
-		})
-	}
-
 	return (
-		<div className="relative flex flex-col-reverse">
+		<div className="flex flex-col-reverse relative">
 			<textarea
 				className={clsx(input, "peer")}
 				rows={3}
@@ -93,7 +64,7 @@ export default function BBCTextArea({
 					if (event.ctrlKey) {
 						const shortcut = shortcuts.find((s) => s.key === event.code)
 						if (shortcut?.type === "bbcode") {
-							runBBCodeShortcut(event, shortcut.tag)
+							onChangeText(applyBBCodeShortcut(event, shortcut))
 						}
 					}
 				}}
@@ -111,7 +82,7 @@ export default function BBCTextArea({
 				</p>
 			)}
 
-			<div className="absolute top-0 right-0 p-1">
+			<div className="p-1 top-0 right-0 absolute">
 				<KeyboardShortcutsPopoverButton />
 			</div>
 
@@ -128,7 +99,7 @@ export default function BBCTextArea({
 						)}
 					>
 						<div
-							className="px-3 py-2 overflow-y-auto shadow bg-midnight-0 max-h-32"
+							className="bg-midnight-0 shadow max-h-32 py-2 px-3 overflow-y-auto"
 							style={{ width: textAreaRect.width }}
 						>
 							<p className="text-sm opacity-70">Preview</p>
@@ -152,4 +123,41 @@ function useDebouncedValue<T>(value: T, duration = 500): T {
 	}, [duration, value])
 
 	return currentValue
+}
+
+function applyBBCodeShortcut(
+	event: SyntheticEvent<HTMLTextAreaElement>,
+	shortcut: BBCodeShortcut,
+): string {
+	event.preventDefault()
+
+	const target = event.currentTarget
+	const { selectionStart, selectionEnd, value } = target
+
+	const tagStart = shortcut.hasValue
+		? `[${shortcut.tag}=]`
+		: `[${shortcut.tag}]`
+	const tagEnd = `[/${shortcut.tag}]`
+
+	const newText =
+		value.slice(0, selectionStart) +
+		tagStart +
+		value.slice(selectionStart, selectionEnd) +
+		tagEnd +
+		value.slice(selectionEnd)
+
+	// need to wait for a re-render before setting the selection
+	requestAnimationFrame(() => {
+		if (shortcut.hasValue) {
+			const position = selectionStart + tagStart.length - 1
+			target.setSelectionRange(position, position)
+		} else {
+			target.setSelectionRange(
+				selectionStart + tagStart.length,
+				selectionEnd + tagStart.length,
+			)
+		}
+	})
+
+	return newText
 }
