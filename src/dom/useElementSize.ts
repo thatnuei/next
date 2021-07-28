727cb32387638f4f35d892700626be02a0eecd2d
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-27 23:27:05 -0500

    more better optimized useElementSize

--- src/dom/useElementSize.ts
+++ src/dom/useElementSize.ts
@@ -1,22 +1,27 @@
 import type { RefObject } from "react"
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import ResizeObserver from "resize-observer-polyfill"
 
 export function useElementSize(
 	ref: Element | RefObject<Element> | undefined | null,
 ) {
-	const [size, setSize] = useState({ width: 0, height: 0 })
+	const [width, setWidth] = useState(0)
+	const [height, setHeight] = useState(0)
 
 	useEffect(() => {
 		const element = ref instanceof Element ? ref : ref?.current
 		if (!element) return
 
 		const observer = new ResizeObserver(([info]: ResizeObserverEntry[]) => {
-			if (info) setSize(info.target.getBoundingClientRect())
+			if (info) {
+				const rect = info.target.getBoundingClientRect()
+				setWidth(rect.width)
+				setHeight(rect.height)
+			}
 		})
 		observer.observe(element)
 		return () => observer.disconnect()
 	}, [ref])
 
-	return size
+	return useMemo(() => ({ width, height }), [width, height])
 }
