cd18f69223e009f630f171ba3adce8654272561a
nuei <61118778+thatnuei@users.noreply.github.com>
2021-07-30 01:04:19 -0500

    fix/improve character menu behavior

---
+++ src/character/CharacterMenu.tsx
@@ -0,0 +1,113 @@
+import clsx from "clsx"
+import Button from "../dom/Button"
+import ExternalLink from "../dom/ExternalLink"
+// import TextInput from "../dom/TextInput"
+import { getProfileUrl } from "../flist/helpers"
+import { usePrivateChatActions } from "../privateChat/state"
+import { routes } from "../router"
+import { useSocketActions } from "../socket/SocketConnection"
+// import { input } from "../ui/components"
+import Icon from "../ui/Icon"
+import * as icons from "../ui/icons"
+import { useUserActions } from "../user"
+import CharacterMemoInput from "./CharacterMemoInput"
+import CharacterSummary from "./CharacterSummary"
+import { useCharacterRoles } from "./state"
+
+const menuItemClass = clsx`flex opacity-50 transition-opacity hover:opacity-100 p-2 gap-2 items-center`
+
+export default function CharacterMenu({ name }: { name: string }) {
+	const { friendships, isBookmarked, isIgnored } = useCharacterRoles(name)
+	const { send } = useSocketActions()
+	const { addBookmark, removeBookmark } = useUserActions()
+	const privateChatActions = usePrivateChatActions(name)
+
+	return (
+		<>
+			<div className={`p-2 space-y-3 bg-midnight-0`}>
+				<CharacterSummary name={name} />
+
+				{friendships
+					.filter(({ them }) => them === name)
+					.map((item, index) => (
+						<div
+							key={index}
+							className={`flex flex-row items-center px-2 py-1 space-x-1 text-sm text-green-400 bg-green-500 bg-opacity-20`}
+						>
+							<Icon which={icons.heart} />
+							<div>{item.us}</div>
+						</div>
+					))}
+			</div>
+
+			<div className={`flex flex-col`}>
+				<ExternalLink className={menuItemClass} href={getProfileUrl(name)}>
+					<Icon which={icons.link} />
+					Profile
+				</ExternalLink>
+
+				<Button
+					className={menuItemClass}
+					onClick={() => {
+						routes.privateChat({ partnerName: name }).push()
+						privateChatActions.open()
+					}}
+				>
+					<Icon which={icons.message} />
+					Message
+				</Button>
+
+				<Button
+					className={menuItemClass}
+					role="checkbox"
+					aria-checked={isBookmarked}
+					onClick={() => {
+						if (isBookmarked) {
+							removeBookmark({ name }).catch(console.error) // show error toast
+						} else {
+							addBookmark({ name }).catch(console.error) // show error toast
+						}
+					}}
+				>
+					<Icon which={isBookmarked ? icons.bookmark : icons.bookmarkHollow} />
+					{isBookmarked ? "Remove bookmark" : "Bookmark"}
+				</Button>
+
+				<Button
+					className={menuItemClass}
+					role="checkbox"
+					aria-checked={isIgnored}
+					onClick={() => {
+						send({
+							type: "IGN",
+							params: {
+								action: isIgnored ? "delete" : "add",
+								character: name,
+							},
+						})
+					}}
+				>
+					<Icon which={isIgnored ? icons.ignore : icons.ignoreHollow} />
+					{isIgnored ? "Unignore" : "Ignore"}
+				</Button>
+			</div>
+
+			<div className="space-y-2 bg-midnight-0 p-2">
+				<details>
+					<summary className="cursor-pointer text-sm select-none">Memo</summary>
+					<CharacterMemoInput name={name} />
+				</details>
+
+				{/* <details>
+					<summary className="cursor-pointer text-sm select-none">
+						Nickname
+					</summary>
+					<p className="my-1 text-xs">
+						This nickname will show up instead of their actual character name.
+					</p>
+					<TextInput className={input} placeholder="Stanley" />
+				</details> */}
+			</div>
+		</>
+	)
+}
--- src/character/CharacterMenuTarget.tsx
+++ src/character/CharacterMenuTarget.tsx
@@ -1,119 +1,45 @@
-import * as React from "react"
-import Button from "../dom/Button"
+import { Portal } from "@headlessui/react"
+import type { ReactNode } from "react"
+import { useState } from "react"
+import { FocusOn } from "react-focus-on"
 import ExternalLink from "../dom/ExternalLink"
 import { getProfileUrl } from "../flist/helpers"
-import { usePrivateChatActions } from "../privateChat/state"
-import { routes } from "../router"
-import { useSocketActions } from "../socket/SocketConnection"
-import ContextMenu, {
-	ContextMenuButton,
-	ContextMenuCheckbox,
-	ContextMenuItem,
-	ContextMenuPanel,
-} from "../ui/ContextMenu"
-import Icon from "../ui/Icon"
-import * as icons from "../ui/icons"
-import { useUserActions } from "../user"
-import CharacterMemoInput from "./CharacterMemoInput"
-import CharacterSummary from "./CharacterSummary"
-import { useCharacterRoles } from "./state"
+import { raisedPanel } from "../ui/components"
+import usePopper from "../ui/usePopper"
+import CharacterMenu from "./CharacterMenu"
 
 interface Props {
 	name: string
-	children: React.ReactNode
+	children: ReactNode
 }
 
 export default function CharacterMenuTarget({ name, children }: Props) {
-	return (
-		<ContextMenu>
-			<ContextMenuButton>
-				<ExternalLink href={getProfileUrl(name)}>{children}</ExternalLink>
-			</ContextMenuButton>
-			<ContextMenuPanel>
-				<CharacterMenu name={name} />
-			</ContextMenuPanel>
-		</ContextMenu>
-	)
-}
-
-function CharacterMenu({ name }: { name: string }) {
-	const { friendships, isBookmarked, isIgnored } = useCharacterRoles(name)
-	const { send } = useSocketActions()
-	const { addBookmark, removeBookmark } = useUserActions()
-	const privateChatActions = usePrivateChatActions(name)
-
+	const { popperRef, referenceRef } = usePopper()
+	const [open, setOpen] = useState(false)
 	return (
 		<>
-			<div className={`p-2 space-y-3 bg-midnight-0`}>
-				<CharacterSummary name={name} />
+			<ExternalLink
+				href={getProfileUrl(name)}
+				ref={referenceRef}
+				onContextMenu={(event) => {
+					event.preventDefault()
+					setOpen(true)
+				}}
+			>
+				{children}
+			</ExternalLink>
 
-				{friendships
-					.filter(({ them }) => them === name)
-					.map((item, index) => (
-						<div
-							key={index}
-							className={`flex flex-row items-center px-2 py-1 space-x-1 text-sm text-green-400 bg-green-500 bg-opacity-20`}
-						>
-							<Icon which={icons.heart} />
-							<div>{item.us}</div>
+			{open && (
+				<Portal>
+					<FocusOn onClickOutside={() => setOpen(false)}>
+						<div ref={popperRef} className={raisedPanel}>
+							<div className={"w-60 bg-midnight-1"}>
+								<CharacterMenu name={name} />
+							</div>
 						</div>
-					))}
-			</div>
-
-			<div className={`flex flex-col`}>
-				<ContextMenuItem icon={<Icon which={icons.link} />}>
-					<ExternalLink href={getProfileUrl(name)}>Profile</ExternalLink>
-				</ContextMenuItem>
-
-				<ContextMenuItem icon={<Icon which={icons.message} />}>
-					<Button
-						onClick={() => {
-							routes.privateChat({ partnerName: name }).push()
-							privateChatActions.open()
-						}}
-					>
-						Message
-					</Button>
-				</ContextMenuItem>
-
-				<ContextMenuCheckbox
-					icon={
-						<Icon
-							which={isBookmarked ? icons.bookmark : icons.bookmarkHollow}
-						/>
-					}
-					checked={isBookmarked}
-					onCheckedChange={(shouldBookmark) => {
-						if (shouldBookmark) {
-							addBookmark({ name }).catch(console.error) // show error toast
-						} else {
-							removeBookmark({ name }).catch(console.error) // show error toast
-						}
-					}}
-				>
-					<Button>{isBookmarked ? "Remove bookmark" : "Bookmark"}</Button>
-				</ContextMenuCheckbox>
-
-				<ContextMenuCheckbox
-					icon={<Icon which={isIgnored ? icons.ignore : icons.ignoreHollow} />}
-					checked={isIgnored}
-					onCheckedChange={(shouldIgnore) => {
-						send({
-							type: "IGN",
-							params: {
-								action: shouldIgnore ? "add" : "delete",
-								character: name,
-							},
-						})
-					}}
-				>
-					<Button>{isIgnored ? "Unignore" : "Ignore"}</Button>
-				</ContextMenuCheckbox>
-			</div>
-
-			<div className={`p-2 bg-midnight-0`}>
-				<CharacterMemoInput name={name} />
-			</div>
+					</FocusOn>
+				</Portal>
+			)}
 		</>
 	)
 }
