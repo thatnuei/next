0e435e689ffa85ad657f39db5118ef3d63e1233a
nuei <61118778+thatnuei@users.noreply.github.com>
2021-09-01 21:24:51 -0500

    remove ChatCommandHandlers

--- src/chat/ChatCommandHandlers.tsx
+++
@@ -1,8 +0,0 @@
-import { useChannelCommandHandler } from "../channel/state"
-import { useNotificationCommandListener } from "../notifications/state"
-
-export default function ChatCommandHandlers() {
-  useChannelCommandHandler()
-  useNotificationCommandListener()
-  return null
-}
